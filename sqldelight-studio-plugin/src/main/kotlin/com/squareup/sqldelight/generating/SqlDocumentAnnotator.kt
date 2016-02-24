1b87bd8d90f59b50b3d6be5bc699d962e90ac181
Alec Strong <astrong@squareup.com>
2016-02-23 19:04:55 -0500

    Create the model files on creation and during the synchronization step instead of the annotation step

--- sqldelight-studio-plugin/src/main/kotlin/com/squareup/sqldelight/generating/SqlDocumentAnnotator.kt
+++ sqldelight-studio-plugin/src/main/kotlin/com/squareup/sqldelight/generating/SqlDocumentAnnotator.kt
@@ -17,48 +17,22 @@ package com.squareup.sqldelight.generating
 
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.ExternalAnnotator
-import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.squareup.sqldelight.SqliteCompiler
 import com.squareup.sqldelight.SqliteCompiler.Status
 import com.squareup.sqldelight.lang.SqliteFile
 
-internal class SqlDocumentAnnotator : ExternalAnnotator<Pair<SqliteFile, TableGenerator>, SqlDocumentAnnotator.Generation>() {
-  private val localFileSystem = LocalFileSystem.getInstance()
-  private val sqliteCompiler = SqliteCompiler<PsiElement>()
-
-  override fun collectInformation(file: PsiFile) = Pair(file as SqliteFile, TableGenerator.create(file))
-
-  override fun doAnnotate(pair: Pair<SqliteFile, TableGenerator>): Generation? {
-    val tableGenerator = pair.second
-    val status = sqliteCompiler.write(tableGenerator)
-    val file = localFileSystem.findFileByIoFile(tableGenerator.outputDirectory)
-    file?.refresh(false, true)
-    val generatedFile = file?.findFileByRelativePath(
-        tableGenerator.packageDirectory + "/" + tableGenerator.generatedFileName + ".java")
-    if (generatedFile != pair.first.generatedFile?.virtualFile) {
-      WriteCommandAction.runWriteCommandAction(pair.first.project, { pair.first.generatedFile?.delete() })
-    }
-    return Generation(status, generatedFile)
-  }
-
-  override fun apply(file: PsiFile, generation: Generation, holder: AnnotationHolder) {
-    when (generation.status.result) {
-      SqliteCompiler.Status.Result.FAILURE -> holder.createErrorAnnotation(
-          generation.status.originatingElement,
-          generation.status.errorMessage)
-      SqliteCompiler.Status.Result.SUCCESS -> {
-        if (generation.generatedFile == null) return
-        val document = FileDocumentManager.getInstance().getDocument(generation.generatedFile)
-        document?.createGuardedBlock(0, document.textLength)
-        (file as SqliteFile).generatedFile = file.getManager().findFile(generation.generatedFile)
-      }
+internal class SqlDocumentAnnotator : ExternalAnnotator<Status<PsiElement>, Status<PsiElement>>() {
+  override fun collectInformation(file: PsiFile) = (file as SqliteFile).status
+  override fun doAnnotate(status: Status<PsiElement>) = status
+  override fun apply(file: PsiFile, status: Status<PsiElement>, holder: AnnotationHolder) {
+    if (status.result == Status.Result.FAILURE) {
+      holder.createErrorAnnotation(status.originatingElement, status.errorMessage)
+    } else {
+      val generatedFile = (file as SqliteFile).generatedFile ?: return
+      val document = FileDocumentManager.getInstance().getDocument(generatedFile.virtualFile)
+      document?.createGuardedBlock(0, document.textLength)
     }
   }
-
-  class Generation internal constructor(val status: Status<PsiElement>, val generatedFile: VirtualFile?)
 }
