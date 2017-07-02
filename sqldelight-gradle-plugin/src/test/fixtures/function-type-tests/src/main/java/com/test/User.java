f70421ed867c6374f368418e812a13a75d97fea4
Hannes Struß <x@hannesstruss.de>
2017-07-02 19:56:24 +0200

    Add fixture test for function types

---
+++ sqldelight-gradle-plugin/src/test/fixtures/function-type-tests/src/main/java/com/test/User.java
@@ -0,0 +1,38 @@
+package com.test;
+
+import com.squareup.sqldelight.ColumnAdapter;
+import com.squareup.sqldelight.EnumColumnAdapter;
+import java.util.List;
+import java.util.Map;
+
+public class User implements UserModel {
+  private static final Creator<User> CREATOR = new Creator<User>() {
+    public User create(long id, int age, String gender) {
+      return new User(id, age, gender);
+    }
+  };
+
+  public static final Factory<User> FACTORY = new Factory<>(CREATOR);
+
+  private final long id;
+  private final int age;
+  private final String gender;
+
+  public User(long id, int age, String gender) {
+    this.id = id;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  @Override public long id() {
+    return id;
+  }
+
+  @Override public int age() {
+    return age;
+  }
+
+  @Override public String gender() {
+    return gender;
+  }
+}
