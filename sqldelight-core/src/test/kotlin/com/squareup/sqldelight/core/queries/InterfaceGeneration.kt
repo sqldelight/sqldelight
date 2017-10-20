62b2284d65489c17eb5170342be50ee31633cde5
Egor Andreevici <andreevich.egor@gmail.com>
2017-10-20 22:51:07 +0300

    Improve test setup in the core module

--- sqldelight-core/src/test/kotlin/com/squareup/sqldelight/core/queries/InterfaceGeneration.kt
+++ sqldelight-core/src/test/kotlin/com/squareup/sqldelight/core/queries/InterfaceGeneration.kt
@@ -1,46 +1,28 @@
 package com.squareup.sqldelight.core.queries
 
-import com.google.common.truth.Truth
-import com.squareup.sqldelight.core.TestEnvironment
+import com.google.common.truth.Truth.assertThat
 import com.squareup.sqldelight.core.compiler.SqlDelightCompiler
-import com.squareup.sqldelight.core.lang.SqlDelightFile
+import com.squareup.sqldelight.core.util.FixtureCompiler
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
-import org.junit.runners.Parameterized.Parameters
-import java.io.File
-import java.util.LinkedHashMap
 
-@RunWith(Parameterized::class)
-class InterfaceGeneration(val fixtureRoot: File, val name: String) {
-  @Test
-  fun execute() {
-    val parser = TestEnvironment()
-    val environment = parser.build(fixtureRoot.path)
-    val output = LinkedHashMap<String, StringBuilder>()
+class InterfaceGeneration {
 
-    environment.forSourceFiles { psiFile ->
-      SqlDelightCompiler.writeQueryInterfaces(psiFile as SqlDelightFile) { fileName ->
-        val builder = StringBuilder()
-        output.put(fileName, builder)
-        return@writeQueryInterfaces builder
-      }
-    }
-
-    for ((fileName, actualOutput) in output) {
-      val expectedFile = File(fixtureRoot, fileName)
-      Truth.assertThat(expectedFile.exists()).named("No file with name $expectedFile").isTrue()
-
-      Truth.assertThat(expectedFile.readText()).named(fileName).isEqualTo(actualOutput.toString())
-    }
+  @Test fun noUniqueQueries() {
+    checkFixtureCompiles("no-unique-queries")
   }
 
-  companion object {
-    @Suppress("unused") // Used by Parameterized JUnit runner reflectively.
-    @Parameters(name = "{1}")
-    @JvmStatic fun parameters(): List<Array<Any>> =
-        File("src/test/query-interface-fixtures").listFiles()
-            .filter { it.isDirectory }
-            .map { arrayOf(it, it.name) }
+  @Test fun queryRequiresType() {
+    checkFixtureCompiles("query-requires-type")
+  }
+
+  private fun checkFixtureCompiles(fixtureRoot: String) {
+    val result = FixtureCompiler.compileFixture(
+        "src/test/query-interface-fixtures/$fixtureRoot",
+        SqlDelightCompiler::writeQueryInterfaces)
+    for ((expectedFile, actualOutput) in result.compilerOutput) {
+      assertThat(expectedFile.exists()).named("No file with name $expectedFile").isTrue()
+      assertThat(expectedFile.readText()).named(expectedFile.name).isEqualTo(
+          actualOutput.toString())
+    }
   }
 }
--- sqldelight-core/src/test/kotlin/com/squareup/sqldelight/core/tables/InterfaceGeneration.kt
+++ sqldelight-core/src/test/kotlin/com/squareup/sqldelight/core/tables/InterfaceGeneration.kt
@@ -1,45 +1,24 @@
 package com.squareup.sqldelight.core.tables
 
 import com.google.common.truth.Truth.assertThat
-import com.squareup.sqldelight.core.TestEnvironment
 import com.squareup.sqldelight.core.compiler.SqlDelightCompiler
-import com.squareup.sqldelight.core.lang.SqlDelightFile
+import com.squareup.sqldelight.core.util.FixtureCompiler
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
-import org.junit.runners.Parameterized.Parameters
-import java.io.File
-import java.util.LinkedHashMap
 
-@RunWith(Parameterized::class)
-class InterfaceGeneration(val fixtureRoot: File, val name: String) {
-  @Test fun execute() {
-    val parser = TestEnvironment()
-    val environment = parser.build(fixtureRoot.path)
-    val output = LinkedHashMap<String, StringBuilder>()
+class InterfaceGeneration {
 
-    environment.forSourceFiles { psiFile ->
-      SqlDelightCompiler.writeTableInterfaces(psiFile as SqlDelightFile) { fileName ->
-        val builder = StringBuilder()
-        output.put(fileName, builder)
-        return@writeTableInterfaces builder
-      }
-    }
-
-    for ((fileName, actualOutput) in output) {
-      val expectedFile = File(fixtureRoot, fileName)
-      assertThat(expectedFile.exists()).named("No file with name $expectedFile").isTrue()
-
-      assertThat(expectedFile.readText()).named(fileName).isEqualTo(actualOutput.toString())
-    }
+  @Test fun requiresAdapter() {
+    checkFixtureCompiles("requires-adapter")
   }
 
-  companion object {
-    @Suppress("unused") // Used by Parameterized JUnit runner reflectively.
-    @Parameters(name = "{1}")
-    @JvmStatic fun parameters(): List<Array<Any>> =
-        File("src/test/table-interface-fixtures").listFiles()
-            .filter { it.isDirectory }
-            .map { arrayOf(it, it.name) }
+  private fun checkFixtureCompiles(fixtureRoot: String) {
+    val result = FixtureCompiler.compileFixture(
+        "src/test/table-interface-fixtures/$fixtureRoot",
+        SqlDelightCompiler::writeTableInterfaces)
+    for ((expectedFile, actualOutput) in result.compilerOutput) {
+      assertThat(expectedFile.exists()).named("No file with name $expectedFile").isTrue()
+      assertThat(expectedFile.readText()).named(expectedFile.name).isEqualTo(
+          actualOutput.toString())
+    }
   }
 }
