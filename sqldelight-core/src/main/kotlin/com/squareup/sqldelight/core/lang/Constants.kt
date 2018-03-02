261f9d62e5f6cca09a97a035cde577e40cce586d
Alec Strong <astrong@squareup.com>
2018-03-02 14:38:33 -0500

    Generate the query subclass for select queries with parameters

--- sqldelight-core/src/main/kotlin/com/squareup/sqldelight/core/lang/Constants.kt
+++ sqldelight-core/src/main/kotlin/com/squareup/sqldelight/core/lang/Constants.kt
@@ -3,7 +3,7 @@ package com.squareup.sqldelight.core.lang
 import com.alecstrong.sqlite.psi.core.psi.SqliteCreateTableStmt
 import com.squareup.kotlinpoet.ClassName
 
-internal val RESULT_SET_TYPE = ClassName("com.squareup.sqldelight.db", "ResultSet")
+internal val RESULT_SET_TYPE = ClassName("com.squareup.sqldelight.db", "SqlResultSet")
 internal val RESULT_SET_NAME = "resultSet"
 
 internal val DATABASE_TYPE = ClassName("com.squareup.sqldelight.db", "SqlDatabase")
@@ -17,5 +17,10 @@ internal val SqliteCreateTableStmt.adapterName
   get() = "${tableName.name}$ADAPTER_NAME"
 
 internal val STATEMENT_NAME = "statement"
+internal val STATEMENT_TYPE = ClassName("com.squareup.sqldelight.db", "SqlPreparedStatement")
 
 internal val QUERY_TYPE = ClassName("com.squareup.sqldelight", "Query")
+
+internal val DIRTIED_FUNCTION = "dirtied"
+
+internal val MAPPER_NAME = "mapper"
