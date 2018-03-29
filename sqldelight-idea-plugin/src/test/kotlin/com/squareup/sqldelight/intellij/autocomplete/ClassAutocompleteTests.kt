30e89e453da77e68671740f8f8dfba24523e7e2d
Alec Strong <astrong@squareup.com>
2018-03-28 20:56:03 -0400

    Add back class completion and resolution

---
+++ sqldelight-idea-plugin/src/test/kotlin/com/squareup/sqldelight/intellij/autocomplete/ClassAutocompleteTests.kt
@@ -0,0 +1,34 @@
+package com.squareup.sqldelight.intellij.autocomplete
+
+import com.squareup.sqldelight.core.lang.SqlDelightFileType
+import com.squareup.sqldelight.intellij.SqlDelightProjectTestCase
+
+class ClassAutocompleteTests : SqlDelightProjectTestCase() {
+  fun testClassAutocompleteFindsLocalKotlinClasses() {
+    myFixture.configureByText(SqlDelightFileType, """
+      |CREATE TABLE test (
+      |  value TEXT AS Ko<caret>
+      |);
+    """.trimMargin())
+
+    myFixture.completeBasic()
+
+    myFixture.checkResult("""
+      |import com.example.KotlinClass;
+      |
+      |CREATE TABLE test (
+      |  value TEXT AS KotlinClass
+      |);
+    """.trimMargin())
+  }
+
+  fun testUnresolvedClassName() {
+    myFixture.configureByText(SqlDelightFileType, """
+      |CREATE TABLE test (
+      |  value TEXT AS <error descr="Unresolved reference: KoolKidz">KoolKidz</error>
+      |);
+    """.trimMargin())
+
+    myFixture.checkHighlighting()
+  }
+}
