a49d2b20407168e861c20d974e6f906d7ca60727
Alec Strong <AlecStrong@users.noreply.github.com>
2018-04-01 10:46:31 -0400

    Introduce a kotlin gradle plugin (#793)

---
+++ sqldelight-gradle/plugin/src/test/kotlin/com/squareup/sqldelight/PropertiesFileTest.kt
@@ -0,0 +1,36 @@
+package com.squareup.sqldelight
+
+import com.google.common.truth.Truth.assertThat
+import com.squareup.sqldelight.core.SqlDelightPropertiesFile
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Test
+import java.io.File
+
+class PropertiesFileTest {
+  @Test fun `properties file generates correctly`() {
+    val fixtureRoot = File("src/test/properties-file")
+
+    GradleRunner.create()
+        .withProjectDir(fixtureRoot)
+        .withPluginClasspath()
+        .withArguments("clean", "generateSqlDelightInterface", "--stacktrace",
+            "-Dsqldelight.skip.runtime=true")
+        .build()
+
+    // verify
+    val propertiesFile = File(fixtureRoot, SqlDelightPropertiesFile.NAME)
+    assertThat(propertiesFile.exists()).isTrue()
+
+    val properties = SqlDelightPropertiesFile.fromFile(propertiesFile)
+    assertThat(properties.packageName).isEqualTo("com.example")
+    assertThat(properties.outputDirectory).isEqualTo("build/sqldelight")
+    assertThat(properties.sourceSets).hasSize(1)
+
+    with(properties.sourceSets[0]) {
+      assertThat(this).hasSize(1)
+      assertThat(this[0]).isEqualTo("src/main/sqldelight")
+    }
+
+    propertiesFile.delete()
+  }
+}
