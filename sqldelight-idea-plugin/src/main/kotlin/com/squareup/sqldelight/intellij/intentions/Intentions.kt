933ab4a60b236a4f85dadfe87364c2ebe673aebf
Egor Andreevici <egor@squareup.com>
2018-05-02 11:48:34 -0400

    Add intention to expand wildcard in select statement into column names

---
+++ sqldelight-idea-plugin/src/main/kotlin/com/squareup/sqldelight/intellij/intentions/Intentions.kt
@@ -0,0 +1,5 @@
+package com.squareup.sqldelight.intellij.intentions
+
+internal const val INTENTIONS_FAMILY_NAME_REFACTORINGS = "Refactorings"
+
+internal const val INTENTION_EXPAND_COLUMN_NAMES_TEXT = "Expand * into column names"
