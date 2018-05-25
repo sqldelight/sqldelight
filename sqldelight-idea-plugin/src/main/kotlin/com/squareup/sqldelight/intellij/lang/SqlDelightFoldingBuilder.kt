620d0f8483190e2aed79b008e4a55a41182c97a2
Egor Andreevici <andreevich.egor@gmail.com>
2018-05-25 07:40:04 -0400

    Implement folding support for views, indexes and triggers (#842)

--- sqldelight-idea-plugin/src/main/kotlin/com/squareup/sqldelight/intellij/lang/SqlDelightFoldingBuilder.kt
+++ sqldelight-idea-plugin/src/main/kotlin/com/squareup/sqldelight/intellij/lang/SqlDelightFoldingBuilder.kt
@@ -16,6 +16,9 @@
 
 package com.squareup.sqldelight.intellij.lang
 
+import com.alecstrong.sqlite.psi.core.psi.SqliteCreateIndexStmt
+import com.alecstrong.sqlite.psi.core.psi.SqliteCreateTriggerStmt
+import com.alecstrong.sqlite.psi.core.psi.SqliteCreateViewStmt
 import com.alecstrong.sqlite.psi.core.psi.SqliteTypes
 import com.intellij.lang.ASTNode
 import com.intellij.lang.folding.FoldingBuilder
@@ -28,6 +31,7 @@ import com.intellij.psi.PsiWhiteSpace
 import com.squareup.sqldelight.core.SqlDelightTypes
 import com.squareup.sqldelight.core.psi.SqlDelightStmtIdentifier
 import com.squareup.sqldelight.intellij.util.prevSiblingOfType
+import org.jetbrains.kotlin.psi.psiUtil.endOffset
 import org.jetbrains.kotlin.psi.psiUtil.getChildOfType
 
 class SqlDelightFoldingBuilder : FoldingBuilder, DumbAware {
@@ -38,44 +42,79 @@ class SqlDelightFoldingBuilder : FoldingBuilder, DumbAware {
   private fun ASTNode.createFoldingDescriptors(): Array<FoldingDescriptor> {
     return getChildren(null)
         .filter { it.elementType == SqlDelightTypes.SQL_STMT_LIST }
-        .flatMap { it.getChildren(null).toList() }
-        .mapNotNull { statement ->
-          when (statement.elementType) {
-            SqlDelightTypes.IMPORT_STMT_LIST -> statement.psi.toImportListDescriptor()
-            SqliteTypes.STATEMENT -> {
-              val psi = statement.psi
-              val sqlStatement = statement.firstChildNode?.firstChildNode
-              if (sqlStatement?.elementType == SqliteTypes.CREATE_TABLE_STMT) {
-                psi.toCreateTableDescriptor(sqlStatement?.psi)
-              } else {
-                psi.toStatementDescriptor(psi.prevSiblingOfType<SqlDelightStmtIdentifier>())
+        .flatMap {
+          val descriptors = mutableListOf<FoldingDescriptor>()
+          val statements = it.getChildren(null).toList()
+          for (statement in statements) {
+            when (statement.elementType) {
+              SqlDelightTypes.IMPORT_STMT_LIST ->
+                statement.psi.toImportListDescriptor()?.let(descriptors::add)
+              SqliteTypes.STATEMENT -> {
+                val psi = statement.psi
+                val sqlStatement = statement.firstChildNode?.firstChildNode
+                when (sqlStatement?.elementType) {
+                  SqliteTypes.CREATE_TABLE_STMT ->
+                    psi.toCreateTableDescriptor(sqlStatement?.psi)?.let(descriptors::add)
+                  SqliteTypes.CREATE_VIEW_STMT ->
+                    psi.toCreateViewDescriptor(sqlStatement?.psi)?.let(descriptors::add)
+                  SqliteTypes.CREATE_TRIGGER_STMT ->
+                    psi.toCreateTriggerDescriptor(sqlStatement?.psi)?.let(descriptors::add)
+                  SqliteTypes.CREATE_INDEX_STMT ->
+                    psi.toCreateIndexDescriptor(sqlStatement?.psi)?.let(descriptors::add)
+                }
+                val stmtIdentifier = psi.prevSiblingOfType<SqlDelightStmtIdentifier>()
+                if (stmtIdentifier?.identifier() != null) {
+                  psi.toStatementDescriptor(stmtIdentifier)?.let(descriptors::add)
+                }
               }
             }
-            else -> null
           }
+          return@flatMap descriptors
         }
         .toTypedArray()
   }
 
   private fun PsiElement.toCreateTableDescriptor(createTableStmt: PsiElement?): FoldingDescriptor? {
     val openingBraceElement = createTableStmt?.node?.findChildByType(SqliteTypes.LP) ?: return null
-    val start = openingBraceElement.textRange.startOffset
-    val end = lastChild.textRange.endOffset
+    val start = openingBraceElement.startOffset
+    val end = lastChild.endOffset
+    if (start >= end) return null
     return NamedFoldingDescriptor(this, start, end, null, "(...);")
+  }
+
+  private fun PsiElement.toCreateViewDescriptor(createViewStmt: PsiElement?): FoldingDescriptor? {
+    val viewNameElement = (createViewStmt as? SqliteCreateViewStmt)?.viewName ?: return null
+    return toStatementDescriptor(viewNameElement)
+  }
+
+  private fun PsiElement.toCreateTriggerDescriptor(
+    createTriggerStmt: PsiElement?
+  ): FoldingDescriptor? {
+    val triggerNameElement =
+        (createTriggerStmt as? SqliteCreateTriggerStmt)?.triggerName ?: return null
+    return toStatementDescriptor(triggerNameElement)
+  }
+
+  private fun PsiElement.toCreateIndexDescriptor(createIndexStmt: PsiElement?): FoldingDescriptor? {
+    val indexNameElement = (createIndexStmt as? SqliteCreateIndexStmt)?.indexName ?: return null
+    return toStatementDescriptor(indexNameElement)
   }
 
   private fun PsiElement.toStatementDescriptor(stmtIdentifier: PsiElement?): FoldingDescriptor? {
     if (stmtIdentifier == null) return null
-    val start = stmtIdentifier.textRange.endOffset
-    val end = lastChild.textRange.endOffset
+    if (lastChild.node.elementType != SqliteTypes.SEMI) return null
+    val start = stmtIdentifier.endOffset
+    val end = lastChild.endOffset
+    if (start >= end) return null
     return NamedFoldingDescriptor(this, start, end, null, "...")
   }
 
   private fun PsiElement.toImportListDescriptor(): FoldingDescriptor? {
     if (children.size < 2) return null
     val whitespaceElement = firstChild.getChildOfType<PsiWhiteSpace>() ?: return null
-    val start = whitespaceElement.textRange.endOffset
-    val end = lastChild.textRange.endOffset
+    val start = whitespaceElement.endOffset
+    val end = lastChild.endOffset
+    if (start >= end) return null
     return NamedFoldingDescriptor(this, start, end, null, "...")
   }
 
