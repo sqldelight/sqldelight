0a7b03e78d07699a9420a2a408467b833988dc3a
Egor Andreevici <egor@squareup.com>
2018-06-11 14:21:58 -0400

    Use ArrayDeque

--- sqlite-migrations/src/main/kotlin/com/squareup/sqlite/migrations/DatabaseFilesCollector.kt
+++ sqlite-migrations/src/main/kotlin/com/squareup/sqlite/migrations/DatabaseFilesCollector.kt
@@ -1,17 +1,18 @@
 package com.squareup.sqlite.migrations
 
 import java.io.File
+import java.util.ArrayDeque
 
 object DatabaseFilesCollector {
   inline fun forDatabaseFiles(sourceFolders: Iterable<File>, block: (File) -> Unit) {
-    val folders = sourceFolders.toMutableList()
+    val folders = ArrayDeque<File>().apply { sourceFolders.forEach(this::addLast) }
     while (folders.isNotEmpty()) {
-      val folder = folders.removeAt(0)
+      val folder = folders.removeFirst()
       folder.listFiles()?.forEach { file ->
         if (file.name.endsWith(".db")) {
           block(file)
         } else if (file.isDirectory) {
-          folders.add(file)
+          folders.addLast(file)
         }
       }
     }
