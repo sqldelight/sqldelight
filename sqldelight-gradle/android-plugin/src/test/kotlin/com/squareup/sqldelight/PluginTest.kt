928cb9bac7fd64d7b9c03f6dd3a110e18361a005
Alec Strong <astrong@squareup.com>
2018-06-18 13:56:41 -0400

    Throw an exception if the wrong plugin is applied for the project

---
+++ sqldelight-gradle/android-plugin/src/test/kotlin/com/squareup/sqldelight/PluginTest.kt
@@ -0,0 +1,32 @@
+package com.squareup.sqldelight
+
+import com.google.common.truth.Truth
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Test
+import java.io.File
+
+class PluginTest {
+  @Test
+  fun `Applying the android plugin without AGP applied throws`() {
+    val fixtureRoot = File("src/test/kotlin-project")
+    val runner = GradleRunner.create()
+        .withProjectDir(fixtureRoot)
+        .withPluginClasspath()
+
+    val result = runner
+        .withArguments("build", "--stacktrace", "-Dsqldelight.skip.runtime=true")
+        .buildAndFail()
+    Truth.assertThat(result.output)
+        .contains("""
+      Kotlin projects need to apply the sqldelight kotlin plugin:
+
+      buildscript {
+        dependencies {
+          classpath "com.squareup.sqldelight:gradle-plugin:$VERSION
+        }
+      }
+
+      apply plugin: "com.squareup.sqldelight"
+      """.trimIndent())
+  }
+}
