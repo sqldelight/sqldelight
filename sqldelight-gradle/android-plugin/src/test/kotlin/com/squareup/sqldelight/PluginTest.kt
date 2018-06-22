384068402d83a771b72e39e06ef6d261b40978d0
Alec Strong <AlecStrong@users.noreply.github.com>
2018-06-22 12:56:56 -0400

    Support android library projects (#889)

--- sqldelight-gradle/android-plugin/src/test/kotlin/com/squareup/sqldelight/PluginTest.kt
+++ sqldelight-gradle/android-plugin/src/test/kotlin/com/squareup/sqldelight/PluginTest.kt
@@ -1,6 +1,7 @@
 package com.squareup.sqldelight
 
 import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import org.gradle.testkit.runner.GradleRunner
 import org.junit.Test
 import java.io.File
@@ -28,5 +29,19 @@ class PluginTest {
 
       apply plugin: "com.squareup.sqldelight"
       """.trimIndent())
+  }
+
+  @Test
+  fun `Applying the android plugin works fine for library projects`() {
+    val fixtureRoot = File("src/test/library-project")
+    val runner = GradleRunner.create()
+        .withProjectDir(fixtureRoot)
+        .withPluginClasspath()
+
+    val result = runner
+        .withArguments("clean", "generateDebugSqlDelightInterface", "--stacktrace",
+            "-Dsqldelight.skip.runtime=true")
+        .build()
+    assertThat(result.output).contains("BUILD SUCCESSFUL")
   }
 }
