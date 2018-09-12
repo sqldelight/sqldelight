2e7c914ef026b4504bff194e4863bb2eb95f0cf2
Jake Wharton <jakewharton@gmail.com>
2018-09-11 23:53:21 -0400

    Combine the Gradle plugins into one (#930)
    This varies its behavior based on whether or not an Android plugin is applied and always requires some Kotlin plugin.

---
+++ sqldelight-gradle-plugin/src/test/kotlin/com/squareup/sqldelight/GenerateSchemaTest.kt
@@ -0,0 +1,63 @@
+package com.squareup.sqldelight
+
+import com.google.common.truth.Truth.assertThat
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Test
+import java.io.File
+
+class GenerateSchemaTest {
+  @Test fun `schema file generates correctly`() {
+    val fixtureRoot = File("src/test/schema-file")
+    val schemaFile = File(fixtureRoot, "src/main/sqldelight/databases/1.db")
+    if (schemaFile.exists()) schemaFile.delete()
+
+    GradleRunner.create()
+        .withProjectDir(fixtureRoot)
+        .withPluginClasspath()
+        .withArguments("clean", "generateSqlDelightSchema", "--stacktrace",
+            "-Dsqldelight.skip.runtime=true")
+        .build()
+
+    // verify
+    assertThat(schemaFile.exists())
+        .isTrue()
+
+    schemaFile.delete()
+  }
+
+  @Test fun `schema file generates correctly with existing sqm files`() {
+    val fixtureRoot = File("src/test/schema-file-sqm")
+
+    GradleRunner.create()
+        .withProjectDir(fixtureRoot)
+        .withPluginClasspath()
+        .withArguments("clean", "generateSqlDelightSchema", "--stacktrace",
+            "-Dsqldelight.skip.runtime=true")
+        .build()
+
+    // verify
+    val schemaFile = File(fixtureRoot, "src/main/sqldelight/databases/3.db")
+    assertThat(schemaFile.exists())
+        .isTrue()
+
+    schemaFile.delete()
+  }
+
+  @Test fun `schema file generates correctly for android`() {
+    val fixtureRoot = File("src/test/schema-file")
+    val schemaFile = File(fixtureRoot, "src/main/sqldelight/databases/1.db")
+    if (schemaFile.exists()) schemaFile.delete()
+
+    GradleRunner.create()
+        .withProjectDir(fixtureRoot)
+        .withPluginClasspath()
+        .withArguments(
+            "clean", "generateSqlDelightSchema", "--stacktrace",
+            "-Dsqldelight.skip.runtime=true"
+        )
+        .build()
+
+    // verify
+    assertThat(schemaFile.exists()).isTrue()
+  }
+}
