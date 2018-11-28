ceb60519fabe9d9c1b0d8ea7ba5dc941a199139a
Alec Strong <AlecStrong@users.noreply.github.com>
2018-11-27 21:44:53 -0500

    A basic test that verifies a statement compiled on one thread can be run on another (#1082)

--- sqldelight-gradle-plugin/src/test/integration-multiplatform/src/commonMain/kotlin/com/squareup/sqldelight/integration/common.kt
+++ sqldelight-gradle-plugin/src/test/integration-multiplatform/src/commonMain/kotlin/com/squareup/sqldelight/integration/common.kt
@@ -3,3 +3,13 @@ package com.squareup.sqldelight.integration
 import com.squareup.sqldelight.db.SqlDatabase
 
 expect fun createSqlDatabase(): SqlDatabase
+
+// TODO: Replace these with stately primitives?
+expect class MPWorker(){
+  fun <T> runBackground(backJob:()->T):MPFuture<T>
+  fun requestTermination()
+}
+
+expect class MPFuture<T>{
+  fun consume():T
+}
