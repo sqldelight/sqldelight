5f7da294beac586e3fa4e167ff306bdfba7fd815
Alec Strong <astrong@squareup.com>
2018-12-29 08:52:32 -0500

    Upgrade to kotlin 1.3.11

--- drivers/ios-driver/src/nativeTest/kotlin/com/squareup/sqldelight/drivers/ios/IosDriverTest.kt
+++ drivers/ios-driver/src/nativeTest/kotlin/com/squareup/sqldelight/drivers/ios/IosDriverTest.kt
@@ -16,33 +16,11 @@ class IosDriverTest : DriverTest() {
     return NativeSqlDatabase(schema, name)
   }
 
-  // TODO: https://github.com/JetBrains/kotlin-native/issues/2328
-
-  @BeforeTest fun setup2() {
-    super.setup()
-  }
-
-  @AfterTest fun tearDown2() {
-    super.tearDown()
-  }
-
   // Sanity check of the driver.
   @Test fun basicTest() {
     val cursor = database.getConnection().prepareStatement(0, "SELECT 1", SELECT, 0).executeQuery()
     cursor.next()
     assertEquals(1, cursor.getLong(0))
     cursor.close()
-  }
-
-  @Test fun `insert can run multiple times2`() {
-    super.`insert can run multiple times`()
-  }
-
-  @Test fun `query can run multiple times2`() {
-    super.`query can run multiple times`()
-  }
-
-  @Test fun `SqlResultSet getters return null if the column values are NULL2`() {
-    super.`SqlResultSet getters return null if the column values are NULL`()
   }
 }
