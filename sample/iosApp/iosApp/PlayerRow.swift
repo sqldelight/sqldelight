c8e7d9c5ab8e9e6658b8015bd6562c5a0769f4a6
Kevin Galligan <kgalligan@gmail.com>
2019-01-18 17:18:43 -0500

    Custom rows

--- sample/iosApp/iosApp/PlayerRow.swift
+++ sample/iosApp/iosApp/PlayerRow.swift
@@ -11,15 +11,18 @@ import UIKit
 import common
 
 class PlayerRow: UITableViewCell, PlayerCell {
+    @IBOutlet weak var nameText: UILabel!
+    @IBOutlet weak var teamText: UILabel!
+    @IBOutlet weak var numberText: UILabel!
     func fillName(name: String) {
-        textLabel?.text = name
+        nameText.text = name
     }
     
     func fillNumber(number: String) {
-        
+        numberText.text = number
     }
     
     func fillTeamName(teamName: String) {
-        detailTextLabel?.text = teamName
+        teamText.text = teamName
     }
 }
