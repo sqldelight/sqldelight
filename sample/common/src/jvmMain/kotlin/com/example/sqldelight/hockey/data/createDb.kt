c354d3b283450aeb5f9e0996cc124dc1f0c5d7cc
Kevin Galligan <kgalligan@gmail.com>
2019-01-18 18:20:36 -0500

    Make android driver lateinit. Minor reformat

--- sample/common/src/jvmMain/kotlin/com/example/sqldelight/hockey/data/createDb.kt
+++ sample/common/src/jvmMain/kotlin/com/example/sqldelight/hockey/data/createDb.kt
@@ -1,15 +1,12 @@
 package com.example.sqldelight.hockey.data
 
 import com.example.sqldelight.hockey.HockeyDb
+import com.example.sqldelight.hockey.data.AndroidDb.driver
 import com.squareup.sqldelight.db.SqlDriver
 import java.lang.IllegalStateException
 
-var driver: SqlDriver? = null
-
-actual fun createDb(): HockeyDb {
-  driver?.let {
-    return createQueryWrapper(it)
-  }
-
-  throw IllegalStateException("Driver must be set")
+object AndroidDb{
+  lateinit var driver: SqlDriver
 }
+
+actual fun createDb(): HockeyDb = createQueryWrapper(driver)
