a6c17843cab187df26b8469bb5c3dbdba9f7a2e2
Alec Strong <astrong@squareup.com>
2019-01-20 13:28:12 -0500

    Default to current package when resolving non-primitive types

--- sqldelight-idea-plugin/src/main/kotlin/com/squareup/sqldelight/intellij/FileIndex.kt
+++ sqldelight-idea-plugin/src/main/kotlin/com/squareup/sqldelight/intellij/FileIndex.kt
@@ -40,7 +40,7 @@ class FileIndex(
       file
     }
     val folder = sourceFolders(original)
-        .first { PsiTreeUtil.findCommonParent(original, it) != null }
+        .firstOrNull { PsiTreeUtil.findCommonParent(original, it) != null } ?: return ""
     val folderPath = folder.virtualFile.path
     val filePath = original.virtualFile!!.path
     return filePath.substring(folderPath.length + 1, filePath.indexOf(original.name) - 1).replace('/', '.')
---
+++ sqldelight-idea-plugin/src/test/kotlin/com/squareup/sqldelight/intellij/annotations/ClassNameAnnotatorTest.kt
@@ -0,0 +1,13 @@
+package com.squareup.sqldelight.intellij.annotations
+
+import com.squareup.sqldelight.intellij.SqlDelightProjectTestCase
+
+class ClassNameAnnotatorTest : SqlDelightProjectTestCase() {
+  fun testResolveOnSamePackageImport() {
+    myFixture.openFileInEditor(
+        tempRoot.findFileByRelativePath("src/main/sqldelight/com/example/Main.sq")!!
+    )
+
+    myFixture.checkHighlighting()
+  }
+}
