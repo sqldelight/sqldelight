e82df0430cb06a0e24744381cc8a8c011071d6fd
Alec Strong <astrong@squareup.com>
2019-02-23 11:21:51 -0500

    The gradle DSL is powered entirely by methodMissing in groovy in the SqlDelightExtension class

--- sqldelight-gradle-plugin/src/main/kotlin/com/squareup/sqldelight/gradle/SqlDelightExtension.kt
+++ sqldelight-gradle-plugin/src/main/kotlin/com/squareup/sqldelight/gradle/SqlDelightExtension.kt
@@ -1,11 +1,39 @@
 package com.squareup.sqldelight.gradle
 
-import org.gradle.api.file.FileCollection
-import java.io.File
+import groovy.lang.Closure
+import org.gradle.api.Project
+import org.gradle.util.ConfigureUtil
 
-open class SqlDelightExtension(
-  var packageName: String? = null,
-  var className: String? = null,
-  var sourceSet: FileCollection? = null,
-  var schemaOutputDirectory: File? = null
-)
+open class SqlDelightExtension {
+  internal val databases = mutableListOf<SqlDelightDatabase>()
+  internal var configuringDatabase: SqlDelightDatabase? = null
+  internal lateinit var project: Project
+
+  fun methodMissing(name: String, args: Any): Any {
+    configuringDatabase?.methodMissing(name, args)?.let { return it }
+
+    val closure = (args as? Array<*>)?.getOrNull(0) as? Closure<*>
+        ?: throw IllegalStateException("""
+        |Expected a closure for database names:
+        |
+        |sqldelight {
+        |  $name {
+        |    packageName = "com.sample"
+        |    sourceSet = files("src/main/sqldelight")
+        |  }
+        |}
+      """.trimMargin())
+
+    val database = SqlDelightDatabase(project, name = name)
+    configuringDatabase = database
+    ConfigureUtil.configure(closure, database)
+    configuringDatabase = null
+
+    if (databases.any { it.name == database.name }) {
+      throw IllegalStateException("There is already a database defined for ${database.name}")
+    }
+
+    databases.add(database)
+    return Unit
+  }
+}
