9dc698afa881ccd193de7d207193263615986b81
Alec Strong <astrong@squareup.com>
2019-02-26 12:41:26 -0500

    Continue to handle old DSL methods

--- sqldelight-gradle-plugin/src/main/kotlin/com/squareup/sqldelight/gradle/SqlDelightExtension.kt
+++ sqldelight-gradle-plugin/src/main/kotlin/com/squareup/sqldelight/gradle/SqlDelightExtension.kt
@@ -1,13 +1,26 @@
 package com.squareup.sqldelight.gradle
 
 import groovy.lang.Closure
+import org.gradle.api.GradleException
 import org.gradle.api.Project
+import org.gradle.api.file.FileCollection
 import org.gradle.util.ConfigureUtil
+import java.io.File
 
 open class SqlDelightExtension {
   internal val databases = mutableListOf<SqlDelightDatabase>()
   internal var configuringDatabase: SqlDelightDatabase? = null
   internal lateinit var project: Project
+
+  // TODO: Remove these after 1.1.0
+  var packageName: String? = null
+    set(value) = newDsl()
+  var className: String? = null
+    set(value) = newDsl()
+  var sourceSet: FileCollection? = null
+    set(value) = newDsl()
+  var schemaOutputDirectory: File? = null
+    set(value) = newDsl()
 
   fun methodMissing(name: String, args: Any): Any {
     configuringDatabase?.methodMissing(name, args)?.let { return it }
@@ -35,5 +48,28 @@ open class SqlDelightExtension {
 
     databases.add(database)
     return Unit
+  }
+
+  companion object {
+    private fun newDsl(): Nothing = throw GradleException("""
+      |Format of specifying databases has changed from:
+      |
+      |sqldelight {
+      |  className = "MyDatabase"
+      |  packageName = "com.example"
+      |  sourceSet = files("src/main/sqldelight")
+      |  schemaOutputDirectory = file("src/main/sqldelight/migrations")
+      |}
+      |
+      |to
+      |
+      |sqldelight {
+      |  MyDatabase {
+      |    packageName = "com.example"
+      |    sourceFolders = ["sqldelight"]
+      |    schemaOutputDirectory = file("src/main/sqldelight/migrations")
+      |  }
+      |}
+    """.trimMargin())
   }
 }
