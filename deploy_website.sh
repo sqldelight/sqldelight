66487d3e95e31cc3ccc47016f1cd606dda24214f
Alec Strong <astrong@squareup.com>
2019-12-13 13:09:18 -0500

    Also include Dokka docs in the website

--- deploy_website.sh
+++ deploy_website.sh
@@ -20,6 +20,22 @@ git clone $REPO $DIR
 # Move working directory into temp folder
 cd $DIR
 
+# Generate the API docs
+./gradlew dokka
+
+# Dokka filenames like `-http-url/index.md` don't work well with MkDocs <title> tags.
+# Assign metadata to the file's first Markdown heading.
+# https://www.mkdocs.org/user-guide/writing-your-docs/#meta-data
+title_markdown_file() {
+  TITLE_PATTERN="s/^[#]+ *(.*)/title: \1 - OkHttp/"
+  echo "---"                                                     > "$1.fixed"
+  cat $1 | sed -E "$TITLE_PATTERN" | grep "title: " | head -n 1 >> "$1.fixed"
+  echo "---"                                                    >> "$1.fixed"
+  echo                                                          >> "$1.fixed"
+  cat $1                                                        >> "$1.fixed"
+  mv "$1.fixed" "$1"
+}
+
 # Copy in special files that GitHub wants in the project root.
 cp UPGRADING.md docs/upgrading.md
 cp CHANGELOG.md docs/changelog.md
