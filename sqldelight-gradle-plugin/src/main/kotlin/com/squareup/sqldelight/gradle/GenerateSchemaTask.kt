868bffdb23275c90deb15f9867a0f0b292c9caa6
Jake Wharton <jakew@google.com>
2019-12-16 22:57:23 -0500

    Convert GenerateSchemaTask to use Gradle worker

--- sqldelight-gradle-plugin/src/main/kotlin/com/squareup/sqldelight/gradle/GenerateSchemaTask.kt
+++ sqldelight-gradle-plugin/src/main/kotlin/com/squareup/sqldelight/gradle/GenerateSchemaTask.kt
@@ -4,7 +4,10 @@ import com.squareup.sqldelight.VERSION
 import com.squareup.sqldelight.core.SqlDelightEnvironment
 import com.squareup.sqldelight.core.lang.SqlDelightFile
 import com.squareup.sqldelight.core.lang.util.forInitializationStatements
+import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.FileTree
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Internal
@@ -14,12 +17,16 @@ import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.SkipWhenEmpty
 import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.CacheableTask
+import org.gradle.workers.WorkAction
+import org.gradle.workers.WorkParameters
+import org.gradle.workers.WorkerExecutor
 import java.io.File
 import java.sql.DriverManager
+import javax.inject.Inject
 
 @CacheableTask
-open class GenerateSchemaTask : SourceTask() {
+open class GenerateSchemaTask
+@Inject constructor(val workerExecutor: WorkerExecutor) : SourceTask() {
   @Suppress("unused") // Required to invalidate the task on version updates.
   @Input fun pluginVersion() = VERSION
 
@@ -31,24 +38,10 @@ open class GenerateSchemaTask : SourceTask() {
 
   @TaskAction
   fun generateSchemaFile() {
-    val environment = SqlDelightEnvironment(
-        sourceFolders = sourceFolders.filter { it.exists() },
-        dependencyFolders = emptyList(),
-        moduleName = project.name
-    )
-
-    var maxVersion = 1
-
-    environment.forMigrationFiles { migrationFile ->
-      maxVersion = maxOf(maxVersion, migrationFile.version + 1)
-    }
-
-    DriverManager.getConnection("jdbc:sqlite:$outputDirectory/$maxVersion.db").use { connection ->
-      val sourceFiles = ArrayList<SqlDelightFile>()
-      environment.forSourceFiles { file -> sourceFiles.add(file as SqlDelightFile) }
-      sourceFiles.forInitializationStatements { sqlText ->
-        connection.prepareStatement(sqlText).execute()
-      }
+    workerExecutor.noIsolation().submit(GenerateSchema::class.java) {
+      it.sourceFolders.set(sourceFolders.filter(File::exists))
+      it.outputDirectory.set(outputDirectory)
+      it.moduleName.set(project.name)
     }
   }
 
@@ -57,5 +50,35 @@ open class GenerateSchemaTask : SourceTask() {
   @PathSensitive(PathSensitivity.RELATIVE)
   override fun getSource(): FileTree {
     return super.getSource()
+  }
+
+  interface GenerateSchemaWorkParameters : WorkParameters {
+    val sourceFolders: Property<List<File>>
+    val outputDirectory: DirectoryProperty
+    val moduleName: Property<String>
+  }
+
+  abstract class GenerateSchema : WorkAction<GenerateSchemaWorkParameters> {
+    override fun execute() {
+      val environment = SqlDelightEnvironment(
+          sourceFolders = parameters.sourceFolders.get(),
+          dependencyFolders = emptyList(),
+          moduleName = parameters.moduleName.get()
+      )
+
+      var maxVersion = 1
+      environment.forMigrationFiles { migrationFile ->
+        maxVersion = maxOf(maxVersion, migrationFile.version + 1)
+      }
+
+      val outputDirectory = parameters.outputDirectory.get().asFile
+      DriverManager.getConnection("jdbc:sqlite:$outputDirectory/$maxVersion.db").use { connection ->
+        val sourceFiles = ArrayList<SqlDelightFile>()
+        environment.forSourceFiles { file -> sourceFiles.add(file as SqlDelightFile) }
+        sourceFiles.forInitializationStatements { sqlText ->
+          connection.prepareStatement(sqlText).execute()
+        }
+      }
+    }
   }
 }
