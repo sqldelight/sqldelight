f5b4c736fb9a2e1a5abd2a64629214260359dcb3
Alec Strong <astrong@squareup.com>
2019-12-19 09:26:27 -0500

    Provide PR and Snapshot workflows for github actions

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -6,19 +6,12 @@
 # http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
 SLUG="cashapp/sqldelight"
-JDK="openjdk8"
 BRANCH="master"
 
 set -e
 
-if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
+if [ "$GITHUB_REPOSITORY" != "$SLUG" ]; then
   echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
-elif [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
-  echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$TRAVIS_JDK_VERSION'."
-elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "Skipping snapshot deployment: was pull request."
-elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
-  echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
 else
   echo "Deploying snapshot..."
   ./gradlew $1
