3f394544ad52ba2aa738a740aa0eac760b3845c3
Tarek Belkahia <tarek.belkahia@gmail.com>
2020-01-31 12:38:00 +0000

    Switch sample to js plugin instead of mpp

--- sample/web/webpack.config.d/resources.js
+++ sample/web/webpack.config.d/resources.js
@@ -1,7 +1,7 @@
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 config.plugins.push(
     new CopyWebpackPlugin([
-        { from: '../../../../sample/web/src/jsMain/resources',
+        { from: '../../../../sample/web/src/main/resources',
             to: '../../../../sample/web/build/distributions' }
     ])
 );
