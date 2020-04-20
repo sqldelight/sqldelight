4d69185c15d260435dd28c5a003c99694594bcd9
Zac Sweers <zsweers@slack-corp.com>
2020-04-20 17:01:58 -0400

    Copy rx2 sources

---
+++ extensions/rxjava3-extensions/src/test/kotlin/com/squareup/sqldelight/runtime/rx/QueryObservableTest.kt
@@ -0,0 +1,39 @@
+package com.squareup.sqldelight.runtime.rx
+
+import com.squareup.sqldelight.Query
+import com.squareup.sqldelight.db.SqlPreparedStatement
+import com.squareup.sqldelight.db.SqlCursor
+import com.squareup.sqldelight.internal.copyOnWriteList
+import com.squareup.sqldelight.runtime.rx.Employee.Companion.SELECT_EMPLOYEES
+import com.squareup.sqldelight.runtime.rx.TestDb.Companion.TABLE_EMPLOYEE
+import io.reactivex.schedulers.Schedulers
+import org.junit.Test
+
+class QueryObservableTest {
+  @Test fun mapToListThrowsFromQueryRun() {
+    val error = IllegalStateException("test exception")
+
+    val query = object : Query<Any>(copyOnWriteList(), { throw AssertionError("Must not be called") }) {
+      override fun execute() = throw error
+    }
+
+    query.asObservable(Schedulers.trampoline()).mapToList()
+        .test()
+        .assertNoValues()
+        .assertError(error)
+  }
+
+  @Test fun mapToListThrowsFromMapFunction() {
+    val db = TestDb()
+    val error = IllegalStateException("test exception")
+
+    db.createQuery(TABLE_EMPLOYEE, SELECT_EMPLOYEES, { throw error })
+        .asObservable(Schedulers.trampoline())
+        .mapToList()
+        .test()
+        .assertNoValues()
+        .assertError(error)
+
+    db.close()
+  }
+}
---
+++ extensions/rxjava3-extensions/src/test/kotlin/com/squareup/sqldelight/runtime/rx/QueryTest.kt
@@ -0,0 +1,121 @@
+package com.squareup.sqldelight.runtime.rx
+
+import com.squareup.sqldelight.runtime.rx.Employee.Companion.MAPPER
+import com.squareup.sqldelight.runtime.rx.Employee.Companion.SELECT_EMPLOYEES
+import com.squareup.sqldelight.runtime.rx.TestDb.Companion.TABLE_EMPLOYEE
+import io.reactivex.schedulers.Schedulers
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.util.Optional
+
+class QueryTest {
+  private val db = TestDb()
+
+  @After fun tearDown() {
+    db.close()
+  }
+
+  @Test fun mapToOne() {
+    db.createQuery(TABLE_EMPLOYEE, "$SELECT_EMPLOYEES LIMIT 1", MAPPER)
+        .asObservable(Schedulers.trampoline())
+        .mapToOne()
+        .test()
+        .assertValue(Employee("alice", "Alice Allison"))
+  }
+
+  @Test fun `mapToOne throws on multiple rows`() {
+    db.createQuery(TABLE_EMPLOYEE, SELECT_EMPLOYEES + " LIMIT 2", MAPPER)
+        .asObservable(Schedulers.trampoline())
+        .mapToOne()
+        .test()
+        .assertError { it.message!!.contains("ResultSet returned more than 1 row") }
+  }
+
+  @Test fun mapToOneOrDefault() {
+    db.createQuery(TABLE_EMPLOYEE, SELECT_EMPLOYEES + " LIMIT 1", MAPPER)
+        .asObservable(Schedulers.trampoline())
+        .mapToOneOrDefault(Employee("fred", "Fred Frederson"))
+        .test()
+        .assertValue(Employee("alice", "Alice Allison"))
+  }
+
+  @Test fun `mapToOneOrDefault throws on multiple rows`() {
+    db.createQuery(TABLE_EMPLOYEE, SELECT_EMPLOYEES + " LIMIT 2", MAPPER) //
+        .asObservable(Schedulers.trampoline())
+        .mapToOneOrDefault(Employee("fred", "Fred Frederson"))
+        .test()
+        .assertError { it.message!!.contains("ResultSet returned more than 1 row") }
+  }
+
+  @Test fun `mapToOneOrDefault returns default when no results`() {
+    val defaultEmployee = Employee("fred", "Fred Frederson")
+
+    db.createQuery(TABLE_EMPLOYEE, SELECT_EMPLOYEES + " LIMIT 0", MAPPER) //
+        .asObservable(Schedulers.trampoline())
+        .mapToOneOrDefault(Employee("fred", "Fred Frederson"))
+        .test()
+        .assertValue(defaultEmployee)
+  }
+
+  @Test fun mapToList() {
+    db.createQuery(TABLE_EMPLOYEE, SELECT_EMPLOYEES, MAPPER)
+        .asObservable(Schedulers.trampoline())
+        .mapToList()
+        .test()
+        .assertValue(listOf(
+            Employee("alice", "Alice Allison"), //
+            Employee("bob", "Bob Bobberson"), //
+            Employee("eve", "Eve Evenson")
+        ))
+  }
+
+  @Test fun `mapToList empty when no rows`() {
+    db.createQuery(TABLE_EMPLOYEE, SELECT_EMPLOYEES + " WHERE 1=2", MAPPER)
+        .asObservable(Schedulers.trampoline())
+        .mapToList()
+        .test()
+        .assertValue(emptyList())
+  }
+
+  @Test fun mapToOptional() {
+    db.createQuery(TABLE_EMPLOYEE, SELECT_EMPLOYEES + " LIMIT 1", MAPPER)
+        .asObservable(Schedulers.trampoline())
+        .mapToOptional()
+        .test()
+        .assertValue(Optional.of(Employee("alice", "Alice Allison")))
+  }
+
+  @Test fun `mapToOptional throws on multiple rows`() {
+    db.createQuery(TABLE_EMPLOYEE, SELECT_EMPLOYEES + " LIMIT 2", MAPPER) //
+        .asObservable(Schedulers.trampoline())
+        .mapToOptional()
+        .test()
+        .assertError { it.message!!.contains("ResultSet returned more than 1 row") }
+  }
+
+  @Test fun `mapToOptional empty when no results`() {
+    db.createQuery(TABLE_EMPLOYEE, SELECT_EMPLOYEES + " LIMIT 0", MAPPER) //
+        .asObservable(Schedulers.trampoline())
+        .mapToOptional()
+        .test()
+        .assertValue(Optional.empty())
+  }
+
+  @Test fun mapToOneNonNull() {
+    db.createQuery(TABLE_EMPLOYEE, SELECT_EMPLOYEES + " LIMIT 1", MAPPER)
+        .asObservable(Schedulers.trampoline())
+        .mapToOneNonNull()
+        .test()
+        .assertValue(Employee("alice", "Alice Allison"))
+  }
+
+  @Test fun `mapToOneNonNull doesnt emit for no results`() {
+    db.createQuery(TABLE_EMPLOYEE, SELECT_EMPLOYEES + " LIMIT 0", MAPPER)
+        .asObservable(Schedulers.trampoline())
+        .take(1) // Ensure we have an event (complete) that the test observer can validate.
+        .mapToOneNonNull()
+        .test()
+        .assertComplete()
+  }
+}
