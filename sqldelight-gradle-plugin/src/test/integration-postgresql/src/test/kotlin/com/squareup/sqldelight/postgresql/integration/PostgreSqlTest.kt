9c955e95d6166a8a7fb4df9c07142ec09d3c4cc9
Veyndan John Stuart <veyndan@gmail.com>
2020-05-18 22:44:12 +0200

    Remove unnecessary instantiation of test container

--- sqldelight-gradle-plugin/src/test/integration-postgresql/src/test/kotlin/com/squareup/sqldelight/postgresql/integration/PostgreSqlTest.kt
+++ sqldelight-gradle-plugin/src/test/integration-postgresql/src/test/kotlin/com/squareup/sqldelight/postgresql/integration/PostgreSqlTest.kt
@@ -2,41 +2,27 @@ package com.squareup.sqldelight.postgresql.integration
 
 import com.google.common.truth.Truth.assertThat
 import com.squareup.sqldelight.sqlite.driver.JdbcDriver
-import java.sql.Connection
 import java.sql.DriverManager
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
 class PostgreSqlTest {
+  val conn = DriverManager.getConnection("jdbc:tc:postgresql:9.6.8:///my_db")
+  val driver = object : JdbcDriver() {
+    override fun getConnection() = conn
+  }
+  val database = MyDatabase(driver)
 
-  val connPostgres = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres")
-  lateinit var connMyDb: Connection
-  lateinit var driver: JdbcDriver
-  lateinit var database: MyDatabase
-
-  @Before
-  fun before() {
-    connPostgres.prepareStatement("CREATE DATABASE my_db;").execute()
-
-    connMyDb = DriverManager.getConnection("jdbc:postgresql://localhost:5432/my_db")
-    driver = object : JdbcDriver() {
-      override fun getConnection() = connMyDb
-    }
+  @Before fun before() {
     MyDatabase.Schema.create(driver)
-    database = MyDatabase(driver)
   }
 
-  @After
-  fun after() {
-    connMyDb.close()
-
-    connPostgres.prepareStatement("DROP DATABASE IF EXISTS my_db;").execute()
-    connPostgres.close()
+  @After fun after() {
+    conn.close()
   }
 
-  @Test
-  fun simpleSelect() {
+  @Test fun simpleSelect() {
     database.dogQueries.insertDog("Tilda", "Pomeranian", true)
     assertThat(database.dogQueries.selectDogs().executeAsOne())
       .isEqualTo(Dog.Impl(
