c384a2cb611740c06d962acc586d12d280dc44f1
Jeff Gulbronson <JGulbronson@users.noreply.github.com>
2020-05-31 16:57:40 -0400

    Add integration test for array parameter in MySQL (#1700)
    * Add integration test for array parameter in MySQL
    
    * Clean up Before/After
    
    * Fix test after rebase

--- sqldelight-gradle-plugin/src/test/integration-mysql/src/test/kotlin/com/squareup/sqldelight/mysql/integration/MySqlTest.kt
+++ sqldelight-gradle-plugin/src/test/integration-mysql/src/test/kotlin/com/squareup/sqldelight/mysql/integration/MySqlTest.kt
@@ -2,28 +2,64 @@ package com.squareup.sqldelight.mysql.integration
 
 import com.google.common.truth.Truth.assertThat
 import com.squareup.sqldelight.sqlite.driver.JdbcDriver
+import java.sql.Connection
 import java.sql.DriverManager
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
 class MySqlTest {
-    val conn = DriverManager.getConnection("jdbc:tc:mysql:///myDb")
-    val driver = object : JdbcDriver() {
-        override fun getConnection() = conn
-    }
-    val database = MyDatabase(driver)
+    lateinit var connection: Connection
+    lateinit var dogQueries: DogQueries
 
-    @Before fun before() {
+    @Before
+    fun before() {
+        connection = DriverManager.getConnection("jdbc:tc:mysql:///myDb")
+        val driver = object : JdbcDriver() {
+            override fun getConnection() = connection
+        }
+        val database = MyDatabase(driver)
+
         MyDatabase.Schema.create(driver)
+        dogQueries = database.dogQueries
+    }
+
+    @After
+    fun after() {
+        connection.close()
     }
 
     @Test fun simpleSelect() {
-        database.dogQueries.insertDog("Tilda", "Pomeranian", true)
-        assertThat(database.dogQueries.selectDogs().executeAsOne())
+        dogQueries.insertDog("Tilda", "Pomeranian", true)
+        assertThat(dogQueries.selectDogs().executeAsOne())
             .isEqualTo(Dog(
                 name = "Tilda",
                 breed = "Pomeranian",
                 is_good = true
             ))
+    }
+
+    @Test
+    fun simpleSelectWithIn() {
+        dogQueries.insertDog("Tilda", "Pomeranian", true)
+        dogQueries.insertDog("Tucker", "Portuguese Water Dog", true)
+        dogQueries.insertDog("Cujo", "Pomeranian", false)
+        dogQueries.insertDog("Buddy", "Pomeranian", true)
+        assertThat(dogQueries.selectDogsByBreedAndNames(
+            breed = "Pomeranian",
+            name = listOf("Tilda", "Buddy")
+        ).executeAsList())
+            .containsExactly(
+                Dog(
+                    name = "Tilda",
+                    breed = "Pomeranian",
+                    is_good = true
+                ),
+                Dog(
+                    name = "Buddy",
+                    breed = "Pomeranian",
+                    is_good = true
+                )
+            )
     }
 }
