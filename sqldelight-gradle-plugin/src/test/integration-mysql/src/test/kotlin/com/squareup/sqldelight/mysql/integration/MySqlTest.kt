3b81383c39068458d53a26f692ab4737c606bd5f
Jeff Gulbronson <jgulbronson@gmail.com>
2020-05-31 15:24:23 -0400

    Add integration test for array parameter in MySQL

--- sqldelight-gradle-plugin/src/test/integration-mysql/src/test/kotlin/com/squareup/sqldelight/mysql/integration/MySqlTest.kt
+++ sqldelight-gradle-plugin/src/test/integration-mysql/src/test/kotlin/com/squareup/sqldelight/mysql/integration/MySqlTest.kt
@@ -3,18 +3,14 @@ package com.squareup.sqldelight.mysql.integration
 import com.google.common.truth.Truth.assertThat
 import com.squareup.sqldelight.sqlite.driver.JdbcDriver
 import java.sql.DriverManager
+import org.junit.AfterClass
 import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
 
 class MySqlTest {
-    val conn = DriverManager.getConnection("jdbc:tc:mysql:///myDb")
-    val driver = object : JdbcDriver() {
-        override fun getConnection() = conn
-    }
-    val database = MyDatabase(driver)
-
     @Before fun before() {
-        MyDatabase.Schema.create(driver)
+        dogQueries.deleteDogs()
     }
 
     @Test fun simpleSelect() {
@@ -25,5 +21,46 @@ class MySqlTest {
                 breed = "Pomeranian",
                 is_good = true
             ))
+    }
+
+    @Test fun simpleSelectWithIn() {
+        dogQueries.insertDog("Tilda", "Pomeranian", true)
+        dogQueries.insertDog("Tucker", "Portuguese Water Dog", true)
+        dogQueries.insertDog("Cujo", "Pomeranian", false)
+        dogQueries.insertDog("Buddy", "Pomeranian", true)
+        assertThat(dogQueries.selectDogsByBreedAndNames(
+            breed = "Pomeranian",
+            name = listOf("Tilda", "Buddy")
+        ).executeAsList())
+            .containsExactly(
+                Dog(
+                    name = "Tilda",
+                    breed = "Pomeranian",
+                    is_good = true
+                ),
+                Dog(
+                    name = "Buddy",
+                    breed = "Pomeranian",
+                    is_good = true
+                )
+            )
+    }
+
+    companion object {
+        lateinit var dogQueries: DogQueries
+        val conn = DriverManager.getConnection("jdbc:tc:mysql:///myDb")
+        val driver = object : JdbcDriver() {
+            override fun getConnection() = conn
+        }
+        val database = MyDatabase(driver)
+
+        @BeforeClass @JvmStatic fun beforeClass() {
+            MyDatabase.Schema.create(driver)
+            dogQueries = database.dogQueries
+        }
+
+        @AfterClass @JvmStatic fun afterClass() {
+            conn.close()
+        }
     }
 }
