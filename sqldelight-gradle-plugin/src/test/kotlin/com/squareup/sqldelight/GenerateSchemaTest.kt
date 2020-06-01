9b7e607130bd2bc523afb017e24263c93b9b773b
Alec Strong <astrong@squareup.com>
2020-05-31 23:29:38 -0400

    If the output db file already exists when generating a schema, delete it

--- sqldelight-gradle-plugin/src/test/kotlin/com/squareup/sqldelight/GenerateSchemaTest.kt
+++ sqldelight-gradle-plugin/src/test/kotlin/com/squareup/sqldelight/GenerateSchemaTest.kt
@@ -24,6 +24,34 @@ class GenerateSchemaTest {
     schemaFile.delete()
   }
 
+  @Test fun `generateSchema task can run twice`() {
+    val fixtureRoot = File("src/test/schema-file")
+    val schemaFile = File(fixtureRoot, "src/main/sqldelight/databases/1.db")
+    if (schemaFile.exists()) schemaFile.delete()
+
+    GradleRunner.create()
+        .withProjectDir(fixtureRoot)
+        .withPluginClasspath()
+        .withArguments("clean", "generateMainDatabaseSchema", "--stacktrace")
+        .build()
+
+    // verify
+    assertThat(schemaFile.exists())
+        .isTrue()
+
+    GradleRunner.create()
+        .withProjectDir(fixtureRoot)
+        .withPluginClasspath()
+        .withArguments("clean", "--rerun-tasks", "generateMainDatabaseSchema", "--stacktrace")
+        .build()
+
+    // verify
+    assertThat(schemaFile.exists())
+        .isTrue()
+
+    schemaFile.delete()
+  }
+
   @Test fun `schema file generates correctly with existing sqm files`() {
     val fixtureRoot = File("src/test/schema-file-sqm")
 
