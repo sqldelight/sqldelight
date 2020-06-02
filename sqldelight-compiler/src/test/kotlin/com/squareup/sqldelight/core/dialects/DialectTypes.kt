4d1e4aee6645cddb8163e517187d096d1cbfabe2
Alec Strong <AlecStrong@users.noreply.github.com>
2020-06-02 12:30:42 -0400

    Use burst for dialect testing (#1721)

---
+++ sqldelight-compiler/src/test/kotlin/com/squareup/sqldelight/core/dialects/DialectTypes.kt
@@ -0,0 +1,14 @@
+package com.squareup.sqldelight.core.dialects
+
+import com.alecstrong.sql.psi.core.DialectPreset
+import com.alecstrong.sql.psi.core.DialectPreset.HSQL
+import com.alecstrong.sql.psi.core.DialectPreset.MYSQL
+import com.alecstrong.sql.psi.core.DialectPreset.POSTGRESQL
+import com.alecstrong.sql.psi.core.DialectPreset.SQLITE_3_18
+import com.alecstrong.sql.psi.core.DialectPreset.SQLITE_3_24
+
+internal val DialectPreset.textType
+  get() = when (this) {
+    MYSQL, SQLITE_3_24, SQLITE_3_18, POSTGRESQL -> "TEXT"
+    HSQL -> "VARCHAR(8)"
+  }
