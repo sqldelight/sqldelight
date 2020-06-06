4fd1f7e9642c4a82035adc065184b4891ff62837
Jeff Gulbronson <jgulbronson@gmail.com>
2020-06-06 15:50:17 -0400

    Support MONTH and YEAR functions in MySQL

--- sqldelight-compiler/src/main/kotlin/com/squareup/sqldelight/core/lang/util/ExprUtil.kt
+++ sqldelight-compiler/src/main/kotlin/com/squareup/sqldelight/core/lang/util/ExprUtil.kt
@@ -200,6 +200,8 @@ private fun SqlFunctionExpr.functionType() = when (functionName.text.toLowerCase
 private fun SqlFunctionExpr.mySqlFunctionType() = when (functionName.text.toLowerCase()) {
   "greatest" -> encapsulatingType(exprList, INTEGER, REAL, TEXT, BLOB)
   "concat" -> encapsulatingType(exprList, TEXT)
+  "month" -> IntermediateType(INTEGER)
+  "year" -> IntermediateType(INTEGER)
   else -> throw AssertionError("Unknown function for MySQL: ${functionName.text}")
 }
 
