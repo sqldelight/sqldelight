1b535e94879b0264c82da15c9b14176ca68b1d2d
Alec Strong <astrong@squareup.com>
2020-09-02 08:10:41 -0400

    Ensure we only have one daemon at a time for a project

--- sqldelight-idea-plugin/src/main/kotlin/com/squareup/sqldelight/intellij/ProjectService.kt
+++ sqldelight-idea-plugin/src/main/kotlin/com/squareup/sqldelight/intellij/ProjectService.kt
@@ -18,6 +18,7 @@ package com.squareup.sqldelight.intellij
 import com.alecstrong.sql.psi.core.DialectPreset
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
@@ -30,17 +31,30 @@ import com.squareup.sqldelight.core.SqlDelightFileIndex
 import com.squareup.sqldelight.core.SqlDelightProjectService
 import com.squareup.sqldelight.core.SqlDelightPropertiesFile
 import com.squareup.sqldelight.core.lang.SqlDelightFileType
-import java.io.File
 import org.gradle.tooling.GradleConnector
+import org.gradle.tooling.internal.consumer.DefaultGradleConnector
+import timber.log.Timber
+import java.io.File
+import java.util.concurrent.TimeUnit
 
 class ProjectService(val project: Project) : SqlDelightProjectService, Disposable {
   private val connector: GradleConnector by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED) {
-    GradleConnector.newConnector()
+    Timber.i("Created a new gradle connection for $project")
+    val connector = GradleConnector.newConnector()
+    if (connector is DefaultGradleConnector) {
+      connector.daemonMaxIdleTime(5, TimeUnit.SECONDS)
+    }
+    return@lazy connector
   }
 
   private val fileIndexes = LinkedHashMap<Module, SqlDelightFileIndex>()
 
+  init {
+    Timber.plant(LoggerTree(Logger.getInstance("SQLDelight[${project.name}]")))
+  }
+
   override fun dispose() {
+    Timber.i("Disconnecting from connection on $project")
     connector.disconnect()
   }
 
@@ -68,26 +82,36 @@ class ProjectService(val project: Project) : SqlDelightProjectService, Disposabl
     return ProjectRootManager.getInstance(project).fileIndex.getModuleForFile(vFile)
   }
 
-  override fun fileIndex(module: Module): SqlDelightFileIndex {
+  override fun fileIndex(module: Module): SqlDelightFileIndex = synchronized(module) {
     val index = fileIndexes[module]
     if (index != null) return index
 
-    val projectConnection = connector
-        .forProjectDirectory(File(ExternalSystemApiUtil.getExternalProjectPath(module) ?: return defaultIndex))
-        .connect()
+    return ApplicationManager.getApplication().runReadAction<SqlDelightFileIndex> {
+      Timber.i("Finding project path for $module")
+      val projectPath = ExternalSystemApiUtil.getExternalProjectPath(module)
+      Timber.i("Starting new project connection for $projectPath")
+      val projectConnection = connector
+          .forProjectDirectory(File(projectPath ?: return@runReadAction defaultIndex))
+          .connect()
+      Timber.i("Finding SQLDelight model for $projectPath")
 
-    val properties = try {
-      projectConnection.getModel(SqlDelightPropertiesFile::class.java)!!
-    } catch (e: Throwable) {
-      return defaultIndex.also { fileIndexes[module] = it }
-    } finally {
-      projectConnection.close()
-    }
+      val properties = try {
+        projectConnection.getModel(SqlDelightPropertiesFile::class.java)!!.also {
+          Timber.i("Found SQLDelight model for $projectPath")
+        }
+      } catch (e: Throwable) {
+        Timber.i(e, "Got error while finding SQLDelight model")
+        return@runReadAction defaultIndex.also { fileIndexes[module] = it }
+      } finally {
+        projectConnection.close()
+      }
 
-    dialectPreset = properties.databases.first().dialectPreset
-    return FileIndex(properties.databases.first()).also {
-      Disposer.register(module, Disposable { fileIndexes.remove(module) })
-      fileIndexes[module] = it
+      dialectPreset = properties.databases.first().dialectPreset
+      return@runReadAction FileIndex(properties.databases.first()).also {
+        Disposer.register(module, Disposable { fileIndexes.remove(module) })
+        Timber.i("Setting the file index for $module")
+        fileIndexes[module] = it
+      }
     }
   }
 
