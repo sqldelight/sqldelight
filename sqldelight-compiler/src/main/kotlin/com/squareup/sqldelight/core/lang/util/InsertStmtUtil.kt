39ef24a527bc600f5bb07e6aea296157eb3c83af
Alec Strong <AlecStrong@users.noreply.github.com>
2020-09-21 09:54:26 -0400

    Pending branch to build against latest sql-psi master (#1955)

--- sqldelight-compiler/src/main/kotlin/com/squareup/sqldelight/core/lang/util/InsertStmtUtil.kt
+++ sqldelight-compiler/src/main/kotlin/com/squareup/sqldelight/core/lang/util/InsertStmtUtil.kt
@@ -1,17 +1,17 @@
 package com.squareup.sqldelight.core.lang.util
 
 import com.alecstrong.sql.psi.core.psi.LazyQuery
+import com.alecstrong.sql.psi.core.psi.SqlColumnDef
 import com.alecstrong.sql.psi.core.psi.SqlColumnName
 import com.alecstrong.sql.psi.core.psi.SqlInsertStmt
-import com.squareup.sqldelight.core.psi.SqlDelightColumnDef
 
 /**
  * The list of columns that values are being provided for.
  */
-internal val SqlInsertStmt.columns: List<SqlDelightColumnDef>
+internal val SqlInsertStmt.columns: List<SqlColumnDef>
   get() {
     val columns = table.query.columns
-        .mapNotNull { (it.element as? SqlColumnName)?.parent as? SqlDelightColumnDef }
+        .mapNotNull { (it.element as? SqlColumnName)?.parent as? SqlColumnDef }
     if (columnNameList.isEmpty()) return columns
 
     val columnMap = linkedMapOf(*columns.map { it.columnName.name to it }.toTypedArray())
