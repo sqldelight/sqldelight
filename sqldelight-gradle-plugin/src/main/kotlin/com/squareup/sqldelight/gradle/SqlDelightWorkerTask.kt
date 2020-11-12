68bb1b0509fff78e2ec5d7461f295df9ced85d67
Ben Asher <ben.asher@autodesk.com>
2020-11-12 08:06:03 -0800

    Allow opt-out of classLoaderIsolation (#2082)
    * Allow opt-out of classLoaderIsolation
    
    * fix validate plugins

---
+++ sqldelight-gradle-plugin/src/main/kotlin/com/squareup/sqldelight/gradle/SqlDelightWorkerTask.kt
@@ -0,0 +1,25 @@
+package com.squareup.sqldelight.gradle
+
+import org.gradle.workers.WorkQueue
+import org.gradle.workers.WorkerExecutor
+
+/**
+ * Common API for interacting with gradle workers
+ * in tasks
+ */
+interface SqlDelightWorkerTask {
+
+  val workerExecutor: WorkerExecutor
+
+  /**
+   * If true, [WorkerExecutor.classLoaderIsolation] is used. Otherwise,
+   * [WorkerExecutor.noIsolation] is used for running the task's action
+   */
+  var useClassLoaderIsolation: Boolean
+
+  fun workQueue(): WorkQueue = if (useClassLoaderIsolation) {
+      workerExecutor.classLoaderIsolation()
+    } else {
+      workerExecutor.noIsolation()
+    }
+}
