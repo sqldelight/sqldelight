ecba593961372e27543875378ae67e05abc9e0d9
Alexander Perfilyev <alexander.perfilyev@gmail.com>
2021-04-08 22:53:44 +0300

    Add import quick fix for unknown type
    closes #683

--- sqldelight-idea-plugin/src/main/kotlin/com/squareup/sqldelight/intellij/intentions/Intentions.kt
+++ sqldelight-idea-plugin/src/main/kotlin/com/squareup/sqldelight/intellij/intentions/Intentions.kt
@@ -2,4 +2,6 @@ package com.squareup.sqldelight.intellij.intentions
 
 internal const val INTENTIONS_FAMILY_NAME_REFACTORINGS = "Refactorings"
 
+internal const val INTENTIONS_FAMILY_NAME_IMPORTS = "Imports"
+
 internal const val INTENTION_EXPAND_COLUMN_NAMES_TEXT = "Expand * into column names"
