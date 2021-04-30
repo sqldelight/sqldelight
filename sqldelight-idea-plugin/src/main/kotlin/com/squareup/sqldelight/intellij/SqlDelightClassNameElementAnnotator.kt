843f49f9a6c0f5c96beae7d227af9640108a8c74
Alexander Perfilyev <alexander.perfilyev@gmail.com>
2021-04-30 16:25:21 +0300

    Add nested class support to import inspection and class annotator (#2362)

--- sqldelight-idea-plugin/src/main/kotlin/com/squareup/sqldelight/intellij/SqlDelightClassNameElementAnnotator.kt
+++ sqldelight-idea-plugin/src/main/kotlin/com/squareup/sqldelight/intellij/SqlDelightClassNameElementAnnotator.kt
@@ -19,11 +19,16 @@ import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.module.ModuleUtil
+import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.search.PsiShortNamesCache
+import com.squareup.sqldelight.core.lang.SqlDelightFile
+import com.squareup.sqldelight.core.lang.psi.ImportStmtMixin
 import com.squareup.sqldelight.core.lang.psi.JavaTypeMixin
+import com.squareup.sqldelight.core.lang.util.findChildrenOfType
 import com.squareup.sqldelight.intellij.intentions.AddImportIntention
+import com.squareup.sqldelight.intellij.util.PsiClassSearchHelper
 
 class SqlDelightClassNameElementAnnotator : Annotator {
   override fun annotate(element: PsiElement, holder: AnnotationHolder) {
@@ -31,16 +36,58 @@ class SqlDelightClassNameElementAnnotator : Annotator {
       return
     }
 
-    holder.newAnnotation(HighlightSeverity.ERROR, "Unresolved reference: ${element.text}")
-      .range(element)
+    val project = element.project
+    val sqlDelightFile = element.containingFile as SqlDelightFile
+    val module = ModuleUtil.findModuleForFile(sqlDelightFile) ?: return
+    val scope = GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(module, false)
+    val outerClassElement = element.firstChild
+    val outerClassName = outerClassElement.text
+    val classes = PsiClassSearchHelper.getClassesByShortName(outerClassName, project, scope)
+
+    val hasImport = sqlDelightFile.hasImport(outerClassName)
+    val psiElement = if (hasImport) {
+      missingNestedClass(classes, element)
+    } else {
+      outerClassElement
+    }
+
+    val needsQuickFix = classes.isNotEmpty() && !hasImport
+
+    holder.newAnnotation(HighlightSeverity.ERROR, "Unresolved reference: ${psiElement.text}")
+      .range(psiElement)
       .highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
       .apply {
-        val classes = PsiShortNamesCache.getInstance(element.project)
-          .getClassesByName(element.text, GlobalSearchScope.allScope(element.project))
-        if (classes.isNotEmpty()) {
-          withFix(AddImportIntention(element.text))
+        if (needsQuickFix) {
+          withFix(AddImportIntention(outerClassName))
         }
       }
       .create()
+  }
+
+  private fun SqlDelightFile.hasImport(outerClassName: String): Boolean {
+    return sqlStmtList?.findChildrenOfType<ImportStmtMixin>()
+      .orEmpty()
+      .any { it.javaType.text.endsWith(outerClassName) }
+  }
+
+  private fun missingNestedClass(classes: List<PsiClass>, javaTypeMixin: JavaTypeMixin): PsiElement {
+    val elementText = javaTypeMixin.text
+    val className = classes.map { clazz -> findMissingNestedClassName(clazz, elementText) }
+      .maxByOrNull { it.length }
+      ?.substringBefore(".") ?: return javaTypeMixin.firstChild
+    return javaTypeMixin.findChildrenOfType<PsiElement>().first { it.textMatches(className) }
+  }
+
+  private fun findMissingNestedClassName(psiClass: PsiClass, className: String): String {
+    val nestedClassName = className.substringBefore(".")
+    if (psiClass.name != nestedClassName) {
+      return nestedClassName
+    }
+    val nextName = className.removePrefix(nestedClassName).removePrefix(".")
+    val lookupString = nextName.substringBefore(".")
+    val nextClass = psiClass.innerClasses.firstOrNull { clazz ->
+      clazz.textMatches(lookupString)
+    }
+    return nextClass?.let { findMissingNestedClassName(it, nextName) } ?: nextName
   }
 }
