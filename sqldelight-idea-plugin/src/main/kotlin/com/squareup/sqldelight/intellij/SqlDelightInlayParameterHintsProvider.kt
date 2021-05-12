8fd835db6d6d5da48a8c2025e75db24bded71e21
Alexander Perfilyev <alexander.perfilyev@gmail.com>
2021-05-12 13:35:47 +0300

    Revert "Show parameter hints from insert-stmt" (#2395)
    This reverts commit a5aab3e8

--- sqldelight-idea-plugin/src/main/kotlin/com/squareup/sqldelight/intellij/SqlDelightInlayParameterHintsProvider.kt
+++ sqldelight-idea-plugin/src/main/kotlin/com/squareup/sqldelight/intellij/SqlDelightInlayParameterHintsProvider.kt
@@ -1,13 +1,12 @@
 package com.squareup.sqldelight.intellij
 
-import com.alecstrong.sql.psi.core.psi.SqlInsertStmt
+import com.alecstrong.sql.psi.core.psi.QueryElement
 import com.alecstrong.sql.psi.core.psi.SqlInsertStmtValues
 import com.alecstrong.sql.psi.core.psi.SqlValuesExpression
 import com.intellij.codeInsight.hints.InlayInfo
 import com.intellij.codeInsight.hints.InlayParameterHintsProvider
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.parentOfType
-import com.intellij.refactoring.suggested.startOffset
+import org.jetbrains.kotlin.psi.psiUtil.startOffset
 
 class SqlDelightInlayParameterHintsProvider : InlayParameterHintsProvider {
 
@@ -15,9 +14,11 @@ class SqlDelightInlayParameterHintsProvider : InlayParameterHintsProvider {
     if (element !is SqlValuesExpression || element.parent !is SqlInsertStmtValues) {
       return emptyList()
     }
-    return element.parentOfType<SqlInsertStmt>()?.columnNameList.orEmpty()
+    return runCatching { element.queryAvailable(element) }
+      .getOrDefault(emptyList())
+      .flatMap(QueryElement.QueryResult::columns)
       .zip(element.exprList) { col, expr ->
-        InlayInfo(col.name, expr.startOffset)
+        InlayInfo(col.element.text, expr.startOffset)
       }
   }
 
