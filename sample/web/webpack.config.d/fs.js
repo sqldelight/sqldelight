57c4cf36e5f4a318a30d3ad34c122022f9986051
Kevin Galligan <kgalligan@gmail.com>
2021-05-21 14:41:48 -0400

    Kotlin 1.5 build (#2410)
    * Kotlin 1.5 build
    
    * Sample without web for now
    
    * Maybe fix JS sample

--- sample/web/webpack.config.d/fs.js
+++ sample/web/webpack.config.d/fs.js
@@ -1,5 +1,8 @@
 // Remove fs in the webpack config in order to build for front end
 // https://github.com/webpack-contrib/css-loader/issues/447
-config.node = {
-    fs: 'empty'
+config.resolve = {
+    fallback: {
+        fs: false,
+        path: false
+    }
 };
