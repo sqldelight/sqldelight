a4373c2b3253d99f26a5931f03dbbe9ba954f23e
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-08-26 02:35:21 +0200

    Add linuxX64 support (#2456)
    * Add linux support
    
    * Add missing platform specific files and fix config of linux target
    
    * Fix thread vars for linux
    
    * Configure compilation depending on host. Based on: https://youtrack.jetbrains.com/issue/KT-30498
    
    * Update workflows
    
    * Add comment
    
    * Bump sqliter version to `1.0.5` and change cross compilation config back to match sqliter config
    
    * Remove mavenLocal repository
    
    * Change ubuntu version from `ubuntu-latest` to `ubuntu-18.04` for steps `Run gradle plugin tests` and `Run gradle plugin integrations`
    
    * Bump sqliter version to `1.0.6`
    
    * Replace all usages of `ubuntu-latest` with `ubuntu-18.04` and add a comment why this is needed
    
    * Explicitly setup jdk 11 instead of vm defaults

---
+++ drivers/native-driver/src/linuxMain/kotlin/com/squareup/sqldelight/drivers/native/util/Stately.kt
@@ -0,0 +1,21 @@
+package com.squareup.sqldelight.drivers.native.util
+
+import co.touchlab.stately.collections.SharedHashMap
+import co.touchlab.stately.collections.SharedLinkedList
+
+/**
+ * This should probably be in Stately
+ */
+internal fun <T> SharedLinkedList<T>.cleanUp(block: (T) -> Unit) {
+  val extractList = ArrayList<T>(size)
+  extractList.addAll(this)
+  this.clear()
+  extractList.forEach(block)
+}
+
+internal fun <K, V> SharedHashMap<K, V>.cleanUp(block: (Map.Entry<K, V>) -> Unit) {
+  val extractMap = HashMap<K, V>(this.size)
+  extractMap.putAll(this)
+  this.clear()
+  extractMap.forEach(block)
+}
