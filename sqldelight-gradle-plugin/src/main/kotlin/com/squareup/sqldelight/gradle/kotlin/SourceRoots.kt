1bb06d37ca598c5b2db5d64e6f99b3834f489002
Martin Bonnin <martin@mbonnin.net>
2021-09-24 18:14:02 +0200

    Fix IDE autocomplete for generated files for Android (#2573)
    * Call variant.addJavaSourceFoldersToModel for Android Studio to pickup
    the generated directory
    
    * run ./gradlew spotlessApply

--- sqldelight-gradle-plugin/src/main/kotlin/com/squareup/sqldelight/gradle/kotlin/SourceRoots.kt
+++ sqldelight-gradle-plugin/src/main/kotlin/com/squareup/sqldelight/gradle/kotlin/SourceRoots.kt
@@ -9,6 +9,7 @@ import org.gradle.api.DomainObjectSet
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.file.SourceDirectorySet
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.api.tasks.TaskContainer
 import org.gradle.api.tasks.TaskProvider
@@ -17,6 +18,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinJvmAndroidCompilation
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinMetadataTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import java.io.File
 
 /**
  * @return A list of source roots and their dependencies.
@@ -102,6 +104,9 @@ private fun BaseExtension.sources(project: Project): List<Source> {
       sourceDirectorySet = sourceSets[variant.name]
         ?: throw IllegalStateException("Couldn't find ${variant.name} in $sourceSets"),
       sourceSets = variant.sourceSets.map { it.name },
+      registerGeneratedDirectory = { outputDirectoryProvider ->
+        variant.addJavaSourceFoldersToModel(outputDirectoryProvider.get())
+      }
     )
   }
 }
@@ -123,6 +128,7 @@ internal data class Source(
   val name: String,
   val variantName: String? = null,
   val sourceSets: List<String>,
+  val registerGeneratedDirectory: ((Provider<File>) -> Unit)? = null
 ) {
   fun closestMatch(sources: Collection<Source>): Source? {
     var matches = sources.filter {
