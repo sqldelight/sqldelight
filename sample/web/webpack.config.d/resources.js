bb6f24e2bf6b980bfa83a42b8e43df64a6cbe96f
Derek <derek@llamabagel.ca>
2021-12-21 11:59:56 -0500

    Remove sql.js dependency from sqljs-driver
    Improve documentation on using copy-webpack-plugin
    Use devNpm for copy-webpack-plugin
    Remove obsolete resources.js config file
    Update sample README

--- sample/web/webpack.config.d/resources.js
+++
@@ -1,7 +0,0 @@
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-config.plugins.push(
-    new CopyWebpackPlugin([
-        { from: '../../../../sample/web/src/main/resources',
-            to: '../../../../sample/web/build/distributions' }
-    ])
-);
