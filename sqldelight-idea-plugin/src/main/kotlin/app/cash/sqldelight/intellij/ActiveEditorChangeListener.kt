4702fd8c9c65039953333a4a92bfcebfcc6ba96a
Alec Strong <astrong@squareup.com>
2022-03-08 21:15:42 -0500

    Add back the file view provider factory

--- sqldelight-idea-plugin/src/main/kotlin/app/cash/sqldelight/intellij/ActiveEditorChangeListener.kt
+++ sqldelight-idea-plugin/src/main/kotlin/app/cash/sqldelight/intellij/ActiveEditorChangeListener.kt
@@ -5,14 +5,15 @@ import com.intellij.openapi.fileEditor.FileEditorManagerEvent
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiManager
 
 internal class ActiveEditorChangeListener : FileEditorManagerListener {
 
   override fun selectionChanged(event: FileEditorManagerEvent) {
     val editor = (event.oldEditor as TextEditor?)?.editor ?: return
     val project = editor.project ?: return
-    val file = PsiDocumentManager.getInstance(project).getPsiFile(editor.document) as? SqlDelightFile? ?: return
+    val file = (PsiDocumentManager.getInstance(project).getPsiFile(editor.document) as? SqlDelightFile?)?.virtualFile ?: return
 
-    FileGeneratorService.getInstance(project).generateFiles(file)
+    PsiManager.getInstance(project).findViewProvider(file)?.contentsSynchronized()
   }
 }
--- sqldelight-idea-plugin/src/main/kotlin/app/cash/sqldelight/intellij/lang/SqlDelightFileViewProviderFactory.kt
+++ sqldelight-idea-plugin/src/main/kotlin/app/cash/sqldelight/intellij/lang/SqlDelightFileViewProviderFactory.kt
@@ -16,14 +16,31 @@
 
 package app.cash.sqldelight.intellij.lang
 
+import app.cash.sqldelight.core.SqlDelightFileIndex
+import app.cash.sqldelight.core.SqlDelightProjectService
+import app.cash.sqldelight.core.compiler.SqlDelightCompiler
+import app.cash.sqldelight.core.lang.MigrationFile
 import app.cash.sqldelight.core.lang.SqlDelightFile
-import app.cash.sqldelight.intellij.FileGeneratorService
+import app.cash.sqldelight.core.lang.SqlDelightQueriesFile
+import app.cash.sqldelight.intellij.util.GeneratedVirtualFile
+import com.alecstrong.sql.psi.core.SqlAnnotationHolder
+import com.alecstrong.sql.psi.core.psi.SqlAnnotatedElement
 import com.intellij.lang.Language
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.util.Condition
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.FileViewProvider
 import com.intellij.psi.FileViewProviderFactory
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiErrorElement
 import com.intellij.psi.PsiManager
 import com.intellij.psi.SingleRootFileViewProvider
+import com.intellij.psi.util.PsiTreeUtil
+import java.io.PrintStream
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicBoolean
 
 class SqlDelightFileViewProviderFactory : FileViewProviderFactory {
   override fun createFileViewProvider(
@@ -32,7 +49,9 @@ class SqlDelightFileViewProviderFactory : FileViewProviderFactory {
     manager: PsiManager,
     eventSystemEnabled: Boolean
   ): FileViewProvider {
-    return SqlDelightFileViewProvider(manager, file, eventSystemEnabled, language)
+    val module = SqlDelightProjectService.getInstance(manager.project).module(file)
+      ?: return SingleRootFileViewProvider(manager, file, eventSystemEnabled)
+    return SqlDelightFileViewProvider(manager, file, eventSystemEnabled, language, module)
   }
 }
 
@@ -40,15 +59,108 @@ private class SqlDelightFileViewProvider(
   manager: PsiManager,
   virtualFile: VirtualFile,
   eventSystemEnabled: Boolean,
-  private val language: Language
+  private val language: Language,
+  private val module: Module
 ) : SingleRootFileViewProvider(manager, virtualFile, eventSystemEnabled, language) {
+  private val threadPool = Executors.newScheduledThreadPool(1)
 
   private val file: SqlDelightFile
     get() = getPsiInner(language) as SqlDelightFile
 
+  private var filesGenerated = emptyList<VirtualFile>()
+    set(value) {
+      (field - value).forEach { it.delete(this) }
+      field = value
+    }
+
+  private var condition = WriteCondition()
+
   override fun contentsSynchronized() {
     super.contentsSynchronized()
 
-    FileGeneratorService.getInstance(file.project).generateFiles(file)
+    if (!SqlDelightFileIndex.getInstance(module).isConfigured ||
+      SqlDelightFileIndex.getInstance(module).sourceFolders(file).isEmpty()
+    ) {
+      return
+    }
+
+    condition.invalidated.set(true)
+
+    if (ApplicationManager.getApplication().isUnitTestMode) {
+      generateSqlDelightCode()
+      return
+    }
+
+    val thisCondition = WriteCondition()
+    condition = thisCondition
+    threadPool.schedule(
+      {
+        ApplicationManager.getApplication().invokeLater(
+          {
+            try {
+              generateSqlDelightCode()
+            } catch (e: Throwable) {
+              // IDE generating code should be best effort - source of truth is always the gradle
+              // build, and its better to ignore the error and try again than crash and require
+              // the IDE restarts.
+              e.printStackTrace()
+            }
+          },
+          thisCondition
+        )
+      },
+      1, TimeUnit.SECONDS
+    )
+  }
+
+  /**
+   * Attempt to generate the SQLDelight code for the file represented by the view provider.
+   */
+  private fun generateSqlDelightCode() {
+    if (module.isDisposed) return
+
+    var shouldGenerate = true
+    val annotationHolder = object : SqlAnnotationHolder {
+      override fun createErrorAnnotation(element: PsiElement, s: String) {
+        shouldGenerate = false
+      }
+    }
+
+    // File is mutable so create a copy that wont be mutated.
+    val file = file.copy() as SqlDelightFile
+
+    shouldGenerate = try {
+      PsiTreeUtil.processElements(file) { element ->
+        when (element) {
+          is PsiErrorElement -> return@processElements false
+          is SqlAnnotatedElement -> element.annotate(annotationHolder)
+        }
+        return@processElements shouldGenerate
+      }
+    } catch (e: Throwable) {
+      // If we encountered an exception while looking for errors, assume it was an error.
+      false
+    }
+
+    if (shouldGenerate && !ApplicationManager.getApplication().isUnitTestMode) ApplicationManager.getApplication().runWriteAction {
+      val files = mutableListOf<VirtualFile>()
+      val fileAppender = { filePath: String ->
+        val vFile: VirtualFile by GeneratedVirtualFile(filePath, module)
+        files.add(vFile)
+        PrintStream(vFile.getOutputStream(this))
+      }
+      if (file is SqlDelightQueriesFile) {
+        SqlDelightCompiler.writeInterfaces(module, file, fileAppender)
+      } else if (file is MigrationFile) {
+        SqlDelightCompiler.writeInterfaces(file, fileAppender)
+      }
+      this.filesGenerated = files
+    }
+  }
+
+  private class WriteCondition : Condition<Any> {
+    var invalidated = AtomicBoolean(false)
+
+    override fun value(t: Any?) = invalidated.get()
   }
 }
