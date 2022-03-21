a879a30bcf0996d7d34454d0b84ef898bcf7df80
andrew <me@satook.com>
2022-03-21 17:12:45 +1100

    Added passing test for Postgres INSERT DO UPDATE
    To help with diagnosis of issue #2791.

--- sqldelight-compiler/src/test/kotlin/app/cash/sqldelight/core/queries/PgInsertOnConflictTest.kt
+++ sqldelight-compiler/src/test/kotlin/app/cash/sqldelight/core/queries/PgInsertOnConflictTest.kt
@@ -33,7 +33,7 @@ class PgInsertOnConflictTest {
 
         Truth.assertThat(generator.function().toString()).isEqualTo(
             """
-            |public fun upsert2Cols(
+            |public fun upsertCols(
             |  id: kotlin.Int?,
             |  c1: kotlin.String?
             |): kotlin.Unit {
@@ -45,6 +45,57 @@ class PgInsertOnConflictTest {
             |    bindLong(1, id?.let { it.toLong() })
             |    bindString(2, c1)
             |    bindString(3, c1)
+            |  }
+            |  notifyQueries(${insert.id}) { emit ->
+            |    emit("data")
+            |  }
+            |}
+            |
+            """.trimMargin()
+        )
+    }
+
+    @Test
+    fun `postgres INSERT DO UPDATE works with 2 columns`() {
+        val file = FixtureCompiler.parseSql(
+            """
+            |CREATE TABLE data (
+            |  id INTEGER NOT NULL PRIMARY KEY,
+            |  col1 TEXT DEFAULT '',
+            |  col2 TEXT DEFAULT '',
+            |  col3 TEXT DEFAULT ''
+            |);
+            |
+            |upsertCols:
+            |INSERT INTO data
+            |VALUES (:id, :c1, :c2, :c3)
+            |ON CONFLICT (id) DO UPDATE SET col1 = :c1, col2 = :c2;
+            """.trimMargin(),
+            tempFolder, dialectPreset = DialectPreset.POSTGRESQL
+        )
+
+        val insert = file.namedMutators.first()
+        val generator = MutatorQueryGenerator(insert)
+
+        Truth.assertThat(generator.function().toString()).isEqualTo(
+            """
+            |public fun upsertCols(
+            |  id: kotlin.Int?,
+            |  c1: kotlin.String?,
+            |  c2: kotlin.String?,
+            |  c3: kotlin.String?
+            |): kotlin.Unit {
+            |  driver.execute(${insert.id}, ""${'"'}
+            |  |INSERT INTO data
+            |  |VALUES (?, ?, ?, ?)
+            |  |ON CONFLICT (id) DO UPDATE SET col1 = ?, col2 = ?
+            |  ""${'"'}.trimMargin(), 6) {
+            |    bindLong(1, id?.let { it.toLong() })
+            |    bindString(2, c1)
+            |    bindString(3, c2)
+            |    bindString(4, c3)
+            |    bindString(5, c1)
+            |    bindString(6, c2)
             |  }
             |  notifyQueries(${insert.id}) { emit ->
             |    emit("data")
@@ -79,7 +130,7 @@ class PgInsertOnConflictTest {
 
         Truth.assertThat(generator.function().toString()).isEqualTo(
             """
-            |public fun upsert2Cols(
+            |public fun upsertCols(
             |  id: kotlin.Int?,
             |  c1: kotlin.String?,
             |  c2: kotlin.String?,
