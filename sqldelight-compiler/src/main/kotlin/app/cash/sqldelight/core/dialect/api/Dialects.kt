7650d9005c62ddd51121328b579c5dec9c7f34b5
Marius Volkhart <marius@volkhart.com>
2022-03-21 20:36:20 -0400

    fixup! Create interfaces needed in compiler to implement codegen

--- sqldelight-compiler/src/main/kotlin/app/cash/sqldelight/core/dialect/api/Dialects.kt
+++ sqldelight-compiler/src/main/kotlin/app/cash/sqldelight/core/dialect/api/Dialects.kt
@@ -10,10 +10,12 @@ import com.alecstrong.sql.psi.core.DialectPreset.SQLITE_3_24
 import com.alecstrong.sql.psi.core.DialectPreset.SQLITE_3_25
 
 /**
- * Temporary shim to map sql-psi [DialectPreset]s to [SqlDelightDialect]s. This is needed because the Gradle plugin doesn't yet
- * have a new API to provide more information about the dialect/driver implementation.
+ * Temporary shim to map sql-psi [DialectPreset]s to [SqlDelightDialect]s. This is needed because the Gradle plugin
+ * doesn't yet have a new API to provide more information about the dialect/driver implementation. The intention is for
+ * all hard references in the compiler to be replaced with the dialect API.
  *
- * See https://github.com/cashapp/sqldelight/issues/2128 and
+ * See https://github.com/cashapp/sqldelight/pull/2918#pullrequestreview-915124435 and
+ * https://github.com/cashapp/sqldelight/issues/2128 and
  * https://github.com/cashapp/sqldelight/issues/2821#issuecomment-1039562786
  */
 fun DialectPreset.toSqlDelightDialect(): SqlDelightDialect = when (this) {
