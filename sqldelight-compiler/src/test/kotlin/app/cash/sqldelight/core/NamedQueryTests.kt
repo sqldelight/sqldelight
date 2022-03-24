ceabe7db7e9af3e9b5918bf01643610273c539f9
Alec Strong <AlecStrong@users.noreply.github.com>
2022-03-24 19:08:37 -0400

    Remove explicit dependencies on sqlite from the compiler (#2935)

--- sqldelight-compiler/src/test/kotlin/app/cash/sqldelight/core/NamedQueryTests.kt
+++ sqldelight-compiler/src/test/kotlin/app/cash/sqldelight/core/NamedQueryTests.kt
@@ -28,7 +28,7 @@ class NamedQueryTests {
     )
 
     val query = file.namedQueries.first()
-    val table = file.sqliteStatements().mapNotNull { it.statement.createTableStmt }.first()
+    val table = file.sqlStatements().mapNotNull { it.statement.createTableStmt }.first()
 
     assertThat(query.tablesObserved).containsExactly(CreateTableName(table.tableName))
   }
@@ -50,7 +50,7 @@ class NamedQueryTests {
     )
 
     val query = file.namedQueries.first()
-    val table = file.sqliteStatements().mapNotNull { it.statement.createTableStmt }.first()
+    val table = file.sqlStatements().mapNotNull { it.statement.createTableStmt }.first()
 
     assertThat(query.tablesObserved).containsExactly(CreateTableName(table.tableName))
   }
@@ -75,7 +75,7 @@ class NamedQueryTests {
     )
 
     val query = file.namedQueries.first()
-    val table = file.sqliteStatements().mapNotNull { it.statement.createTableStmt }.first()
+    val table = file.sqlStatements().mapNotNull { it.statement.createTableStmt }.first()
 
     assertThat(query.tablesObserved).containsExactly(CreateTableName(table.tableName))
   }
@@ -100,7 +100,7 @@ class NamedQueryTests {
     )
 
     val query = file.namedQueries.first()
-    val table = file.sqliteStatements().mapNotNull { it.statement.createTableStmt }.first()
+    val table = file.sqlStatements().mapNotNull { it.statement.createTableStmt }.first()
 
     assertThat(query.tablesObserved).containsExactly(CreateTableName(table.tableName))
   }
@@ -122,7 +122,7 @@ class NamedQueryTests {
     )
 
     val query = file.namedQueries.first()
-    val table = file.sqliteStatements().mapNotNull { it.statement.createTableStmt }.first()
+    val table = file.sqlStatements().mapNotNull { it.statement.createTableStmt }.first()
 
     assertThat(query.tablesObserved).containsExactly(CreateTableName(table.tableName))
   }
