ab9d13e8acae6c8e008aaabfa6d590db01d818a2
Eliezer Graber <eygraber@gmail.com>
2022-03-24 19:39:14 -0400

    Add support for Sqlite 3.30 dialect (#2930)
    * Add support for Sqlite 3.30 dialect
    
    * Update  sqldelight-gradle-plugin/src/main/kotlin/app/cash/sqldelight/gradle/android/PackageName.kt
    
    Co-authored-by: Jake Wharton <github@jakewharton.com>
    
    * Handle all Sqlite dialects in TestDialect binderCheck/cursorCheck
    
    Co-authored-by: Jake Wharton <github@jakewharton.com>

--- sqldelight-compiler/src/test/kotlin/app/cash/sqldelight/core/TestDialect.kt
+++ sqldelight-compiler/src/test/kotlin/app/cash/sqldelight/core/TestDialect.kt
@@ -10,6 +10,7 @@ enum class TestDialect(val dialect: SqlDelightDialect) {
   SQLITE_3_18(SqliteDialect()),
   SQLITE_3_24(app.cash.sqldelight.dialects.sqlite_3_24.SqliteDialect()),
   SQLITE_3_25(app.cash.sqldelight.dialects.sqlite_3_25.SqliteDialect()),
+  SQLITE_3_30(app.cash.sqldelight.dialects.sqlite_3_30.SqliteDialect()),
   POSTGRESQL(PostgreSqlDialect()),
   HSQL(HsqlDialect()),
   MYSQL(MySqlDialect()),
