0ab37fdfabae588b9a5fed0de91bbc07c5a30a85
Alec Strong <astrong@squareup.com>
2022-03-25 12:19:24 -0400

    Move the SQLite 3.30 dialect from SQL PSI.

--- dialects/sqlite-3-30/src/main/kotlin/app/cash/sqldelight/dialects/sqlite_3_30/SqliteDialect.kt
+++ dialects/sqlite-3-30/src/main/kotlin/app/cash/sqldelight/dialects/sqlite_3_30/SqliteDialect.kt
@@ -4,14 +4,15 @@ import app.cash.sqldelight.dialect.api.SqlDelightDialect
 import app.cash.sqldelight.dialect.api.TypeResolver
 import app.cash.sqldelight.dialects.sqlite_3_18.SqliteMigrationStrategy
 import app.cash.sqldelight.dialects.sqlite_3_18.SqliteTypeResolver
-import com.alecstrong.sql.psi.core.DialectPreset.SQLITE_3_30
+import app.cash.sqldelight.dialects.sqlite_3_30.grammar.SqliteParserUtil
+import com.alecstrong.sql.psi.core.DialectPreset.SQLITE_3_25
 import com.intellij.icons.AllIcons
 import com.squareup.kotlinpoet.ClassName
 
 /**
  * A dialect for SQLite.
  */
-class SqliteDialect : SqlDelightDialect {
+open class SqliteDialect : SqlDelightDialect {
   override val driverType = ClassName("app.cash.sqldelight.db", "SqlDriver")
   override val cursorType = ClassName("app.cash.sqldelight.db", "SqlCursor")
   override val preparedStatementType = ClassName("app.cash.sqldelight.db", "SqlPreparedStatement")
@@ -20,7 +21,9 @@ class SqliteDialect : SqlDelightDialect {
   override val migrationStrategy = SqliteMigrationStrategy()
 
   override fun setup() {
-    SQLITE_3_30.setup()
+    SQLITE_3_25.setup()
+    SqliteParserUtil.reset()
+    SqliteParserUtil.overrideSqlParser()
   }
 
   override fun typeResolver(parentResolver: TypeResolver): TypeResolver {
--- dialects/sqlite-3-30/src/test/kotlin/app/cash/sqldelight/dialects/sqlite_3_30/Sqlite330FixturesTest.kt
+++ dialects/sqlite-3-30/src/test/kotlin/app/cash/sqldelight/dialects/sqlite_3_30/Sqlite330FixturesTest.kt
@@ -1,6 +1,5 @@
-package app.cash.sqldelight.dialect.sqlite_3_35
+package app.cash.sqldelight.dialects.sqlite_3_30
 
-import app.cash.sqldelight.dialects.sqlite_3_35.SqliteDialect
 import com.alecstrong.sql.psi.test.fixtures.FixturesTest
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
@@ -8,7 +7,7 @@ import org.junit.runners.Parameterized.Parameters
 import java.io.File
 
 @RunWith(Parameterized::class)
-class Sqlite335FixturesTest(name: String, fixtureRoot: File) : FixturesTest(name, fixtureRoot) {
+class Sqlite330FixturesTest(name: String, fixtureRoot: File) : FixturesTest(name, fixtureRoot) {
   override val replaceRules = arrayOf(
     "ORDER or WHERE expected" to "ORDER, WHERE or WINDOW expected"
   )
@@ -18,7 +17,7 @@ class Sqlite335FixturesTest(name: String, fixtureRoot: File) : FixturesTest(name
   }
 
   companion object {
-    private val fixtures = arrayOf(/*"src/test/fixtures_sqlite_3_18", "src/test/fixtures_sqlite_3_24", "src/test/fixtures_sqlite_3_25",*/ "../sqlite-3-30/src/test/fixtures_sqlite_3_30", "src/test/fixtures_sqlite_3_35")
+    private val fixtures = arrayOf(/*"src/test/fixtures_sqlite_3_18", "src/test/fixtures_sqlite_3_24", "src/test/fixtures_sqlite_3_25",*/ "src/test/fixtures_sqlite_3_30")
 
     @Suppress("unused") // Used by Parameterized JUnit runner reflectively.
     @Parameters(name = "{0}")
