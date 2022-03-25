f0d519b6fc61264e3611e20f23c23d50be16dadf
Alec Strong <astrong@squareup.com>
2022-03-25 12:58:08 -0400

    Move the SQLite 3.24 dialect from SQL PSI

--- dialects/sqlite-3-24/src/main/kotlin/app/cash/sqldelight/dialects/sqlite_3_24/SqliteTypeResolver.kt
+++ dialects/sqlite-3-24/src/main/kotlin/app/cash/sqldelight/dialects/sqlite_3_24/SqliteTypeResolver.kt
@@ -2,19 +2,19 @@ package app.cash.sqldelight.dialects.sqlite_3_24
 
 import app.cash.sqldelight.dialect.api.IntermediateType
 import app.cash.sqldelight.dialect.api.TypeResolver
-import app.cash.sqldelight.dialects.sqlite_3_18.SqliteTypeResolver
+import app.cash.sqldelight.dialects.sqlite_3_24.grammar.psi.SqliteUpsertDoUpdate
 import com.alecstrong.sql.psi.core.psi.SqlExpr
 import com.alecstrong.sql.psi.core.psi.SqlSetterExpression
-import com.alecstrong.sql.psi.core.sqlite_3_24.psi.SqliteUpsertDoUpdate
 import com.intellij.psi.PsiElement
+import app.cash.sqldelight.dialects.sqlite_3_18.SqliteTypeResolver as Sqlite318TypeResolver
 
-class SqliteTypeResolver(parentResolver: TypeResolver) : SqliteTypeResolver(parentResolver) {
+class SqliteTypeResolver(parentResolver: TypeResolver) : Sqlite318TypeResolver(parentResolver) {
   override fun argumentType(
     parent: PsiElement,
     argument: SqlExpr
   ): IntermediateType {
     when (parent) {
-      is SqlSetterExpression -> when (val parentRule = parent.parent!!) {
+      is SqlSetterExpression -> when (parent.parent!!) {
         is SqliteUpsertDoUpdate -> return resolvedType(parent.expr)
       }
     }
