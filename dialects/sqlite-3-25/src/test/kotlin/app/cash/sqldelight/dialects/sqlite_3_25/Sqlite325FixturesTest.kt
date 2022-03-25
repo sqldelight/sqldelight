2f962a564d3285e03d7c9fe8561c483be8a49aa2
Alec Strong <astrong@squareup.com>
2022-03-25 12:40:16 -0400

    Move the SQLite 3.25 dialect from SQL PSI

--- dialects/sqlite-3-25/src/test/kotlin/app/cash/sqldelight/dialects/sqlite_3_25/Sqlite325FixturesTest.kt
+++ dialects/sqlite-3-25/src/test/kotlin/app/cash/sqldelight/dialects/sqlite_3_25/Sqlite325FixturesTest.kt
@@ -1,4 +1,4 @@
-package app.cash.sqldelight.dialects.sqlite_3_30
+package app.cash.sqldelight.dialects.sqlite_3_25
 
 import com.alecstrong.sql.psi.test.fixtures.FixturesTest
 import org.junit.runner.RunWith
@@ -7,7 +7,7 @@ import org.junit.runners.Parameterized.Parameters
 import java.io.File
 
 @RunWith(Parameterized::class)
-class Sqlite330FixturesTest(name: String, fixtureRoot: File) : FixturesTest(name, fixtureRoot) {
+class Sqlite325FixturesTest(name: String, fixtureRoot: File) : FixturesTest(name, fixtureRoot) {
   override val replaceRules = arrayOf(
     "ORDER or WHERE expected" to "ORDER, WHERE or WINDOW expected"
   )
@@ -17,7 +17,7 @@ class Sqlite330FixturesTest(name: String, fixtureRoot: File) : FixturesTest(name
   }
 
   companion object {
-    private val fixtures = arrayOf(/*"src/test/fixtures_sqlite_3_18", "src/test/fixtures_sqlite_3_24",*/ "../sqlite-3-25/src/test/fixtures_sqlite_3_25", "src/test/fixtures_sqlite_3_30")
+    private val fixtures = arrayOf(/*"src/test/fixtures_sqlite_3_18", "src/test/fixtures_sqlite_3_24", */"src/test/fixtures_sqlite_3_25")
 
     @Suppress("unused") // Used by Parameterized JUnit runner reflectively.
     @Parameters(name = "{0}")
