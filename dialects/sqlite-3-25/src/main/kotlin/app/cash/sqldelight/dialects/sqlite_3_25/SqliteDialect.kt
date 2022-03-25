0f96d6756fa2ca5ac1bff7b67db9a660f50ab174
Alec Strong <astrong@squareup.com>
2022-03-25 12:58:08 -0400

    Move the SQLite 3.24 dialect from SQL PSI

--- dialects/sqlite-3-25/src/main/kotlin/app/cash/sqldelight/dialects/sqlite_3_25/SqliteDialect.kt
+++ dialects/sqlite-3-25/src/main/kotlin/app/cash/sqldelight/dialects/sqlite_3_25/SqliteDialect.kt
@@ -1,32 +1,12 @@
 package app.cash.sqldelight.dialects.sqlite_3_25
 
-import app.cash.sqldelight.dialect.api.SqlDelightDialect
-import app.cash.sqldelight.dialect.api.TypeResolver
-import app.cash.sqldelight.dialects.sqlite_3_18.SqliteMigrationStrategy
-import app.cash.sqldelight.dialects.sqlite_3_18.SqliteTypeResolver
 import app.cash.sqldelight.dialects.sqlite_3_25.grammar.SqliteParserUtil
-import com.alecstrong.sql.psi.core.DialectPreset.SQLITE_3_24
-import com.intellij.icons.AllIcons
-import com.squareup.kotlinpoet.ClassName
+import app.cash.sqldelight.dialects.sqlite_3_24.SqliteDialect as Sqlite324Dialect
 
-/**
- * A dialect for SQLite.
- */
-open class SqliteDialect : SqlDelightDialect {
-  override val driverType = ClassName("app.cash.sqldelight.db", "SqlDriver")
-  override val cursorType = ClassName("app.cash.sqldelight.db", "SqlCursor")
-  override val preparedStatementType = ClassName("app.cash.sqldelight.db", "SqlPreparedStatement")
-  override val isSqlite = true
-  override val icon = AllIcons.Providers.Sqlite
-  override val migrationStrategy = SqliteMigrationStrategy()
-
+open class SqliteDialect : Sqlite324Dialect() {
   override fun setup() {
-    SQLITE_3_24.setup()
+    super.setup()
     SqliteParserUtil.reset()
     SqliteParserUtil.overrideSqlParser()
-  }
-
-  override fun typeResolver(parentResolver: TypeResolver): TypeResolver {
-    return SqliteTypeResolver(parentResolver)
   }
 }
