82d3daa140124a86ce712fb7f33d9cd944a33208
Alec Strong <AlecStrong@users.noreply.github.com>
2022-04-07 10:13:10 -0400

    Support PG Booleans and fix inserts on alter tables (#3013)

--- sqldelight-compiler/src/main/kotlin/app/cash/sqldelight/core/lang/util/CreateTableUtil.kt
+++ sqldelight-compiler/src/main/kotlin/app/cash/sqldelight/core/lang/util/CreateTableUtil.kt
@@ -4,5 +4,3 @@ import app.cash.sqldelight.core.compiler.SqlDelightCompiler.allocateName
 import com.alecstrong.sql.psi.core.psi.SqlCreateTableStmt
 import com.squareup.kotlinpoet.ClassName
 
-internal val SqlCreateTableStmt.interfaceType: ClassName
-  get() = ClassName(sqFile().packageName!!, allocateName(tableName).capitalize())
