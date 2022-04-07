7157c646763387a9935df7331d561ab38e32ca55
Alec Strong <AlecStrong@users.noreply.github.com>
2022-04-07 12:00:33 -0400

    Correctly resolve alter table rename statements (#3015)

--- sqldelight-compiler/src/main/kotlin/app/cash/sqldelight/core/lang/util/CreateTableUtil.kt
+++
@@ -1,6 +0,0 @@
-package app.cash.sqldelight.core.lang.util
-
-import app.cash.sqldelight.core.compiler.SqlDelightCompiler.allocateName
-import com.alecstrong.sql.psi.core.psi.SqlCreateTableStmt
-import com.squareup.kotlinpoet.ClassName
-
