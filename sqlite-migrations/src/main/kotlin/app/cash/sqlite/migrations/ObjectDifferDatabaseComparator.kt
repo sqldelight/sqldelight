256f2a46c9752c6feee24adf2545ec4cfcbee6c3
Alec Strong <astrong@squareup.com>
2022-04-11 09:38:55 -0400

    Compare schema definitions for migrations

--- sqlite-migrations/src/main/kotlin/app/cash/sqlite/migrations/ObjectDifferDatabaseComparator.kt
+++ sqlite-migrations/src/main/kotlin/app/cash/sqlite/migrations/ObjectDifferDatabaseComparator.kt
@@ -1,7 +1,12 @@
 package app.cash.sqlite.migrations
 
 import de.danielbechler.diff.ObjectDifferBuilder
+import de.danielbechler.diff.comparison.ComparisonStrategy
 import de.danielbechler.diff.node.DiffNode
+import de.danielbechler.diff.node.DiffNode.State.ADDED
+import de.danielbechler.diff.node.DiffNode.State.CHANGED
+import de.danielbechler.diff.node.DiffNode.State.REMOVED
+import de.danielbechler.diff.node.DiffNode.State.UNTOUCHED
 
 class ObjectDifferDatabaseComparator(
   private val circularReferenceExceptionLogger: ((String) -> Unit)? = null
@@ -21,12 +26,32 @@ class ObjectDifferDatabaseComparator(
       propertyName("importedForeignKeys")
       propertyName("deferrable")
       propertyName("initiallyDeferred")
-      // Definition changes aren't important, its just things like comments or whitespace.
-      propertyName("definition")
     }
     // Partial columns are used for unresolved columns to avoid cycles. Matching based on string
     // is fine for our purposes.
     comparison().ofType(Class.forName("schemacrawler.crawl.ColumnPartial")).toUseEqualsMethod()
+
+    // This is only used to compare definitions. Definitions include whitespace and comments
+    // so we want those removed.
+    comparison().ofType(String::class.java).toUse(object : ComparisonStrategy {
+      override fun compare(node: DiffNode, type: Class<*>, working: Any?, base: Any?) {
+        if (working == null && base == null) {
+          node.state = UNTOUCHED
+        } else if (working == null || working !is String) {
+          node.state = REMOVED
+        } else if (base == null || base !is String) {
+          node.state = ADDED
+        } else if (working.normalizeDefinition()
+          .equals(base.normalizeDefinition(), ignoreCase = true)
+        ) {
+          node.state = UNTOUCHED
+        } else {
+          node.state = CHANGED
+        }
+      }
+
+      private fun String.normalizeDefinition() = replace(Regex("--(.*)"), "").replace(Regex("\\s+"), "")
+    })
 
     // Custom error handler for circular reference warnings which allows to override SL4J warning log
     val circularReferenceExceptionLogger = circularReferenceExceptionLogger
