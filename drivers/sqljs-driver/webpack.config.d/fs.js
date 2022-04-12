7a1cbdc6c270427ff60c414a04554b52e09c8469
Derek Ellis <derekellis170@gmail.com>
2022-04-12 00:20:19 -0400

    Use Karma for js browser tests
    Update test configuration for sqljs-driver and coroutines-extensions
    Mocha only runs in node.
    Add documentation for using JS driver with Karma

--- drivers/sqljs-driver/webpack.config.d/fs.js
+++ drivers/sqljs-driver/webpack.config.d/fs.js
@@ -1,5 +1,9 @@
 // Remove fs in the webpack config in order to build for front end
 // https://github.com/webpack-contrib/css-loader/issues/447
-config.node = {
-    fs: 'empty'
+config.resolve = {
+    fallback: {
+        fs: false,
+        path: false,
+        crypto: false,
+    }
 };
