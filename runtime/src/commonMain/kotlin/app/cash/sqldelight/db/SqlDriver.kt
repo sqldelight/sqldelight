80b18ea9b1924b8245256c6925ec6095abed74ef
Alec Strong <astrong@squareup.com>
2022-04-16 07:51:45 -0400

    Remove the AfterVersion type without the driver

--- runtime/src/commonMain/kotlin/app/cash/sqldelight/db/SqlDriver.kt
+++ runtime/src/commonMain/kotlin/app/cash/sqldelight/db/SqlDriver.kt
@@ -110,43 +110,13 @@ interface SqlDriver : Closeable {
 
 /**
  * Represents a block of code [block] that should be executed during a migration after the migration
- * has finished migrating to [afterVersion].
- */
-class AfterVersion(
-  internal val afterVersion: Int,
-  internal val block: () -> Unit
-)
-
-/**
- * Represents a block of code [block] that should be executed during a migration after the migration
  * has finished migrating to [afterVersion]. Unlike [AfterVersion], this version's lambda accepts a
  * [SqlDriver] as a parameter to make migrations easier.
  */
-class AfterVersionWithDriver(
+class AfterVersion(
   internal val afterVersion: Int,
   internal val block: (SqlDriver) -> Unit
 )
-
-/**
- * Wrap an [AfterVersion] as an [AfterVersionWithDriver].
- */
-fun AfterVersion.toAfterVersionWithDriver() =
-  AfterVersionWithDriver(afterVersion) { block() }
-
-/**
- * Run [SqlDriver.Schema.migrate] normally but execute [callbacks] during the migration whenever
- * it finished upgrading to a version specified by [AfterVersion.afterVersion]. This method
- * takes [AfterVersion] callbacks, which receive no parameters when invoked.
- */
-fun SqlDriver.Schema.migrateWithCallbacks(
-  driver: SqlDriver,
-  oldVersion: Int,
-  newVersion: Int,
-  vararg callbacks: AfterVersion
-) {
-  val wrappedCallbacks = callbacks.map { it.toAfterVersionWithDriver() }.toTypedArray()
-  migrateWithCallbacks(driver, oldVersion, newVersion, *wrappedCallbacks)
-}
 
 /**
  * Run [SqlDriver.Schema.migrate] normally but execute [callbacks] during the migration whenever
@@ -157,7 +127,7 @@ fun SqlDriver.Schema.migrateWithCallbacks(
   driver: SqlDriver,
   oldVersion: Int,
   newVersion: Int,
-  vararg callbacks: AfterVersionWithDriver
+  vararg callbacks: AfterVersion
 ) {
   var lastVersion = oldVersion
 
--- sqldelight-gradle-plugin/src/test/integration-migration-callbacks/src/test/java/app/cash/sqldelight/integration/IntegrationTests.kt
+++ sqldelight-gradle-plugin/src/test/integration-migration-callbacks/src/test/java/app/cash/sqldelight/integration/IntegrationTests.kt
@@ -1,7 +1,6 @@
 package app.cash.sqldelight.integration
 
 import app.cash.sqldelight.db.AfterVersion
-import app.cash.sqldelight.db.AfterVersionWithDriver
 import app.cash.sqldelight.db.migrateWithCallbacks
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver.Companion.IN_MEMORY
@@ -15,29 +14,9 @@ class IntegrationTests {
       driver = database,
       oldVersion = 0,
       newVersion = QueryWrapper.Schema.version,
-      AfterVersion(1) { database.execute(null, "INSERT INTO test (value) VALUES('hello')", 0) },
-      AfterVersion(2) { database.execute(null, "INSERT INTO test2 (value, value2) VALUES('hello2', 'sup2')", 0) },
-      AfterVersion(3) { database.execute(null, "INSERT INTO test2 (new_value, new_value2) VALUES('hello3', 'sup3')", 0) },
-    )
-
-    val queryWrapper = QueryWrapper(database)
-    val values = queryWrapper.testQueries.selectAll().executeAsList()
-    assertThat(values).containsExactly(
-      Test2(new_value = "hello", new_value2 = "sup"),
-      Test2(new_value = "hello2", new_value2 = "sup2"),
-      Test2(new_value = "hello3", new_value2 = "sup3"),
-    )
-  }
-
-  @Test fun migrationCallbacksWithParameter() {
-    val database = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
-    QueryWrapper.Schema.migrateWithCallbacks(
-      driver = database,
-      oldVersion = 0,
-      newVersion = QueryWrapper.Schema.version,
-      AfterVersionWithDriver(1) { it.execute(null, "INSERT INTO test (value) VALUES('hello')", 0) },
-      AfterVersionWithDriver(2) { it.execute(null, "INSERT INTO test2 (value, value2) VALUES('hello2', 'sup2')", 0) },
-      AfterVersionWithDriver(3) { it.execute(null, "INSERT INTO test2 (new_value, new_value2) VALUES('hello3', 'sup3')", 0) },
+      AfterVersion(1) { it.execute(null, "INSERT INTO test (value) VALUES('hello')", 0) },
+      AfterVersion(2) { it.execute(null, "INSERT INTO test2 (value, value2) VALUES('hello2', 'sup2')", 0) },
+      AfterVersion(3) { it.execute(null, "INSERT INTO test2 (new_value, new_value2) VALUES('hello3', 'sup3')", 0) },
     )
 
     val queryWrapper = QueryWrapper(database)
@@ -60,33 +39,9 @@ class IntegrationTests {
       driver = database,
       oldVersion = 2,
       newVersion = QueryWrapper.Schema.version,
-      AfterVersion(1) { database.execute(null, "INSERT INTO test (value) VALUES('hello')", 0) },
-      AfterVersion(2) { database.execute(null, "INSERT INTO test2 (value, value2) VALUES('hello2', 'sup2')", 0) },
-      AfterVersion(3) { database.execute(null, "INSERT INTO test2 (new_value, new_value2) VALUES('hello3', 'sup3')", 0) },
-    )
-
-    val queryWrapper = QueryWrapper(database)
-    val values = queryWrapper.testQueries.selectAll().executeAsList()
-    assertThat(values).containsExactly(
-      Test2(new_value = "hello2", new_value2 = "sup2"),
-      Test2(new_value = "hello3", new_value2 = "sup3"),
-    )
-  }
-
-  @Test fun migrationCallbackLessThanOldVersionNotCalledWithParameter() {
-    val database = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
-    QueryWrapper.Schema.migrate(
-      driver = database,
-      oldVersion = 0,
-      newVersion = 2
-    )
-    QueryWrapper.Schema.migrateWithCallbacks(
-      driver = database,
-      oldVersion = 2,
-      newVersion = QueryWrapper.Schema.version,
-      AfterVersionWithDriver(1) { it.execute(null, "INSERT INTO test (value) VALUES('hello')", 0) },
-      AfterVersionWithDriver(2) { it.execute(null, "INSERT INTO test2 (value, value2) VALUES('hello2', 'sup2')", 0) },
-      AfterVersionWithDriver(3) { it.execute(null, "INSERT INTO test2 (new_value, new_value2) VALUES('hello3', 'sup3')", 0) },
+      AfterVersion(1) { it.execute(null, "INSERT INTO test (value) VALUES('hello')", 0) },
+      AfterVersion(2) { it.execute(null, "INSERT INTO test2 (value, value2) VALUES('hello2', 'sup2')", 0) },
+      AfterVersion(3) { it.execute(null, "INSERT INTO test2 (new_value, new_value2) VALUES('hello3', 'sup3')", 0) },
     )
 
     val queryWrapper = QueryWrapper(database)
@@ -103,31 +58,10 @@ class IntegrationTests {
       driver = database,
       oldVersion = 0,
       newVersion = QueryWrapper.Schema.version,
-      AfterVersion(1) { database.execute(null, "INSERT INTO test (value) VALUES('hello')", 0) },
-      AfterVersion(2) { database.execute(null, "INSERT INTO test2 (value, value2) VALUES('hello2', 'sup2')", 0) },
-      AfterVersion(3) { database.execute(null, "INSERT INTO test2 (new_value, new_value2) VALUES('hello3', 'sup3')", 0) },
-      AfterVersion(4) { database.execute(null, "INSERT INTO test2 (new_value, new_value2) VALUES('hello4', 'sup4')", 0) },
-    )
-
-    val queryWrapper = QueryWrapper(database)
-    val values = queryWrapper.testQueries.selectAll().executeAsList()
-    assertThat(values).containsExactly(
-      Test2(new_value = "hello", new_value2 = "sup"),
-      Test2(new_value = "hello2", new_value2 = "sup2"),
-      Test2(new_value = "hello3", new_value2 = "sup3"),
-    )
-  }
-
-  @Test fun migrationCallbackGreaterThanNewVersionNotCalledWithParameter() {
-    val database = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
-    QueryWrapper.Schema.migrateWithCallbacks(
-      driver = database,
-      oldVersion = 0,
-      newVersion = QueryWrapper.Schema.version,
-      AfterVersionWithDriver(1) { it.execute(null, "INSERT INTO test (value) VALUES('hello')", 0) },
-      AfterVersionWithDriver(2) { it.execute(null, "INSERT INTO test2 (value, value2) VALUES('hello2', 'sup2')", 0) },
-      AfterVersionWithDriver(3) { it.execute(null, "INSERT INTO test2 (new_value, new_value2) VALUES('hello3', 'sup3')", 0) },
-      AfterVersionWithDriver(4) { it.execute(null, "INSERT INTO test2 (new_value, new_value2) VALUES('hello4', 'sup4')", 0) },
+      AfterVersion(1) { it.execute(null, "INSERT INTO test (value) VALUES('hello')", 0) },
+      AfterVersion(2) { it.execute(null, "INSERT INTO test2 (value, value2) VALUES('hello2', 'sup2')", 0) },
+      AfterVersion(3) { it.execute(null, "INSERT INTO test2 (new_value, new_value2) VALUES('hello3', 'sup3')", 0) },
+      AfterVersion(4) { it.execute(null, "INSERT INTO test2 (new_value, new_value2) VALUES('hello4', 'sup4')", 0) },
     )
 
     val queryWrapper = QueryWrapper(database)
