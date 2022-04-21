b8ea2b34630f798a913ee0e2b8148298d82fe9fc
Alec Strong <AlecStrong@users.noreply.github.com>
2022-04-21 15:05:59 -0400

    Use IJ Toolbar control for connections and store them by dialect (#3127)

--- sqldelight-idea-plugin/src/main/kotlin/app/cash/sqldelight/intellij/run/ConnectionOptions.kt
+++ sqldelight-idea-plugin/src/main/kotlin/app/cash/sqldelight/intellij/run/ConnectionOptions.kt
@@ -1,5 +1,6 @@
 package app.cash.sqldelight.intellij.run
 
+import app.cash.sqldelight.core.SqlDelightProjectService
 import app.cash.sqldelight.dialect.api.ConnectionManager.ConnectionProperties
 import com.intellij.ide.util.propComponentProperty
 import com.intellij.openapi.project.Project
@@ -7,27 +8,46 @@ import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Moshi
 
 internal class ConnectionOptions(val project: Project) {
-  private var options: String by propComponentProperty(project, "connection_options", "")
-  var selectedOption: String by propComponentProperty(project, "selected_option", "")
+  private var storedOptions: String by propComponentProperty(project, "stored_options", "")
 
-  fun addOption(properties: ConnectionProperties) {
-    val currentOptions = if (options.isEmpty()) StoredOptions() else adapter.fromJson(options)!!
-    currentOptions.map[properties.key] = properties.serializedProperties
-    options = adapter.toJson(currentOptions)
+  private fun <T> withCurrentDialect(action: DialectConnections.() -> T): T {
+    val currentOptions =
+      if (storedOptions.isEmpty()) StoredOptions()
+      else adapter.fromJson(storedOptions)!!
+
+    val dialectKey = SqlDelightProjectService.getInstance(project).dialect::class.qualifiedName
+      ?: throw IllegalStateException("SqlDelightDialect subclass must be fully qualified.")
+    val connections =
+      currentOptions.dialectConnections.getOrPut(dialectKey) { DialectConnections() }
+    val result = connections.action()
+
+    storedOptions = adapter.toJson(currentOptions)
+    return result
+  }
+
+  fun selectedOption() = withCurrentDialect { selectedOption }
+  fun selectOption(key: String) = withCurrentDialect { selectedOption = key }
+  fun unselectOption() = withCurrentDialect { selectedOption = null }
+  fun removeOption(key: String) = withCurrentDialect {
+    if (selectedOption == key) selectedOption = null
+    orderedOptions.remove(key)
+    serializedProperties.remove(key)
+  }
+
+  fun getKeys(): Collection<String> = withCurrentDialect { orderedOptions }
+
+  fun reorderKeys(keys: List<String>) = withCurrentDialect {
+    orderedOptions = keys.toMutableList()
+  }
+
+  fun addOption(properties: ConnectionProperties) = withCurrentDialect {
+    serializedProperties[properties.key] = properties.serializedProperties
+    orderedOptions.add(properties.key)
     selectedOption = properties.key
   }
 
-  fun unselectOption() {
-    selectedOption = ""
-  }
-
-  fun getKeys(): Collection<String> {
-    return adapter.fromJson(options.ifEmpty { return emptyList() })!!.map.keys
-  }
-
-  fun selectedProperties(): ConnectionProperties {
-    val currentOptions = adapter.fromJson(options)!!
-    return ConnectionProperties(selectedOption, currentOptions.map[selectedOption]!!)
+  fun currentOption() = withCurrentDialect {
+    ConnectionProperties(selectedOption!!, serializedProperties[selectedOption]!!)
   }
 
   companion object {
@@ -37,6 +57,13 @@ internal class ConnectionOptions(val project: Project) {
 
   @JsonClass(generateAdapter = true)
   internal class StoredOptions(
-    val map: MutableMap<String, String> = linkedMapOf()
+    val dialectConnections: MutableMap<String, DialectConnections> = mutableMapOf(),
+  )
+
+  @JsonClass(generateAdapter = true)
+  internal class DialectConnections(
+    var selectedOption: String? = null,
+    var orderedOptions: MutableList<String> = ArrayList(),
+    val serializedProperties: MutableMap<String, String> = mutableMapOf()
   )
 }
