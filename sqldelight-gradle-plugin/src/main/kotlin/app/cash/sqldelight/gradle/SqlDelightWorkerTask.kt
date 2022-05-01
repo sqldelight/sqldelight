8097b5a0db2cc6155acd3b4fb1fd0d9a4c9ab1d2
Matthew Haughton <3flex@users.noreply.github.com>
2022-05-01 15:31:58 +1000

    Remove redundant InputFiles annotation
    It's only necessary to add InputFiles if the Classpath annotation is used
    to maintain compatibility with Gradle versions older than 3.2.

--- sqldelight-gradle-plugin/src/main/kotlin/app/cash/sqldelight/gradle/SqlDelightWorkerTask.kt
+++ sqldelight-gradle-plugin/src/main/kotlin/app/cash/sqldelight/gradle/SqlDelightWorkerTask.kt
@@ -2,7 +2,6 @@ package app.cash.sqldelight.gradle
 
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.tasks.Classpath
-import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.SourceTask
 import org.gradle.workers.WorkQueue
 import org.gradle.workers.WorkerExecutor
@@ -17,7 +16,7 @@ abstract class SqlDelightWorkerTask : SourceTask() {
   @get:Inject
   internal abstract val workerExecutor: WorkerExecutor
 
-  @get:[InputFiles Classpath]
+  @get:Classpath
   val classpath: ConfigurableFileCollection = project.objects.fileCollection()
 
   internal fun workQueue(): WorkQueue =
