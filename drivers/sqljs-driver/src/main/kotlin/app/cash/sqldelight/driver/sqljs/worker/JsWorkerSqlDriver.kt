aa2faa5d2f86b2f11d6a90f353dfbcf08369c45f
Derek Ellis <derekellis170@gmail.com>
2022-05-06 12:03:52 -0700

    Finish SqlJs worker driver (#3202)
    Add transaction and query listener support to worker driver
    Clean up worker code and interfaces
    Add documentation for web workers
    Disable worker tests on node via gradle config

---
+++ drivers/sqljs-driver/src/main/kotlin/app/cash/sqldelight/driver/sqljs/worker/JsWorkerSqlDriver.kt
@@ -0,0 +1,226 @@
+package app.cash.sqldelight.driver.sqljs.worker
+
+import app.cash.sqldelight.async.AsyncQuery
+import app.cash.sqldelight.async.AsyncTransacter
+import app.cash.sqldelight.async.db.AsyncSqlCursor
+import app.cash.sqldelight.async.db.AsyncSqlDriver
+import app.cash.sqldelight.async.db.AsyncSqlPreparedStatement
+import app.cash.sqldelight.driver.sqljs.QueryResults
+import kotlinx.coroutines.await
+import kotlinx.coroutines.suspendCancellableCoroutine
+import org.khronos.webgl.Int8Array
+import org.khronos.webgl.Uint8Array
+import org.w3c.dom.MessageEvent
+import org.w3c.dom.Worker
+import org.w3c.dom.events.Event
+import org.w3c.dom.events.EventListener
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.js.Promise
+
+private fun <T> jsObject(block: T.() -> Unit): T {
+  val o = js("{}").unsafeCast<T>()
+  block(o)
+  return o
+}
+
+suspend fun initAsyncSqlDriver(workerPath: String = "/worker.sql-wasm.js", schema: AsyncSqlDriver.Schema? = null): AsyncSqlDriver {
+  val worker = Worker(workerPath)
+  return Promise<AsyncSqlDriver> { resolve, _ -> resolve(JsWorkerSqlDriver(worker)) }.withSchema(schema)
+}
+
+suspend fun Promise<AsyncSqlDriver>.withSchema(schema: AsyncSqlDriver.Schema? = null): AsyncSqlDriver = await().also { schema?.create(it) }
+
+class JsWorkerSqlDriver(private val worker: Worker) : AsyncSqlDriver {
+  private val listeners = mutableMapOf<String, MutableSet<AsyncQuery.Listener>>()
+  private var messageCounter = 0
+  private var transaction: AsyncTransacter.Transaction? = null
+
+  override suspend fun <R> executeQuery(identifier: Int?, sql: String, mapper: (AsyncSqlCursor) -> R, parameters: Int, binders: (AsyncSqlPreparedStatement.() -> Unit)?): R {
+    val bound = JsWorkerSqlPreparedStatement()
+    binders?.invoke(bound)
+
+    val messageId = messageCounter++
+    val message = jsObject<WorkerMessage> {
+      this.id = messageId
+      this.action = "exec"
+      this.sql = sql
+      this.params = bound.parameters.toTypedArray()
+    }
+
+    val data = worker.sendMessage(messageId, message).unsafeCast<WorkerData>()
+    if (data.error != null) {
+      throw data.error as Throwable
+    }
+
+    val table = if (data.results.isNotEmpty()) {
+      data.results[0]
+    } else {
+      jsObject { values = arrayOf() }
+    }
+
+    @Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE") val cursor = JsWorkerSqlCursor(table)
+    return mapper(cursor)
+  }
+
+  override suspend fun execute(identifier: Int?, sql: String, parameters: Int, binders: (AsyncSqlPreparedStatement.() -> Unit)?): Long {
+    val bound = JsWorkerSqlPreparedStatement()
+    binders?.invoke(bound)
+
+    val messageId = messageCounter++
+    val message = jsObject<WorkerMessage> {
+      this.id = messageId
+      this.action = "exec"
+      this.sql = sql
+      this.params = bound.parameters.toTypedArray()
+    }
+
+    val data = worker.sendMessage(messageId, message)
+    if (data.error != null) {
+      throw data.error as Throwable
+    }
+    return 0
+  }
+
+  override fun addListener(listener: AsyncQuery.Listener, queryKeys: Array<String>) {
+    queryKeys.forEach {
+      listeners.getOrPut(it) { mutableSetOf() }.add(listener)
+    }
+  }
+
+  override fun removeListener(listener: AsyncQuery.Listener, queryKeys: Array<String>) {
+    queryKeys.forEach {
+      listeners[it]?.remove(listener)
+    }
+  }
+
+  override fun notifyListeners(queryKeys: Array<String>) {
+    queryKeys.flatMap { listeners[it].orEmpty() }
+      .distinct()
+      .forEach(AsyncQuery.Listener::queryResultsChanged)
+  }
+
+  override suspend fun close() = worker.terminate()
+
+  override suspend fun newTransaction(): AsyncTransacter.Transaction {
+    val enclosing = transaction
+    val transaction = Transaction(enclosing)
+    this.transaction = transaction
+    if (enclosing == null) {
+      worker.run("BEGIN TRANSACTION")
+    }
+
+    return transaction
+  }
+
+  override fun currentTransaction(): AsyncTransacter.Transaction? = transaction
+
+  private inner class Transaction(
+    override val enclosingTransaction: AsyncTransacter.Transaction?
+  ) : AsyncTransacter.Transaction() {
+    override suspend fun endTransaction(successful: Boolean) {
+      if (enclosingTransaction == null) {
+        if (successful) {
+          worker.run("END TRANSACTION")
+        } else {
+          worker.run("ROLLBACK TRANSACTION")
+        }
+      }
+      transaction = enclosingTransaction
+    }
+  }
+
+  private suspend fun Worker.sendMessage(id: Int, message: dynamic): dynamic = suspendCancellableCoroutine { continuation ->
+    postMessage(message)
+
+    val messageListener = object : EventListener {
+      override fun handleEvent(event: Event) {
+        check(event is MessageEvent)
+
+        if (event.data.asDynamic().id == id) {
+          continuation.resume(event.data)
+          removeEventListener("message", this)
+        }
+      }
+    }
+
+    val errorListener = object : EventListener {
+      override fun handleEvent(event: Event) {
+        worker.removeEventListener("error", this)
+        continuation.resumeWithException(event.asDynamic().error as Throwable)
+      }
+    }
+
+    addEventListener("message", messageListener)
+    addEventListener("error", errorListener)
+
+    continuation.invokeOnCancellation {
+      removeEventListener("message", messageListener)
+      removeEventListener("error", errorListener)
+    }
+  }
+
+  private suspend fun Worker.run(sql: String) {
+    val messageId = messageCounter++
+    val message = jsObject<WorkerMessage> {
+      this.id = messageId
+      this.action = "exec"
+      this.sql = sql
+    }
+
+    sendMessage(messageId, message)
+  }
+}
+
+class JsWorkerSqlCursor(private val table: QueryResults) : AsyncSqlCursor {
+  private var currentRow = -1
+
+  override fun next(): Boolean = ++currentRow < table.values.size
+
+  override fun getString(index: Int): String? = table.values[currentRow][index]
+
+  override fun getLong(index: Int): Long? = (table.values[currentRow][index] as? Double)?.toLong()
+
+  override fun getBytes(index: Int): ByteArray? = (table.values[currentRow][index] as? Uint8Array)?.let { Int8Array(it.buffer).unsafeCast<ByteArray>() }
+
+  override fun getDouble(index: Int): Double? = table.values[currentRow][index]
+
+  override fun getBoolean(index: Int): Boolean? {
+    val double = (table.values[currentRow][index] as? Double)
+    return if (double == null) null
+    else double.toLong() == 1L
+  }
+}
+
+internal class JsWorkerSqlPreparedStatement : AsyncSqlPreparedStatement {
+
+  val parameters = mutableListOf<Any?>()
+
+  override fun bindBytes(index: Int, bytes: ByteArray?) {
+    parameters.add(bytes?.toTypedArray())
+  }
+
+  override fun bindLong(index: Int, long: Long?) {
+    // We convert Long to Double because Kotlin's Double is mapped to JS number
+    // whereas Kotlin's Long is implemented as a JS object
+    parameters.add(long?.toDouble())
+  }
+
+  override fun bindDouble(index: Int, double: Double?) {
+    parameters.add(double)
+  }
+
+  override fun bindString(index: Int, string: String?) {
+    parameters.add(string)
+  }
+
+  override fun bindBoolean(index: Int, boolean: Boolean?) {
+    parameters.add(
+      when (boolean) {
+        null -> null
+        true -> 1.0
+        false -> 0.0
+      }
+    )
+  }
+}
---
+++ drivers/sqljs-driver/src/test/kotlin/com/squareup/sqldelight/drivers/sqljs/JsWorkerTransacterTest.kt
@@ -0,0 +1,187 @@
+package com.squareup.sqldelight.drivers.sqljs
+
+import app.cash.sqldelight.async.AsyncTransacter
+import app.cash.sqldelight.async.AsyncTransacterImpl
+import app.cash.sqldelight.async.db.AsyncSqlDriver
+import app.cash.sqldelight.driver.sqljs.worker.initAsyncSqlDriver
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
+import kotlin.test.fail
+
+class JsWorkerTransacterTest {
+  private val schema = object : AsyncSqlDriver.Schema {
+    override val version = 1
+    override suspend fun create(driver: AsyncSqlDriver) {}
+    override suspend fun migrate(
+      driver: AsyncSqlDriver,
+      oldVersion: Int,
+      newVersion: Int
+    ) {
+    }
+  }
+
+  private fun runTest(block: suspend (AsyncSqlDriver, AsyncTransacter) -> Unit) = kotlinx.coroutines.test.runTest {
+    val driver = initAsyncSqlDriver(schema = schema)
+    val transacter = object : AsyncTransacterImpl(driver) {}
+    block(driver, transacter)
+
+    driver.close()
+  }
+
+  @Test fun afterCommit_runs_after_transaction_commits() = runTest { _, transacter ->
+    var counter = 0
+    transacter.transaction {
+      afterCommit { counter++ }
+      assertEquals(0, counter)
+    }
+
+    assertEquals(1, counter)
+  }
+
+  @Test fun afterCommit_does_not_run_after_transaction_rollbacks() = runTest { _, transacter ->
+    var counter = 0
+    transacter.transaction {
+      afterCommit { counter++ }
+      assertEquals(0, counter)
+      rollback()
+    }
+
+    assertEquals(0, counter)
+  }
+
+  @Test fun afterCommit_runs_after_enclosing_transaction_commits() = runTest { _, transacter ->
+    var counter = 0
+    transacter.transaction {
+      afterCommit { counter++ }
+      assertEquals(0, counter)
+
+      transaction {
+        afterCommit { counter++ }
+        assertEquals(0, counter)
+      }
+
+      assertEquals(0, counter)
+    }
+
+    assertEquals(2, counter)
+  }
+
+  @Test fun afterCommit_does_not_run_in_nested_transaction_when_enclosing_rolls_back() = runTest { _, transacter ->
+    var counter = 0
+    transacter.transaction {
+      afterCommit { counter++ }
+      assertEquals(0, counter)
+
+      transaction {
+        afterCommit { counter++ }
+      }
+
+      rollback()
+    }
+
+    assertEquals(0, counter)
+  }
+
+  @Test fun afterCommit_does_not_run_in_nested_transaction_when_nested_rolls_back() = runTest { _, transacter ->
+    var counter = 0
+    transacter.transaction {
+      afterCommit { counter++ }
+      assertEquals(0, counter)
+
+      transaction {
+        afterCommit { counter++ }
+        rollback()
+      }
+
+      throw AssertionError()
+    }
+
+    assertEquals(0, counter)
+  }
+
+  @Test fun afterRollback_no_ops_if_the_transaction_never_rolls_back() = runTest { _, transacter ->
+    var counter = 0
+    transacter.transaction {
+      afterRollback { counter++ }
+    }
+
+    assertEquals(0, counter)
+  }
+
+  @Test fun afterRollback_runs_after_a_rollback_occurs() = runTest { _, transacter ->
+    var counter = 0
+    transacter.transaction {
+      afterRollback { counter++ }
+      rollback()
+    }
+
+    assertEquals(1, counter)
+  }
+
+  @Test fun afterRollback_runs_after_an_inner_transaction_rolls_back() = runTest { _, transacter ->
+    var counter = 0
+    transacter.transaction {
+      afterRollback { counter++ }
+      transaction {
+        rollback()
+      }
+      throw AssertionError()
+    }
+
+    assertEquals(1, counter)
+  }
+
+  @Test fun afterRollback_runs_in_an_inner_transaction_when_the_outer_transaction_rolls_back() = runTest { _, transacter ->
+    var counter = 0
+    transacter.transaction {
+      transaction {
+        afterRollback { counter++ }
+      }
+      rollback()
+    }
+
+    assertEquals(1, counter)
+  }
+
+  @Test fun transactions_close_themselves_out_properly() = runTest { _, transacter ->
+    var counter = 0
+    transacter.transaction {
+      afterCommit { counter++ }
+    }
+
+    transacter.transaction {
+      afterCommit { counter++ }
+    }
+
+    assertEquals(2, counter)
+  }
+
+  @Test fun setting_no_enclosing_fails_if_there_is_a_currently_running_transaction() = runTest { _, transacter ->
+    transacter.transaction(noEnclosing = true) {
+      assertFailsWith<IllegalStateException> {
+        transacter.transaction(noEnclosing = true) {
+          throw AssertionError()
+        }
+      }
+    }
+  }
+
+  @Test fun an_exception_thrown_in_postRollback_function_is_combined_with_the_exception_in_the_main_body() = runTest { _, transacter ->
+    class ExceptionA : RuntimeException()
+    class ExceptionB : RuntimeException()
+    try {
+      transacter.transaction {
+        afterRollback {
+          throw ExceptionA()
+        }
+        throw ExceptionB()
+      }
+      fail("Should have thrown!")
+    } catch (e: Throwable) {
+      assertTrue("Exception thrown in body not in message($e)") { e.toString().contains("ExceptionA") }
+      assertTrue("Exception thrown in rollback not in message($e)") { e.toString().contains("ExceptionB") }
+    }
+  }
+}
