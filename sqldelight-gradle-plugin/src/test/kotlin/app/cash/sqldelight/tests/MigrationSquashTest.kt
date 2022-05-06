a0f17deb84574a9262c8ed1e7c4eb89aafd73c1a
Alec Strong <astrong@squareup.com>
2022-05-06 11:40:58 -0400

    Implement migration squashing for postgres

--- sqldelight-gradle-plugin/src/test/kotlin/app/cash/sqldelight/tests/MigrationSquashTest.kt
+++ sqldelight-gradle-plugin/src/test/kotlin/app/cash/sqldelight/tests/MigrationSquashTest.kt
@@ -3,22 +3,28 @@ package app.cash.sqldelight.tests
 import app.cash.sqldelight.withCommonConfiguration
 import com.google.common.truth.Truth.assertThat
 import com.google.common.truth.Truth.assertWithMessage
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import org.gradle.testkit.runner.GradleRunner
 import org.junit.Test
+import org.junit.runner.RunWith
 import java.io.File
 
+@RunWith(TestParameterInjector::class)
 class MigrationSquashTest {
-  @Test fun `squash mysql migrations`() {
+  @Test fun `squash migrations`(
+    @TestParameter("mysql", "postgres") dialect: String
+  ) {
     val output = GradleRunner.create()
       .withCommonConfiguration(File("src/test/migration-squash"))
-      .withArguments("clean", "squashMainMyDatabaseMigrations", "--stacktrace")
+      .withArguments("clean", "squashMain${dialect}DatabaseMigrations", "--stacktrace")
       .withDebug(true)
       .build()
 
     assertThat(output.output).contains("BUILD SUCCESSFUL")
 
-    File("src/test/migration-squash/expected").listFiles()!!.forEach { expected ->
-      val actual = File("src/test/migration-squash/src/main/sqldelight/migrations/${expected.name}")
+    File("src/test/migration-squash/expected/$dialect").listFiles()!!.forEach { expected ->
+      val actual = File("src/test/migration-squash/src/main/sqldelight/$dialect/migrations/${expected.name}")
       assertWithMessage("Expected ${expected.name} to be generated").that(actual.exists()).isTrue()
       try {
         assertWithMessage(
