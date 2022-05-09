de59ac3f5fb9db555b90f479eb690bbc7f922190
Alec Strong <astrong@squareup.com>
2022-05-09 13:12:20 -0400

    Use AGP namespace attribute
    (cherry picked from commit 1a004ace0d84e1441f3896ea03544f25beebf11f)

--- sqldelight-gradle-plugin/src/main/kotlin/com/squareup/sqldelight/gradle/android/PackageName.kt
+++ sqldelight-gradle-plugin/src/main/kotlin/com/squareup/sqldelight/gradle/android/PackageName.kt
@@ -1,18 +1,28 @@
 package com.squareup.sqldelight.gradle.android
 
 import com.android.build.gradle.BaseExtension
-import com.android.ide.common.symbols.getPackageNameFromManifest
+import org.gradle.api.GradleException
 import org.gradle.api.Project
 
 internal fun Project.packageName(): String {
   val androidExtension = extensions.getByType(BaseExtension::class.java)
-  androidExtension.sourceSets
-    .map { it.manifest.srcFile }
-    .filter { it.exists() }
-    .forEach {
-      return getPackageNameFromManifest(it)
-    }
-  throw IllegalStateException("No source sets available")
+  return androidExtension.namespace ?: throw GradleException(
+    """
+    |SqlDelight requires a package name to be set. This can be done via the android namespace:
+    |
+    |android {
+    |  namespace "com.example.mypackage"
+    |}
+    |
+    |or the sqldelight configuration:
+    |
+    |sqldelight {
+    |  MyDatabase {
+    |    packageName = "com.example.mypackage"
+    |  }
+    |}
+  """.trimMargin()
+  )
 }
 
 internal fun Project.sqliteVersion(): String? {
