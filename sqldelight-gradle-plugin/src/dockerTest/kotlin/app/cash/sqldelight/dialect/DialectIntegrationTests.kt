d21d2ee310a9e96f5227f10d4920eafa27806914
Matthew Haughton <3flex@users.noreply.github.com>
2022-05-20 21:12:10 +1000

    Put tests using Docker behind a flag (#3193)
    * Move tests requiring Docker to a separate source set
    
    Exclude these tests during execution by passing `-x dockerTest` to the
    ./gradlew command.
    
    * Disable dockerTest task when running instrumentation tests
    
    When -PInstrumentation is passed to Gradle, only instrumentation tests
    should execute.

---
+++ sqldelight-gradle-plugin/src/dockerTest/kotlin/app/cash/sqldelight/dialect/DialectIntegrationTests.kt
@@ -0,0 +1,46 @@
+package app.cash.sqldelight.dialect
+
+import com.google.common.truth.Truth
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Test
+import java.io.File
+
+class DialectIntegrationTests {
+
+  @Test fun integrationTestsMySql() {
+    val runner = GradleRunner.create()
+      .withCommonConfiguration(File("src/test/integration-mysql"))
+      .withArguments("clean", "check", "--stacktrace")
+
+    val result = runner.build()
+    Truth.assertThat(result.output).contains("BUILD SUCCESSFUL")
+  }
+
+  @Test fun integrationTestsMySqlSchemaDefinitions() {
+    val runner = GradleRunner.create()
+      .withCommonConfiguration(File("src/test/integration-mysql-schema"))
+      .withArguments("clean", "check", "--stacktrace")
+
+    val result = runner.build()
+    Truth.assertThat(result.output).contains("BUILD SUCCESSFUL")
+  }
+
+  @Test fun integrationTestsPostgreSql() {
+    val runner = GradleRunner.create()
+      .withCommonConfiguration(File("src/test/integration-postgresql"))
+      .withArguments("clean", "check", "--stacktrace")
+
+    val result = runner.build()
+    Truth.assertThat(result.output).contains("BUILD SUCCESSFUL")
+  }
+}
+
+private fun GradleRunner.withCommonConfiguration(projectRoot: File): GradleRunner {
+  File(projectRoot, "gradle.properties").writeText(
+    """
+      |org.gradle.jvmargs=-Xmx4g -XX:MaxMetaspaceSize=1g
+      |
+    """.trimMargin()
+  )
+  return withProjectDir(projectRoot)
+}
