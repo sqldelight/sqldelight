12e343bd31597e29fd24a6332974003d2a39923f
hfhbd <hfhbd@users.noreply.github.com>
2022-06-12 02:24:11 +0200

    Postgres: Fix copy with same name in multiple tables

--- dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/grammar/mixins/CopyMixin.kt
+++ dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/grammar/mixins/CopyMixin.kt
@@ -3,14 +3,13 @@ package app.cash.sqldelight.dialects.postgresql.grammar.mixins
 import app.cash.sqldelight.dialects.postgresql.grammar.psi.PostgreSqlCopyStdin
 import com.alecstrong.sql.psi.core.psi.QueryElement.QueryResult
 import com.alecstrong.sql.psi.core.psi.SqlCompositeElementImpl
+import com.alecstrong.sql.psi.core.psi.SqlTableName
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 
 internal abstract class CopyMixin(node: ASTNode) : SqlCompositeElementImpl(node), PostgreSqlCopyStdin {
   override fun queryAvailable(child: PsiElement): Collection<QueryResult> {
-    val tables = tablesAvailable(child)
-    return tables.map {
-      it.query
-    }
+    val tableName = child.parent.children.filterIsInstance<SqlTableName>().single()
+    return tableAvailable(child, tableName.name)
   }
 }
