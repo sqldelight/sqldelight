8e8a4fda2d4c85c979f195a27d98680c3b824683
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-06-26 14:29:08 +0200

    Add reproducer for #3338

--- sqldelight-gradle-plugin/src/test/custom-dialect/dialect/src/main/kotlin/foo/FooDialect.kt
+++ sqldelight-gradle-plugin/src/test/custom-dialect/dialect/src/main/kotlin/foo/FooDialect.kt
@@ -1,13 +1,18 @@
 package foo
 
+import app.cash.sqldelight.dialect.api.DialectType
 import app.cash.sqldelight.dialect.api.IntermediateType
-import app.cash.sqldelight.dialect.api.PrimitiveType
 import app.cash.sqldelight.dialect.api.SqlDelightDialect
 import app.cash.sqldelight.dialect.api.TypeResolver
 import app.cash.sqldelight.dialect.api.encapsulatingType
 import app.cash.sqldelight.dialects.sqlite_3_18.SqliteDialect
 import app.cash.sqldelight.dialects.sqlite_3_18.SqliteTypeResolver
 import com.alecstrong.sql.psi.core.psi.SqlFunctionExpr
+import com.alecstrong.sql.psi.core.psi.SqlTypeName
+import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.CodeBlock
+import com.squareup.kotlinpoet.MemberName
+import com.squareup.kotlinpoet.TypeName
 
 class FooDialect : SqlDelightDialect by SqliteDialect() {
   override fun typeResolver(parentResolver: TypeResolver) = CustomResolver(parentResolver)
@@ -15,9 +20,30 @@ class FooDialect : SqlDelightDialect by SqliteDialect() {
   class CustomResolver(private val parentResolver: TypeResolver) : TypeResolver by SqliteTypeResolver(parentResolver) {
     override fun functionType(functionExpr: SqlFunctionExpr): IntermediateType? {
       return when (functionExpr.functionName.text.lowercase()) {
-        "foo" -> encapsulatingType(functionExpr.exprList, PrimitiveType.INTEGER)
+        "foo" -> encapsulatingType(functionExpr.exprList, ExtensionType)
         else -> parentResolver.functionType(functionExpr)
       }
+    }
+
+    override fun definitionType(typeName: SqlTypeName): IntermediateType {
+      return when (typeName) { else -> IntermediateType(ExtensionType) }
+    }
+  }
+
+  private object ExtensionType : DialectType {
+    override val javaType: TypeName = ClassName("kotlin.time", "Duration")
+    override fun cursorGetter(columnIndex: Int, cursorName: String): CodeBlock {
+      return CodeBlock.builder()
+        .add(
+          "$cursorName.getLong($columnIndex)?.%M(%M)",
+          MemberName("kotlin.time", "toDuration", isExtension = true),
+          MemberName("kotlin.time.DurationUnit", "SECONDS")
+        )
+        .build()
+    }
+
+    override fun prepareStatementBinder(columnIndex: String, value: CodeBlock): CodeBlock {
+      return CodeBlock.of("""TODO("Not yet implemented")""")
     }
   }
 }
