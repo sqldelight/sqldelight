c9e4b24ce93bb37fe724c10eab73d896d5313ac5
Veyndan Stuart <veyndan@gmail.com>
2022-07-25 11:37:55 +0100

    Use QueryPagingSource as the invalidater

--- extensions/android-paging3/src/main/java/app/cash/sqldelight/paging3/LimitOffsetPagingSource.kt
+++ extensions/android-paging3/src/main/java/app/cash/sqldelight/paging3/LimitOffsetPagingSource.kt
@@ -3,11 +3,9 @@ package app.cash.sqldelight.paging3
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import androidx.room.RoomDatabase
 import app.cash.sqldelight.Query
 import app.cash.sqldelight.paging3.util.INITIAL_ITEM_COUNT
 import app.cash.sqldelight.paging3.util.INVALID
-import app.cash.sqldelight.paging3.util.ThreadSafeInvalidationObserver
 import app.cash.sqldelight.paging3.util.getClippedRefreshKey
 import app.cash.sqldelight.Transacter
 import app.cash.sqldelight.paging3.util.getLimit
@@ -20,28 +18,19 @@ import kotlin.coroutines.CoroutineContext
  * An implementation of [PagingSource] to perform a LIMIT OFFSET query
  *
  * This class is used for Paging3 to perform Query and RawQuery in Room to return a PagingSource
- * for Pager's consumption. Registers observers on tables lazily and automatically invalidates
- * itself when data changes.
+ * for Pager's consumption.
  */
-abstract class LimitOffsetPagingSource<RowType : Any>(
+internal class LimitOffsetPagingSource<RowType : Any>(
   private val queryProvider: (limit: Int, offset: Int) -> Query<RowType>,
   private val countQuery: Query<Int>,
   private val transacter: Transacter,
   private val context: CoroutineContext,
-  private val db: RoomDatabase,
-  vararg tables: String,
-) : PagingSource<Int, RowType>() {
+) : QueryPagingSource<Int, RowType>() {
 
   internal val itemCount: AtomicInteger = AtomicInteger(INITIAL_ITEM_COUNT)
 
-  private val observer = ThreadSafeInvalidationObserver(
-    tables = tables,
-    onInvalidated = ::invalidate
-  )
-
   override suspend fun load(params: LoadParams<Int>): LoadResult<Int, RowType> {
     return withContext(context) {
-      observer.registerIfNecessary(db)
       val tempCount = itemCount.get()
       // if itemCount is < 0, then it is initial load
       if (tempCount == INITIAL_ITEM_COUNT) {
@@ -81,9 +70,6 @@ abstract class LimitOffsetPagingSource<RowType : Any>(
       params = params,
       itemCount = tempCount,
     )
-    // manually check if database has been updated. If so, the observer's
-    // invalidation callback will invalidate this paging source
-    db.invalidationTracker.refreshVersionsSync()
     @Suppress("UNCHECKED_CAST")
     return if (invalid) INVALID as LoadResult.Invalid<Int, RowType> else loadResult
   }
@@ -114,6 +100,7 @@ abstract class LimitOffsetPagingSource<RowType : Any>(
     val limit: Int = getLimit(params, key)
     val offset: Int = getOffset(params, key, itemCount)
     val data = queryProvider(limit, offset)
+      .also { currentQuery = it }
       .executeAsList()
     val nextPosToLoad = offset + data.size
     val nextKey =
