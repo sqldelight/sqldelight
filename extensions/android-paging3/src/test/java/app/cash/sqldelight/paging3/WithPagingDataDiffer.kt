03744d053d8946109320dfad0686ac50f77c0c2b
Veyndan Stuart <veyndan@gmail.com>
2022-07-28 18:11:23 +0200

    Update extensions/android-paging3/src/test/java/app/cash/sqldelight/paging3/WithPagingDataDiffer.kt
    Co-authored-by: Niklas Baudy <niklas.baudy@vanniktech.de>

--- extensions/android-paging3/src/test/java/app/cash/sqldelight/paging3/WithPagingDataDiffer.kt
+++ extensions/android-paging3/src/test/java/app/cash/sqldelight/paging3/WithPagingDataDiffer.kt
@@ -27,7 +27,7 @@ import kotlinx.coroutines.test.advanceUntilIdle
 
 private object NoopListCallback : ListUpdateCallback {
   override fun onChanged(position: Int, count: Int, payload: Any?) = Unit
-  override fun onMoved(fromPosition: Int, toPosition: Int) {}
+  override fun onMoved(fromPosition: Int, toPosition: Int) = Unit
   override fun onInserted(position: Int, count: Int) {}
   override fun onRemoved(position: Int, count: Int) {}
 }
