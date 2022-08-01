0f4e98747cb2aca4e8de64266864a14f08d14cdb
RobX <robxyy@gmail.com>
2022-08-01 22:25:28 +0800

    Fixes

--- dialects/mysql/src/main/kotlin/app/cash/sqldelight/dialects/mysql/MySqlType.kt
+++ dialects/mysql/src/main/kotlin/app/cash/sqldelight/dialects/mysql/MySqlType.kt
@@ -61,9 +61,10 @@ internal enum class MySqlType(override val javaType: TypeName) : DialectType {
     return CodeBlock.of(
       when (this) {
         TINY_INT, TINY_INT_BOOL, SMALL_INT, INTEGER, BIG_INT, BIT -> "$cursorName.getLong($columnIndex)"
-        DATE, TIME, DATETIME, TIMESTAMP -> "$cursorName.getObject<$javaType>($columnIndex)"
+        DATE, TIME, DATETIME, TIMESTAMP -> "$cursorName.getObject<%T>($columnIndex)"
         NUMERIC -> "$cursorName.getBigDecimal($columnIndex)"
       },
+      javaType,
     )
   }
 }
