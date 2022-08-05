3e065e92ddae792490bdb827f802995bd22d3a1b
hfhbd <hfhbd@users.noreply.github.com>
2022-08-05 22:59:21 +0200

    Infer the Kotlin type of bind parameter, if possible, or fail with a better error message

--- sqldelight-gradle-plugin/src/test/kotlin/app/cash/sqldelight/dialect/DialectIntegrationTests.kt
+++ sqldelight-gradle-plugin/src/test/kotlin/app/cash/sqldelight/dialect/DialectIntegrationTests.kt
@@ -53,7 +53,7 @@ class DialectIntegrationTests {
   @Test fun customFunctionDialect() {
     val runner = GradleRunner.create()
       .withCommonConfiguration(File("src/test/custom-dialect"))
-      .withArguments("clean", "assemble", "--stacktrace")
+      .withArguments("clean", "compileTestKotlin", "--stacktrace")
 
     val result = runner.build()
     Truth.assertThat(result.output).contains("BUILD SUCCESSFUL")
