73d26b07c70f5d7da79bf656f0787b044b18143d
Derek Ellis <derekellis@squareup.com>
2022-08-12 10:04:58 -0400

    Use better api to add dialect dependency
    Add integration test for version catalogs

--- sqldelight-gradle-plugin/src/main/kotlin/app/cash/sqldelight/gradle/SqlDelightDatabase.kt
+++ sqldelight-gradle-plugin/src/main/kotlin/app/cash/sqldelight/gradle/SqlDelightDatabase.kt
@@ -9,7 +9,6 @@ import app.cash.sqldelight.gradle.squash.MigrationSquashTask
 import groovy.lang.GroovyObject
 import org.gradle.api.GradleException
 import org.gradle.api.Project
-import org.gradle.api.artifacts.ExternalModuleDependencyBundle
 import org.gradle.api.internal.catalog.DelegatingProjectDependency
 import org.gradle.api.provider.Provider
 import java.io.File
@@ -42,17 +41,7 @@ class SqlDelightDatabase(
 
   fun dialect(dialect: Any) {
     if (addedDialect) throw IllegalStateException("Can only set a single dialect.")
-
-    val dep = if (dialect is Provider<*>) {
-      when (val dependency = dialect.get()) {
-        is ExternalModuleDependencyBundle -> throw IllegalArgumentException("Dialect can not be a dependency bundle.")
-        else -> dependency
-      }
-    } else {
-      dialect
-    }
-
-    configuration.dependencies.add(project.dependencies.create(dep))
+    project.dependencies.add(configuration.name, dialect)
     addedDialect = true
   }
 
