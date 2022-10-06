49f4edcc25218df92c4c99d9734d2588f1cff67d
hfhbd <hfhbd@users.noreply.github.com>
2022-10-06 15:18:19 +0200

    Make the Callback open again

--- drivers/android-driver/src/main/java/app/cash/sqldelight/driver/android/AndroidSqliteDriver.kt
+++ drivers/android-driver/src/main/java/app/cash/sqldelight/driver/android/AndroidSqliteDriver.kt
@@ -182,7 +182,7 @@ class AndroidSqliteDriver private constructor(
     return openHelper?.close() ?: database.close()
   }
 
-  class Callback(
+  open class Callback(
     private val schema: SqlSchema,
     vararg callbacks: AfterVersion,
   ) : SupportSQLiteOpenHelper.Callback(schema.version) {
