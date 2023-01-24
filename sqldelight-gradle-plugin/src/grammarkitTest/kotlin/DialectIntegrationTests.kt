e585d6adcb7f645bb4e2ae7d4b409c676c3fef2c
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-01-24 17:31:50 +0100

    Use toolchain feature (#3849)
    * Use toolchain feature
    
    * Use Jvm 17 in custom dialect test
    
    * Fix spotless
    
    * Move test with GrammarKit to GrammarKit tests
    
    * Execute tests
    
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

---
+++ sqldelight-gradle-plugin/src/grammarkitTest/kotlin/DialectIntegrationTests.kt
@@ -0,0 +1,38 @@
+package app.cash.sqldelight
+
+import com.google.common.truth.Truth
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Test
+import java.io.File
+
+class DialectIntegrationTests {
+  @Test
+  fun customFunctionDialect() {
+    val runner = GradleRunner.create()
+      .withCommonConfiguration(File("src/test/custom-dialect"))
+      .withArguments("clean", "compileTestKotlin", "--stacktrace")
+
+    val result = runner.build()
+    Truth.assertThat(result.output).contains("BUILD SUCCESSFUL")
+  }
+
+  @Test
+  fun `dialect accepts version catalog dependency`() {
+    val runner = GradleRunner.create()
+      .withCommonConfiguration(File("src/test/integration-catalog"))
+      .withArguments("clean", "compileTestKotlin", "--stacktrace")
+
+    val result = runner.build()
+    Truth.assertThat(result.output).contains("BUILD SUCCESSFUL")
+  }
+
+  private fun GradleRunner.withCommonConfiguration(projectRoot: File): GradleRunner {
+    File(projectRoot, "gradle.properties").writeText(
+      """
+      |org.gradle.jvmargs=-Xmx4g -XX:MaxMetaspaceSize=1g
+      |
+      """.trimMargin(),
+    )
+    return withProjectDir(projectRoot)
+  }
+}
