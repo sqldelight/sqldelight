5eeefb9f563f25f29f4a630ec2b33e9ff1e0273c
Dylan MacKenzie <ecstaticmorse@gmail.com>
2023-03-14 12:42:27 -0700

    Support SQLite's VIRTUAL and STORED options on GENERATED columns
    See https://www.sqlite.org/syntax/column-constraint.html for grammar.
    
    Also makes the prefix for generated columns match SQLite's grammar
    (`[GENERATED ALWAYS] AS ...`) instead of ANSI's (`[ GENERATED ] ALWAYS AS ...`)

--- dialects/sqlite-3-18/src/test/fixtures_sqlite_3_18/generated_columns/Test.s
+++ dialects/sqlite-3-18/src/test/fixtures_sqlite_3_18/generated_columns/Test.s
@@ -1,4 +1,7 @@
 CREATE TABLE test (
   id INTEGER,
+  id_stored INTEGER AS (id * 2) STORED,
+  id_virtual INTEGER AS (id * 2) VIRTUAL,
+  id_no_storage INTEGER AS (id * 2),
   id_bigger_than_ten INTEGER GENERATED ALWAYS AS (id > 10) NOT NULL
 );
