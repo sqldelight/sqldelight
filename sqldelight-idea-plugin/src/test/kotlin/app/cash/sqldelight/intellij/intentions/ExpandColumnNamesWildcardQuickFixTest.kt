5e9b72678a4734f326fa2452829a8cab4dcce754
Alexander Perfilyev <alexander.perfilyev@gmail.com>
2023-03-19 19:58:15 +0300

    Fix for expand wildcard intention to work with qualifier

--- sqldelight-idea-plugin/src/test/kotlin/app/cash/sqldelight/intellij/intentions/ExpandColumnNamesWildcardQuickFixTest.kt
+++ sqldelight-idea-plugin/src/test/kotlin/app/cash/sqldelight/intellij/intentions/ExpandColumnNamesWildcardQuickFixTest.kt
@@ -110,6 +110,57 @@ class ExpandColumnNamesWildcardQuickFixTest : SqlDelightFixtureTestCase() {
     )
   }
 
+  fun testIntentionAvailableOnQualifier() {
+    myFixture.configureByText(
+      SqlDelightFileType,
+      CREATE_TABLE + """
+      |select_all:
+      |SELECT t<caret>.*
+      |FROM test t;
+      """.trimMargin(),
+    )
+
+    val intention = ExpandColumnNamesWildcardQuickFix()
+    assertThat(myFixture.availableIntentions.firstOrNull { it.text == intention.text })
+      .isNotNull()
+  }
+
+  fun testExecuteIntentionWithQualifier() {
+    myFixture.configureByText(
+      SqlDelightFileType,
+      CREATE_TABLE + """
+        |
+        |CREATE TABLE test2 (
+        |  id INTEGER NOT NULL,
+        |  other_title TEXT NOT NULL
+        |);
+        |
+        |selectAll:
+        |SELECT t.<caret>*, test2.id
+        |FROM test t
+        |JOIN test2 ON t.title = test2.other_title;
+      """.trimMargin(),
+    )
+
+    val intention = ExpandColumnNamesWildcardQuickFix()
+    intention.invoke(myFixture.project, myFixture.editor, myFixture.file)
+
+    myFixture.checkResult(
+      CREATE_TABLE + """
+        |
+        |CREATE TABLE test2 (
+        |  id INTEGER NOT NULL,
+        |  other_title TEXT NOT NULL
+        |);
+        |
+        |selectAll:
+        |SELECT t._id, t.title, test2.id
+        |FROM test t
+        |JOIN test2 ON t.title = test2.other_title;
+      """.trimMargin(),
+    )
+  }
+
   companion object {
     private val CREATE_TABLE = """
       |CREATE TABLE test (
