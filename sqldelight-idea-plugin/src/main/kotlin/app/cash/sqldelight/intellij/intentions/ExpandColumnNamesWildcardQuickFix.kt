f04d699f2acb1328effb2f83739aefb60ae6e8b3
Alexander Perfilyev <alexander.perfilyev@gmail.com>
2023-03-20 00:49:16 +0300

    Fix for expand wildcard intention to work with qualifier (#3979)

--- sqldelight-idea-plugin/src/main/kotlin/app/cash/sqldelight/intellij/intentions/ExpandColumnNamesWildcardQuickFix.kt
+++ sqldelight-idea-plugin/src/main/kotlin/app/cash/sqldelight/intellij/intentions/ExpandColumnNamesWildcardQuickFix.kt
@@ -1,54 +1,59 @@
 package app.cash.sqldelight.intellij.intentions
 
-import app.cash.sqldelight.core.lang.SqlDelightFile
-import app.cash.sqldelight.intellij.util.findElementOfTypeAtOffset
 import com.alecstrong.sql.psi.core.psi.SqlResultColumn
 import com.alecstrong.sql.psi.core.psi.SqlSelectStmt
-import com.intellij.codeInsight.intention.impl.BaseIntentionAction
+import com.alecstrong.sql.psi.core.psi.SqlTableName
+import com.intellij.codeInsight.intention.BaseElementAtCaretIntentionAction
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.ReadOnlyModificationException
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
 import org.jetbrains.kotlin.psi.psiUtil.endOffset
 import org.jetbrains.kotlin.psi.psiUtil.startOffset
 
-class ExpandColumnNamesWildcardQuickFix : BaseIntentionAction() {
+class ExpandColumnNamesWildcardQuickFix : BaseElementAtCaretIntentionAction() {
 
   override fun getFamilyName() = INTENTIONS_FAMILY_NAME_REFACTORINGS
 
   override fun getText() = INTENTION_EXPAND_COLUMN_NAMES_TEXT
-
-  override fun isAvailable(project: Project, editor: Editor, file: PsiFile): Boolean {
-    val caret = editor.caretModel.offset
-    return selectStatementAtCaretWithColumnNamesWildcard(
-      file = file as? SqlDelightFile ?: return false,
-      caret = caret,
-    ) != null
+  override fun isAvailable(project: Project, editor: Editor?, element: PsiElement): Boolean {
+    val selectStmt = PsiTreeUtil.getParentOfType(element, SqlSelectStmt::class.java)
+      ?: return false
+    return selectStmt.resultColumnList.any { it.textContains('*') && it.expr == null }
   }
 
-  private fun selectStatementAtCaretWithColumnNamesWildcard(
-    file: SqlDelightFile,
-    caret: Int,
-  ): SqlSelectStmt? {
-    val selectStatement = file.findElementOfTypeAtOffset<SqlSelectStmt>(caret) ?: return null
-    val resultColumns = selectStatement.resultColumnList
-    return if (resultColumns.any { it.textMatches("*") }) selectStatement else null
-  }
+  override fun invoke(project: Project, editor: Editor, element: PsiElement) {
+    val selectStmt = PsiTreeUtil.getParentOfType(element, SqlSelectStmt::class.java) ?: return
 
-  override fun invoke(project: Project, editor: Editor, file: PsiFile) {
-    WriteCommandAction.writeCommandAction(project).run<ReadOnlyModificationException> {
-      val caret = editor.caretModel.offset
-      selectStatementAtCaretWithColumnNamesWildcard(file as SqlDelightFile, caret)?.run {
-        val allColumns = queryExposed()
-          .flatMap { it.columns }
-          .distinctBy { it.element.text }
-          .joinToString(separator = ", ") { it.element.text }
-        val (start, end) = resultColumnList.startEndOffset()
-        editor.document.replaceString(start, end, allColumns)
+    val allColumns = selectStmt.resultColumnList
+      .groupBy({ it.findTableName().orEmpty() }, { it.findColumns() })
+      .flatMap { (key: String, value: List<List<String>>) ->
+        value.flatten().distinct()
+          .map {
+            buildString {
+              if (key.isNotEmpty()) {
+                append(key)
+                append(".")
+              }
+              append(it)
+            }
+          }
       }
+      .joinToString()
+
+    val (start, end) = selectStmt.resultColumnList.startEndOffset()
+    WriteCommandAction.writeCommandAction(project).run<ReadOnlyModificationException> {
+      editor.document.replaceString(start, end, allColumns)
     }
   }
+
+  private fun SqlResultColumn.findTableName() =
+    PsiTreeUtil.findChildOfType(this, SqlTableName::class.java)?.name
+
+  private fun SqlResultColumn.findColumns(): List<String> =
+    queryExposed().flatMap { result -> result.columns.map { it.element.text } }
 
   private fun List<SqlResultColumn>.startEndOffset(): Pair<Int, Int> {
     var start = firstOrNull()?.startOffset ?: 0
