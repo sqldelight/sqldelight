717a17487614fcbf8d16410a1ce333c5b4ffce68
hfhbd <hfhbd@users.noreply.github.com>
2023-04-05 19:32:46 +0200

    Fix sample js tests

--- sample/common/src/jsTest/kotlin/com/example/sqldelight/hockey/BaseTest.kt
+++ sample/common/src/jsTest/kotlin/com/example/sqldelight/hockey/BaseTest.kt
@@ -3,15 +3,14 @@ package com.example.sqldelight.hockey
 import app.cash.sqldelight.driver.sqljs.initSqlDriver
 import com.example.sqldelight.hockey.data.Db
 import com.example.sqldelight.hockey.data.Schema
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.await
 
-actual suspend fun createDriver() = coroutineScope {
+actual suspend fun createDriver() {
   val driver = initSqlDriver(Schema).await()
   Db.dbSetup(driver)
 }
 
-actual suspend fun closeDriver() = coroutineScope {
+actual suspend fun closeDriver() {
   Db.dbClear()
 }
 
