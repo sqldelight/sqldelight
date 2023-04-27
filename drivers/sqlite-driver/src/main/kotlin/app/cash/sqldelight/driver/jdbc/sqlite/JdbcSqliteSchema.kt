75ce41ed85d364d9c297884cbd4da52140357f7b
morki <morki@morki.cz>
2023-04-27 12:18:22 +0200

    Make function easier to discover, similar to `AndroidSqliteDriver` and update user documentation

--- drivers/sqlite-driver/src/main/kotlin/app/cash/sqldelight/driver/jdbc/sqlite/JdbcSqliteSchema.kt
+++ drivers/sqlite-driver/src/main/kotlin/app/cash/sqldelight/driver/jdbc/sqlite/JdbcSqliteSchema.kt
@@ -1,33 +1,44 @@
 package app.cash.sqldelight.driver.jdbc.sqlite
 
 import app.cash.sqldelight.db.*
+import java.util.*
+
+/**
+ * Constructs [JdbcSqliteDriver] and creates or migrates [schema] from current PRAGMA `user_version`.
+ * After that updates PRAGMA `user_version` to migrated [schema] version.
+ * Each of the [callbacks] are executed during the migration whenever the upgrade to the version specified by
+ * [AfterVersion.afterVersion] has been completed.
+ *
+ * @see JdbcSqliteDriver
+ * @see SqlSchema.create
+ * @see SqlSchema.migrate
+ */
+fun JdbcSqliteDriver(
+    url: String,
+    properties: Properties = Properties(),
+    schema: SqlSchema<QueryResult.Value<Unit>>,
+    vararg callbacks: AfterVersion,
+): JdbcSqliteDriver {
+    val driver = JdbcSqliteDriver(url, properties)
+    val version = driver.getVersion()
+
+    if (version == 0) {
+        schema.create(driver).value
+    } else {
+        schema.migrate(driver, version, schema.version, *callbacks).value
+    }
+
+    driver.setVersion(schema.version)
+    return driver
+}
 
 private fun JdbcSqliteDriver.getVersion(): Int {
     val mapper = { cursor: SqlCursor ->
         QueryResult.Value(if (cursor.next().value) cursor.getLong(0)?.toInt() else null)
     }
-
     return executeQuery(null, "PRAGMA user_version", mapper, 0, null).value ?: 0
 }
 
 private fun JdbcSqliteDriver.setVersion(version: Int) {
     execute(null, "PRAGMA user_version = $version", 0, null).value
-}
-
-/**
- * Uses this driver to create or migrate schema from current PRAGMA `user_version`.
- * After that updates PRAGMA `user_version` to migrated [schema] version.
- * Each of the [callbacks] are executed during the migration whenever the upgrade to the version specified by
- * [AfterVersion.afterVersion] has been completed.
- */
-fun JdbcSqliteDriver.setupSchema(schema: SqlSchema<QueryResult.Value<Unit>>, vararg callbacks: AfterVersion) {
-    val version = getVersion()
-
-    if (version == 0) {
-        schema.create(this).value
-    } else {
-        schema.migrate(this, version, schema.version, *callbacks).value
-    }
-
-    setVersion(schema.version)
 }
