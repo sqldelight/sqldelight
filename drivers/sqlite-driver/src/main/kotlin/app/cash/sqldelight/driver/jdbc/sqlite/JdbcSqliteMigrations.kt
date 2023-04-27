e16aaa0a946b1e5a8bbba28d76830c6c8ee0c78d
morki <morki@morki.cz>
2023-04-27 11:14:29 +0200

    Use generic `SqlSchema` and apply suggestions

--- drivers/sqlite-driver/src/main/kotlin/app/cash/sqldelight/driver/jdbc/sqlite/JdbcSqliteMigrations.kt
+++ drivers/sqlite-driver/src/main/kotlin/app/cash/sqldelight/driver/jdbc/sqlite/JdbcSqliteMigrations.kt
@@ -1,23 +1,33 @@
 package app.cash.sqldelight.driver.jdbc.sqlite
 
-import app.cash.sqldelight.db.AfterVersion
-import app.cash.sqldelight.db.SqlSchema
-import app.cash.sqldelight.db.migrateWithCallbacks
+import app.cash.sqldelight.db.*
 
-fun JdbcSqliteDriver.getVersion(): Int {
-  return executeQuery(null, "PRAGMA user_version", { cursor -> if (cursor.next()) cursor.getLong(0)?.toInt() else null }, 0, null).value ?: 0
+private fun JdbcSqliteDriver.getVersion(): Int {
+    val mapper = { cursor: SqlCursor ->
+        QueryResult.Value(if (cursor.next().value) cursor.getLong(0)?.toInt() else null)
+    }
+
+    return executeQuery(null, "PRAGMA user_version", mapper, 0, null).value ?: 0
 }
 
-fun JdbcSqliteDriver.setVersion(version: Int) {
-  execute(null, "PRAGMA user_version = $version", 0, null)
+private fun JdbcSqliteDriver.setVersion(version: Int) {
+    execute(null, "PRAGMA user_version = $version", 0, null).value
 }
 
-fun JdbcSqliteDriver.createSchema(schema: SqlSchema) {
-  schema.create(this)
-  setVersion(schema.version)
-}
+/**
+ * Uses this driver to create or migrate schema from current `PRAGMA user_version`.
+ * Sets new `PRAGMA user_version` to migrated [schema] version
+ * Each of the [callbacks] are executed during the migration whenever the upgrade to the version specified by
+ * [AfterVersion.afterVersion] has been completed.
+ */
+fun JdbcSqliteDriver.setupSchema(schema: SqlSchema<QueryResult.Value<Unit>>, vararg callbacks: AfterVersion) {
+    val version = getVersion()
 
-fun JdbcSqliteDriver.migrateSchema(schema: SqlSchema, vararg callbacks: AfterVersion) {
-  schema.migrateWithCallbacks(this, getVersion(), schema.version, *callbacks)
-  setVersion(schema.version)
+    if (version == 0) {
+        schema.create(this).value
+    } else {
+        schema.migrate(this, version, schema.version, *callbacks).value
+    }
+
+    setVersion(schema.version)
 }
