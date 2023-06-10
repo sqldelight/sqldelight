e66926565c50252dd8499a0ba435d7917f410aaf
hfhbd <hfhbd@users.noreply.github.com>
2023-06-10 14:06:14 +0200

    Refactor build script using mpp targetHierarchy builder

--- buildLogic/multiplatform-convention/src/main/kotlin/app/cash/sqldelight/multiplatform/MultiplatformConventions.kt
+++ buildLogic/multiplatform-convention/src/main/kotlin/app/cash/sqldelight/multiplatform/MultiplatformConventions.kt
@@ -4,8 +4,6 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-import org.jetbrains.kotlin.gradle.targets.js.KotlinJsTarget
 import org.jetbrains.kotlin.konan.target.HostManager
 
 class MultiplatformConventions : Plugin<Project> {
@@ -13,57 +11,52 @@ class MultiplatformConventions : Plugin<Project> {
     project.plugins.apply("org.jetbrains.kotlin.multiplatform")
 
     (project.kotlinExtension as KotlinMultiplatformExtension).apply {
-      val nativeMain = sourceSets.create("nativeMain").apply {
-        dependsOn(sourceSets.getByName("commonMain"))
-      }
-      val nativeTest = sourceSets.create("nativeTest").apply {
-        dependsOn(sourceSets.getByName("commonTest"))
-      }
-
-      targets.whenObjectAdded {
-        if (it is KotlinNativeTarget) {
-          sourceSets.getByName("${it.name}Main").dependsOn(nativeMain)
-          sourceSets.getByName("${it.name}Test").dependsOn(nativeTest)
-        }
-
-        if (it is KotlinJsTarget) {
-          it.browser {
-            testTask {
-              useKarma {
-                useChromeHeadless()
-              }
-            }
-          }
-          it.compilations.all {
-            it.kotlinOptions.apply {
-              moduleKind = "umd"
-              sourceMap = true
-              sourceMapEmbedSources = null
-            }
-          }
-        }
-      }
+      targetHierarchy.default()
 
       jvm()
 
       js {
-        browser()
+        browser {
+          testTask {
+            useKarma {
+              useChromeHeadless()
+            }
+          }
+        }
+        compilations.configureEach {
+          it.kotlinOptions {
+            moduleKind = "umd"
+            sourceMap = true
+            sourceMapEmbedSources = null
+          }
+        }
       }
 
+      // tier 1
+      linuxX64()
+      macosX64()
+      macosArm64()
+      iosSimulatorArm64()
       iosX64()
-      iosArm64()
-      tvosX64()
-      tvosArm64()
+
+      // tier 2
+      // linuxArm64()
+      watchosSimulatorArm64()
       watchosX64()
       watchosArm32()
       watchosArm64()
-      macosX64()
-      mingwX64()
-      linuxX64()
-      macosArm64()
-      iosSimulatorArm64()
-      watchosSimulatorArm64()
       tvosSimulatorArm64()
+      tvosX64()
+      tvosArm64()
+      iosArm64()
+
+      // tier 3
+      // androidNativeArm32()
+      // androidNativeArm64()
+      // androidNativeX86()
+      // androidNativeX64()
+      mingwX64()
+      // watchosDeviceArm64()
 
       // linking fails for the linux test build if not built on a linux host
       // ensure the tests and linking for them is only done on linux hosts
