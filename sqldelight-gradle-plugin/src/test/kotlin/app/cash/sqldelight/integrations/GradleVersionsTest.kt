51dc26ed46c38da9524986b28a66a50f3e76a093
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-06-20 20:52:14 +0200

    Fix Gradle version test

--- sqldelight-gradle-plugin/src/test/kotlin/app/cash/sqldelight/integrations/GradleVersionsTest.kt
+++ sqldelight-gradle-plugin/src/test/kotlin/app/cash/sqldelight/integrations/GradleVersionsTest.kt
@@ -34,7 +34,7 @@ class GradleVersionsTest(private val gradleVersion: String) {
       // MIN_GRADLE_VERSION,
       // We use version catalogs in tests too but this feature is only stable since 7.4.
       // Test MIN_GRADLE_VERSION too if MIN_GRADLE_VERSION is higher than 7.4.
-      "7.4",
+      "7.4.2",
       "7.6.1",
       "8.0.2",
     )
