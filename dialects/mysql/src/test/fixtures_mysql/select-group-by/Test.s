sqldelight/sqldelight

commit ce827d11f551625b01b8c567a991c54e26b42157

Use Long as migration version (#4297)
* Use long as migration version

* Add integration test

* Bump to sql-psi 0.4.5

* Fix int usage

* Fix dialect test

* Fix dialect tests

* Fix schema version

* Fix schema version

* Fix tests

---------

Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>
Co-authored-by: Alec Kazakova <AlecStrong@users.noreply.github.com>

dialects/sqlite-3-18/src/test/kotlin/app/cash/sqldelight/dialects/sqlite_3_18/NullabilityTest.kt
@@ -1,119 +1,0 @@
-package app.cash.sqldelight.dialects.sqlite_3_18
-
-import com.alecstrong.sql.psi.core.SqlParserUtil
-import com.alecstrong.sql.psi.test.fixtures.compileFile
-import com.google.common.truth.Truth.assertThat
-import org.junit.After
-import org.junit.Before
-import org.junit.Test
-import java.io.File
-
-class NullabilityTest {
-  @Before
-  fun before() {
-    File("build/tmp").deleteRecursively()
-  }
-
-  @After
-  fun after() {
-    SqlParserUtil.reset()
-    File("build/tmp").deleteRecursively()
-  }
-
-  @Test fun outerJoin() {
-    SqliteDialect().setup()
-    val file = compileFile(
-      """
-      |CREATE TABLE car (
-      |  _id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-      |  id TEXT NOT NULL,
-      |  brand TEXT NOT NULL
-      |);
-      |
-      |CREATE TABLE owner (
-      |  _id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-      |  name TEXT NOT NULL,
-      |  carId TEXT
-      |);
-      |
-      |SELECT *
-      |FROM (SELECT owner.name, car.brand AS carBrand, car.rowid AS rowid
-      |      FROM owner
-      |      LEFT OUTER JOIN car ON owner.carId = car.id)
-      |WHERE carBrand = ?;
-      """.trimMargin(),
-    )
-
-    val select = file.sqlStmtList!!.stmtList.mapNotNull { it.compoundSelectStmt }.single()
-    val projection = select.queryExposed().flatMap { it.columns }
-
-    assertThat(projection[0].nullable).isNull()
-    assertThat(projection[1].nullable).isTrue()
-    assertThat(projection[2].nullable).isTrue()
-  }
-
-  @Test fun leftJoinGroupBy() {
-    SqliteDialect().setup()
-    val file = compileFile(
-      """
-      |CREATE TABLE target (
-      |  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-      |  coacheeId INTEGER NOT NULL,
-      |  name TEXT NOT NULL
-      |);
-      |
-      |CREATE TABLE challengeTarget (
-      |  targetId INTEGER NOT NULL,
-      |  challengeId INTEGER NOT NULL
-      |);
-      |
-      |CREATE TABLE challenge (
-      |  id INTEGER NOT NULL,
-      |  cancelledAt INTEGER,
-      |  emoji TEXT
-      |);
-      |
-      |SELECT target.id AS id, target.name AS name, challenge.emoji
-      |  FROM target
-      |  LEFT JOIN challengeTarget
-      |    ON challengeTarget.targetId = target.id
-      |  LEFT JOIN challenge
-      |    ON challengeTarget.challengeId = challenge.id AND challenge.cancelledAt IS NULL
-      |  WHERE target.coacheeId = ?
-      |  GROUP BY 1
-      |  ORDER BY target.name COLLATE NOCASE ASC
-      |;
-      """.trimMargin(),
-    )
-
-    val select = file.sqlStmtList!!.stmtList.mapNotNull { it.compoundSelectStmt }.single()
-    val projection = select.queryExposed().flatMap { it.columns }
-
-    assertThat(projection[0].nullable).isNull()
-    assertThat(projection[1].nullable).isNull()
-    assertThat(projection[2].nullable).isTrue()
-  }
-
-  @Test fun checkNotNull() {
-    SqliteDialect().setup()
-    val file = compileFile(
-      """
-      |CREATE TABLE target (
-      |  name TEXT,
-      |  thing TEXT
-      |);
-      |
-      |SELECT name, name AS poop, thing AS poop2
-      |FROM target
-      |WHERE name IS NOT NULL AND thing IS NOT NULL;
-      """.trimMargin(),
-    )
-
-    val select = file.sqlStmtList!!.stmtList.mapNotNull { it.compoundSelectStmt }.single()
-    val projection = select.queryExposed().flatMap { it.columns }
-
-    assertThat(projection[0].nullable).isFalse()
-    assertThat(projection[1].nullable).isFalse()
-    assertThat(projection[2].nullable).isFalse()
-  }
-}

dialects/sqlite-3-18/src/test/kotlin/app/cash/sqldelight/dialects/sqlite_3_18/Sqlite318FixturesTest.kt
@@ -12,6 +12,10 @@ class Sqlite318FixturesTest(name: String, fixtureRoot: File) : FixturesTest(name
     SqliteDialect().setup()
   }
 
+  override val replaceRules: Array<Pair<String, String>> = arrayOf(
+    " INT NOT NULL" to " INTEGER NOT NULL",
+  )
+
   companion object {
     private val fixtures = arrayOf("src/test/fixtures_upsert_not_supported", "src/test/fixtures_sqlite_3_18")
 

dialects/sqlite-3-24/src/test/kotlin/app/cash/sqldelight/dialects/sqlite_3_24/Sqlite324FixturesTest.kt
@@ -10,6 +10,7 @@ import java.io.File
 class Sqlite324FixturesTest(name: String, fixtureRoot: File) : FixturesTest(name, fixtureRoot) {
   override val replaceRules = arrayOf(
     "ORDER or WHERE expected" to "ORDER, WHERE or WINDOW expected",
+    " INT NOT NULL" to " INTEGER NOT NULL",
   )
 
   override fun setupDialect() {

dialects/sqlite-3-25/src/main/kotlin/app/cash/sqldelight/dialects/sqlite_3_25/grammar/sqlite.bnf
@@ -54,7 +54,7 @@ result_column ::= ( MULTIPLY
   implements = "com.alecstrong.sql.psi.core.psi.SqlResultColumn"
   override = true
 }
-select_stmt ::= SELECT [ DISTINCT | ALL ] {result_column} ( COMMA {result_column} ) * [ FROM {join_clause} ] [ WHERE <<expr '-1'>> ] [ GROUP BY <<expr '-1'>> ( COMMA <<expr '-1'>> ) * [ HAVING <<expr '-1'>> ] ] [ WINDOW window_name AS window_defn ( COMMA window_name AS window_defn ) * ] | VALUES {values_expression} ( COMMA {values_expression} ) * {
+select_stmt ::= SELECT [ DISTINCT | ALL ] {result_column} ( COMMA {result_column} ) * [ FROM {join_clause} ] [ WHERE <<expr '-1'>> ] [{group_by}] [ HAVING <<expr '-1'>> ] [ WINDOW window_name AS window_defn ( COMMA window_name AS window_defn ) * ] | VALUES {values_expression} ( COMMA {values_expression} ) * {
   extends = "com.alecstrong.sql.psi.core.psi.impl.SqlSelectStmtImpl"
   implements = "com.alecstrong.sql.psi.core.psi.SqlSelectStmt"
   override = true

dialects/sqlite-3-25/src/test/kotlin/app/cash/sqldelight/dialects/sqlite_3_25/Sqlite325FixturesTest.kt
@@ -10,6 +10,7 @@ import java.io.File
 class Sqlite325FixturesTest(name: String, fixtureRoot: File) : FixturesTest(name, fixtureRoot) {
   override val replaceRules = arrayOf(
     "ORDER or WHERE expected" to "ORDER, WHERE or WINDOW expected",
+    " INT NOT NULL" to " INTEGER NOT NULL",
   )
 
   override fun setupDialect() {

dialects/sqlite-3-30/src/test/kotlin/app/cash/sqldelight/dialects/sqlite_3_30/Sqlite330FixturesTest.kt
@@ -10,6 +10,7 @@ import java.io.File
 class Sqlite330FixturesTest(name: String, fixtureRoot: File) : FixturesTest(name, fixtureRoot) {
   override val replaceRules = arrayOf(
     "ORDER or WHERE expected" to "ORDER, WHERE or WINDOW expected",
+    " INT NOT NULL" to " INTEGER NOT NULL",
   )
 
   override fun setupDialect() {

dialects/sqlite-3-33/src/test/kotlin/app/cash/sqldelight/dialects/sqlite_3_33/Sqlite333FixturesTest.kt
@@ -10,6 +10,7 @@ import java.io.File
 class Sqlite333FixturesTest(name: String, fixtureRoot: File) : FixturesTest(name, fixtureRoot) {
   override val replaceRules = arrayOf(
     "ORDER or WHERE expected" to "ORDER, WHERE or WINDOW expected",
+    " INT NOT NULL" to " INTEGER NOT NULL",
   )
 
   override fun setupDialect() {

dialects/sqlite-3-35/src/test/kotlin/app/cash/sqldelight/dialects/sqlite_3_35/Sqlite335FixturesTest.kt
@@ -10,6 +10,7 @@ import java.io.File
 class Sqlite335FixturesTest(name: String, fixtureRoot: File) : FixturesTest(name, fixtureRoot) {
   override val replaceRules = arrayOf(
     "ORDER or WHERE expected" to "ORDER, WHERE or WINDOW expected",
+    " INT NOT NULL" to " INTEGER NOT NULL",
   )
 
   override fun setupDialect() {

commit 1e319038d188b7539033e5c33e7d89801a237b50

Test for select with group by and coalesce
We ran into this in our queries. I bisected and found ce827d11f551625b01b8c567a991c54e26b42157 introduced a breaking change here. This only breaks when columns are referenced inside of functions (first query works, second doesn't)

dialects/mysql/src/test/fixtures_mysql/select-group-by/Test.s
@@ -1,0 +1,12 @@
+CREATE TABLE test(
+  someColumn VARCHAR(8) NOT NULL,
+  someColumn2 VARCHAR(8) NOT NULL
+);
+
+SELECT someColumn
+FROM test
+GROUP BY someColumn;
+
+SELECT someColumn
+FROM test
+GROUP BY COALESCE(someColumn, 'default');