ea524b9cbb9ebc6f477b3b344d7a943d6cc485f5
Kevin Galligan <kgalligan@gmail.com>
2023-08-25 12:06:16 -0400

    Ensure thread safety for query listener collection on native driver

--- drivers/native-driver/src/nativeMain/kotlin/app/cash/sqldelight/driver/native/NativeSqlDatabase.kt
+++ drivers/native-driver/src/nativeMain/kotlin/app/cash/sqldelight/driver/native/NativeSqlDatabase.kt
@@ -9,6 +9,7 @@ import app.cash.sqldelight.db.SqlCursor
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.db.SqlPreparedStatement
 import app.cash.sqldelight.db.SqlSchema
+import app.cash.sqldelight.driver.native.util.PoolLock
 import co.touchlab.sqliter.DatabaseConfiguration
 import co.touchlab.sqliter.DatabaseConnection
 import co.touchlab.sqliter.DatabaseManager
@@ -136,7 +137,8 @@ class NativeSqliteDriver(
   // Once a transaction is started and connection borrowed, it will be here, but only for that
   // thread
   private val borrowedConnectionThread = ThreadLocalRef<Borrowed<ThreadConnection>>()
-  private val listeners = mutableMapOf<String, MutableMap<Query.Listener, Unit>>()
+  private val listeners = mutableMapOf<String, MutableSet<Query.Listener>>()
+  private val lock = PoolLock(reentrant = true)
 
   init {
     if (databaseManager.configuration.inMemory) {
@@ -173,20 +175,26 @@ class NativeSqliteDriver(
   }
 
   override fun addListener(vararg queryKeys: String, listener: Query.Listener) {
-    queryKeys.forEach {
-      listeners.getOrPut(it) { mutableMapOf() }.put(listener, Unit)
+    lock.withLock {
+      queryKeys.forEach {
+        listeners.getOrPut(it) { mutableSetOf() }.add(listener)
+      }
     }
   }
 
   override fun removeListener(vararg queryKeys: String, listener: Query.Listener) {
-    queryKeys.forEach {
-      listeners.get(it)?.remove(listener)
+    lock.withLock {
+      queryKeys.forEach {
+        listeners.get(it)?.remove(listener)
+      }
     }
   }
 
   override fun notifyListeners(vararg queryKeys: String) {
     val listenersToNotify = mutableSetOf<Query.Listener>()
-    queryKeys.forEach { key -> listeners.get(key)?.let { listenersToNotify.addAll(it.keys) } }
+    lock.withLock {
+      queryKeys.forEach { key -> listeners.get(key)?.let { listenersToNotify.addAll(it) } }
+    }
     listenersToNotify.forEach(Query.Listener::queryResultsChanged)
   }
 
