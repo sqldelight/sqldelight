b486e49f498347e63960281fd97679abb275a887
Marius Volkhart <marius@volkhart.com>
2023-09-01 00:19:59 -0400

    Add Postgres Regex function definitions
    It's not unreasonable for someone to use these functions in a SELECT, so we should have return types for them.

--- dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/PostgreSqlTypeResolver.kt
+++ dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/PostgreSqlTypeResolver.kt
@@ -172,6 +172,9 @@ class PostgreSqlTypeResolver(private val parentResolver: TypeResolver) : TypeRes
     "jsonb_path_exists", "jsonb_path_match", "jsonb_path_exists_tz", "jsonb_path_match_tz" -> IntermediateType(BOOLEAN)
     "currval", "lastval", "nextval", "setval" -> IntermediateType(BIG_INT)
     "generate_series" -> encapsulatingType(exprList, INTEGER, BIG_INT, REAL, TIMESTAMP_TIMEZONE, TIMESTAMP)
+    "regexp_count", "regexp_instr" -> IntermediateType(INTEGER)
+    "regexp_like" -> IntermediateType(BOOLEAN)
+    "regexp_replace", "regexp_substr" -> IntermediateType(TEXT)
     else -> null
   }
 
