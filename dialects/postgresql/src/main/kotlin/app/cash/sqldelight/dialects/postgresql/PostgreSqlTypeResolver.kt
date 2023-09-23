d22fd500dad4be333ab59d63b3cd7b7def95c7c3
griff і⊙ <346896+griffio@users.noreply.github.com>
2023-09-23 15:33:19 +0100

    Update PostgreSqlTypeResolver temporal type
    For encapsulatingType allow expressions to return temporal types
        PostgreSqlType.INTERVAL,
        PostgreSqlType.TIMESTAMP_TIMEZONE
        PostgreSqlType.TIMESTAMP
    
    e.g 31 * INTERVAL '1 minute' + INTERVAL '2 days'
    
    Timestamp should be ordered last
    
    When binary expression contains TIMESTAMP and INTERVAL, TIMESTAMP must be selected

--- dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/PostgreSqlTypeResolver.kt
+++ dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/PostgreSqlTypeResolver.kt
@@ -222,6 +222,9 @@ class PostgreSqlTypeResolver(private val parentResolver: TypeResolver) : TypeRes
           REAL,
           TEXT,
           BLOB,
+          PostgreSqlType.INTERVAL,
+          PostgreSqlType.TIMESTAMP_TIMEZONE,
+          PostgreSqlType.TIMESTAMP,
         )
       }
     }
