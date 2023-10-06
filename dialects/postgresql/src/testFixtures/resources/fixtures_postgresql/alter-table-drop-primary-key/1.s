dac2cba1acb3f81bacc68253ad2081064754e335
Shawn Zurbrigg <shawn@faire.com>
2023-10-06 09:22:09 -0400

    review comments

--- dialects/postgresql/src/testFixtures/resources/fixtures_postgresql/alter-table-drop-primary-key/1.s
+++ dialects/postgresql/src/testFixtures/resources/fixtures_postgresql/alter-table-drop-primary-key/1.s
@@ -7,4 +7,5 @@ CREATE TABLE primary_key_data(
 
 ALTER TABLE primary_key_data
     DROP CONSTRAINT primary_key_data_pkey,
+    DROP CONSTRAINT IF EXISTS non_existent_key,
     ADD CONSTRAINT primary_key_value_pkey PRIMARY KEY (value);
