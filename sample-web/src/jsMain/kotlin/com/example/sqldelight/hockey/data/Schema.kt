6c57055a734b5b30dc09cdc051bc362289babb4e
Derek Ellis <derekellis@squareup.com>
2023-10-18 11:17:51 -0400

    Split web sample into its own project (#4666)
    * Split web sample into its own project
    
    * Remove kotlinUpgradeYarnLock from mobile sample build
    
    * Make gradlew executable

---
+++ sample-web/src/jsMain/kotlin/com/example/sqldelight/hockey/data/Schema.kt
@@ -0,0 +1,1 @@
+package com.example.sqldelight.hockey.data
---
+++ sample-web/webpack.config.d/sqljs-config.js
@@ -0,0 +1,17 @@
+// {project}/webpack.config.d/sqljs.js
+config.resolve = {
+  fallback: {
+    fs: false,
+    path: false,
+    crypto: false,
+  }
+};
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+config.plugins.push(
+  new CopyWebpackPlugin({
+    patterns: [
+      '../../node_modules/sql.js/dist/sql-wasm.wasm'
+    ]
+  })
+);
