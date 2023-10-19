58a1313fc336920dec7ff4d47490024dddd51923
Kevin Galligan <kgalligan@gmail.com>
2023-10-19 02:17:44 -0400

    Kgalligan/2023 10 05/update stately (#4701)

--- extensions/androidx-paging3/src/commonTest/kotlin/app/cash/sqldelight/paging3/RunDbTest.kt
+++ extensions/androidx-paging3/src/commonTest/kotlin/app/cash/sqldelight/paging3/RunDbTest.kt
@@ -3,9 +3,10 @@ package app.cash.sqldelight.paging3
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
+import kotlin.time.Duration.Companion.seconds
 
 @OptIn(ExperimentalCoroutinesApi::class)
-fun DbTest.runDbTest(body: suspend TestScope.() -> Unit) = runTest {
+fun DbTest.runDbTest(body: suspend TestScope.() -> Unit) = runTest(timeout = 1000.seconds) {
   val driver = provideDbDriver()
   setup(driver)
   body()
