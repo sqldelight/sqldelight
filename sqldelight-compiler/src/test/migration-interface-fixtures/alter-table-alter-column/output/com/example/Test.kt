4dccbf5327c490e0a16d407286a303de30b9d289
griff і⊙ <346896+griffio@users.noreply.github.com>
2023-11-22 13:44:54 +0000

    Add migration tests
    Testing that the data class properties have update nullable types and unmodified remain the same

---
+++ sqldelight-compiler/src/test/migration-interface-fixtures/alter-table-alter-column/output/com/example/Test.kt
@@ -0,0 +1,11 @@
+package com.example
+
+import kotlin.Int
+import kotlin.String
+
+public data class Test(
+  public val first: String?,
+  public val second: Int,
+  public val third: String,
+  public val fourth: Int?,
+)
