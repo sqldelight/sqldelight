75fb2939b29e35ae7f235a97b1ac8d946d1066e7
griff і⊙ <346896+griffio@users.noreply.github.com>
2023-11-27 15:55:41 +0000

    AlterTableAlterColumnMixin extends SqlColumnDef to allow operating with TableInterfaceGenerator.
    getColumnConstraintList - use create table constraints - nullability is override in queryColumn
    
    getColumnName - use alter table column name
    
    getColumnType - use alter table column data type if present or use create table column def
    
    getJavadoc - use create table defaults - not used

--- dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/grammar/mixins/AlterTableAlterColumnMixin.kt
+++ dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/grammar/mixins/AlterTableAlterColumnMixin.kt
@@ -1,22 +1,64 @@
 package app.cash.sqldelight.dialects.postgresql.grammar.mixins
 
 import app.cash.sqldelight.dialects.postgresql.grammar.psi.PostgreSqlAlterTableAlterColumn
+import com.alecstrong.sql.psi.core.psi.AliasElement
 import com.alecstrong.sql.psi.core.psi.AlterTableApplier
 import com.alecstrong.sql.psi.core.psi.LazyQuery
+import com.alecstrong.sql.psi.core.psi.NamedElement
+import com.alecstrong.sql.psi.core.psi.SqlColumnConstraint
 import com.alecstrong.sql.psi.core.psi.SqlColumnName
-import com.alecstrong.sql.psi.core.psi.SqlCompositeElementImpl
+import com.alecstrong.sql.psi.core.psi.SqlColumnType
 import com.alecstrong.sql.psi.core.psi.SqlTypes
+import com.alecstrong.sql.psi.core.psi.impl.SqlColumnDefImpl
+import com.alecstrong.sql.psi.core.psi.mixins.ColumnDefMixin
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
 import com.intellij.psi.util.elementType
 
 internal abstract class AlterTableAlterColumnMixin(
   node: ASTNode,
-) : SqlCompositeElementImpl(node),
+) : SqlColumnDefImpl(node),
   PostgreSqlAlterTableAlterColumn,
   AlterTableApplier {
 
-  private val columnName
-    get() = children.filterIsInstance<SqlColumnName>().first()
+  override fun getColumnConstraintList(): MutableList<SqlColumnConstraint> {
+    return (
+      (
+        tablesAvailable(this).first()
+          .query.columns.first { it.element.textMatches(columnName) }.element as NamedElement
+        ).columnDefSource()!!.columnConstraintList
+      )
+  }
+
+  override fun getColumnName(): SqlColumnName {
+    return children.filterIsInstance<SqlColumnName>().first()
+  }
+
+  override fun getColumnType(): SqlColumnType {
+    return children.filterIsInstance<SqlColumnType>().firstOrNull()
+      ?: return (
+        (
+          tablesAvailable(this).first()
+            .query.columns.first { it.element.textMatches(columnName) }.element as NamedElement
+          ).columnDefSource()!!.columnType
+        )
+  }
+
+  override fun getJavadoc(): PsiElement? {
+    return (
+      (
+        tablesAvailable(this).first()
+          .query.columns.first { it.element.textMatches(columnName) }.element as NamedElement
+        ).columnDefSource()!!.javadoc
+      )
+  }
+
+  private fun NamedElement.columnDefSource(): ColumnDefMixin? {
+    if (this.parent is ColumnDefMixin) return this.parent as ColumnDefMixin
+    if (this is AliasElement) return (source() as NamedElement).columnDefSource()
+    if (this is SqlColumnName) return (reference!!.resolve() as NamedElement).columnDefSource()
+    return null
+  }
 
   override fun applyTo(lazyQuery: LazyQuery): LazyQuery {
     val nullableColumn: Boolean? = columnNotNullClause?.let {
@@ -27,11 +69,12 @@ internal abstract class AlterTableAlterColumnMixin(
       }
     }
 
+    val alterColumnTable = getColumnName()
     return LazyQuery(
       tableName = lazyQuery.tableName,
       query = {
         val columns = lazyQuery.query.columns.map { queryColumn ->
-          if (queryColumn.element.textMatches(columnName)) queryColumn.copy(nullable = nullableColumn) else queryColumn
+          if (queryColumn.element.textMatches(alterColumnTable)) queryColumn.copy(element = alterColumnTable, nullable = nullableColumn) else queryColumn
         }
         lazyQuery.query.copy(columns = columns)
       },
