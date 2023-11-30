03c7f1e034559a082b8cad2d240cb2b03aa154ea
griff і⊙ <346896+griffio@users.noreply.github.com>
2023-11-30 16:21:07 +0000

    Fix 4837 alter table alter column (#4846)
    * AlterTableAlterColumnMixin extends SqlColumnDef to allow operating with TableInterfaceGenerator.
    
    getColumnConstraintList - use create table constraints - nullability is override in queryColumn
    
    getColumnName - use alter table column name
    
    getColumnType - use alter table column data type if present or use create table column def
    
    getJavadoc - use create table defaults - not used
    
    * AlterTableAlterColumnMixin tests migration
    Add column and expect type to be migrated from Int (Integer) to Long (BIGINT)
    
    * Fix the null elements
    
    Second version to enable passing tests
    
    TODO - improvements
    
    * Propagate the nullable type
    
    Changing the column type must propagate the existing not null/null constraint
    
    * update tests
    
    Change fifth column to nullable
    Change fifth column to BIGINT (Long)
    Expect Long?
    
    * AlterTableAlterColumnMixin columnConstraintList
    
    getColumnConstraintList return null seems to work as is not used
    
    * getColumnConstraintList returns constraints
    
    reinstate after testing

--- sqldelight-compiler/src/test/migration-interface-fixtures/alter-table-alter-column/output/com/example/DataQueries.kt
+++ sqldelight-compiler/src/test/migration-interface-fixtures/alter-table-alter-column/output/com/example/DataQueries.kt
@@ -4,6 +4,7 @@ import app.cash.sqldelight.TransacterImpl
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.JdbcPreparedStatement
 import kotlin.Int
+import kotlin.Long
 import kotlin.String
 
 public class DataQueries(
@@ -14,14 +15,16 @@ public class DataQueries(
     second: Int,
     third: String,
     fourth: Int?,
+    fifth: Long?,
   ) {
     driver.execute(-2_134_278_654,
-        """INSERT INTO test (first, second, third, fourth) VALUES (?, ?, ?, ?)""", 4) {
+        """INSERT INTO test (first, second, third, fourth, fifth) VALUES (?, ?, ?, ?, ?)""", 5) {
           check(this is JdbcPreparedStatement)
           bindString(0, first)
           bindInt(1, second)
           bindString(2, third)
           bindInt(3, fourth)
+          bindLong(4, fifth)
         }
     notifyQueries(-2_134_278_654) { emit ->
       emit("test")
