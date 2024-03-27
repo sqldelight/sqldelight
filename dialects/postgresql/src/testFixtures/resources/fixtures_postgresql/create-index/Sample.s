1f4bb0aee62af94f0c4267ef0efac2ccebd68ac0
griff і⊙ <346896+griffio@users.noreply.github.com>
2024-03-27 19:23:24 +0000

    Add grammar for GIN (#5027)
    support gin operator classes
    
    Add fixture tests to check basic syntax
    
    Co-authored-by: Philip Wedemann <22521688+hfhbd@users.noreply.github.com>

--- dialects/postgresql/src/testFixtures/resources/fixtures_postgresql/create-index/Sample.s
+++ dialects/postgresql/src/testFixtures/resources/fixtures_postgresql/create-index/Sample.s
@@ -6,3 +6,15 @@ CREATE TABLE abg (
 );
 
 CREATE INDEX CONCURRENTLY beta_gamma_idx ON abg (beta, gamma);
+
+
+CREATE TABLE json_gin(
+  alpha JSONB,
+  beta JSONB
+);
+
+CREATE INDEX gin_alpha_1 ON json_gin USING GIN (alpha);
+CREATE INDEX gin_alpha_beta_2 ON json_gin USING GIN (alpha, beta);
+CREATE INDEX gin_alpha_beta_3 ON json_gin USING GIN (alpha jsonb_ops, beta);
+CREATE INDEX gin_alpha_beta_4 ON json_gin USING GIN (alpha, beta jsonb_path_ops);
+CREATE INDEX gin_alpha_beta_5 ON json_gin USING GIN (alpha jsonb_path_ops, beta jsonb_ops);
