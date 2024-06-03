3f937b5669243d7ab51ed5e418942e242f864091
Eliezer Graber <me@eygraber.com>
2024-06-03 03:40:27 -0400

    Use an in memory db for some JVM tests

--- drivers/kmp-driver/src/jvmTest/kotlin/com/squareup/sqldelight/kmp/KmpQueryTest.kt
+++ drivers/kmp-driver/src/jvmTest/kotlin/com/squareup/sqldelight/kmp/KmpQueryTest.kt
@@ -6,27 +6,9 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.db.SqlSchema
 import app.cash.sqldelight.driver.kmp.KmpSqliteDriver
 import com.squareup.sqldelight.driver.test.QueryTest
-import org.junit.After
-import org.junit.ClassRule
-import org.junit.rules.TemporaryFolder
 
 class KmpQueryTest : QueryTest() {
-  companion object {
-    @JvmField
-    @ClassRule
-    val globalFolder: TemporaryFolder = TemporaryFolder().apply {
-      create()
-    }
-
-    val database = globalFolder.newFile("test.db")
-  }
-
-  @After
-  fun cleanupDatabase() {
-    KmpDriverTest.database.delete()
-  }
-
   override fun setupDatabase(schema: SqlSchema<QueryResult.Value<Unit>>): SqlDriver {
-    return KmpSqliteDriver(BundledSQLiteDriver(), database.absolutePath, schema)
+    return KmpSqliteDriver(BundledSQLiteDriver(), "", schema)
   }
 }
