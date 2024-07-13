a7f249a3b24d3828e07152bc1a419ad6be22215d
griff і⊙ <346896+griffio@users.noreply.github.com>
2024-07-13 15:58:40 +0100

    Update DistinctOnExpressionMixin.kt
    Update DistinctOnExpressionMixin.kt - expose missing tables
    
    Query the columns or any tables prefixing columns in the distinctOn clause
    
    As there could be one or more columns with the same table prefix, remove any duplicates

--- dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/grammar/mixins/DistinctOnExpressionMixin.kt
+++ dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/grammar/mixins/DistinctOnExpressionMixin.kt
@@ -2,11 +2,13 @@ package app.cash.sqldelight.dialects.postgresql.grammar.mixins
 
 import app.cash.sqldelight.dialects.postgresql.grammar.psi.PostgreSqlDistinctOnExpr
 import com.alecstrong.sql.psi.core.SqlAnnotationHolder
+import com.alecstrong.sql.psi.core.psi.NamedElement
 import com.alecstrong.sql.psi.core.psi.QueryElement
 import com.alecstrong.sql.psi.core.psi.SqlColumnName
 import com.alecstrong.sql.psi.core.psi.SqlCompositeElementImpl
 import com.alecstrong.sql.psi.core.psi.SqlResultColumn
 import com.alecstrong.sql.psi.core.psi.SqlSelectStmt
+import com.alecstrong.sql.psi.core.psi.SqlTableName
 import com.alecstrong.sql.psi.core.psi.impl.SqlCompoundSelectStmtImpl
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
@@ -19,7 +21,13 @@ internal abstract class DistinctOnExpressionMixin(node: ASTNode) :
   private val distinctOnColumns get() = children.filterIsInstance<SqlResultColumn>()
 
   override fun queryAvailable(child: PsiElement): Collection<QueryElement.QueryResult> {
-    return (parent as SqlSelectStmt).queryExposed()
+    val distinctOnColumnsWithTablePrefix: List<NamedElement?> =
+      distinctOnColumns.mapNotNull { PsiTreeUtil.findChildOfType(it, SqlTableName::class.java) }
+    return if (distinctOnColumnsWithTablePrefix.isEmpty()) {
+      (parent as SqlSelectStmt).queryExposed()
+    } else {
+      distinctOnColumnsWithTablePrefix.flatMap { tableAvailable(child, it!!.name) }.associateBy { it.table }.values
+    }
   }
 
   // Some idea of the basic validation finds the ORDER BY columns in the DISTINCT ON
