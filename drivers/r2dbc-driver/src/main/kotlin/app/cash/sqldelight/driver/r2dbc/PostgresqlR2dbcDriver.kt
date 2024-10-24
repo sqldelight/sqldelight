8cb29c0f6f34660698c6a747975ba0100ee5a8a7
griff і⊙ <346896+griffio@users.noreply.github.com>
2024-10-24 17:12:42 +0100

    Update PostgresqlR2dbcDriver.kt
    Make compatible with R2dbcDriver
    For integration tests

--- drivers/r2dbc-driver/src/main/kotlin/app/cash/sqldelight/driver/r2dbc/PostgresqlR2dbcDriver.kt
+++ drivers/r2dbc-driver/src/main/kotlin/app/cash/sqldelight/driver/r2dbc/PostgresqlR2dbcDriver.kt
@@ -2,13 +2,16 @@ package app.cash.sqldelight.driver.r2dbc
 
 import app.cash.sqldelight.db.SqlDriver
 import io.r2dbc.spi.Connection
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 /**
  * The native R2dbcDriver Postgresql driver implements the Postgresql wire protocol for bind arguments -
  * that uses positional arguments and index bind parameters - a special implementation is provided here.
  * Other native R2dbcDrivers for MySql/MariaDb use `?` and is the default in SqlDelight
  */
-class PostgresqlR2dbcDriver(connection: Connection) : SqlDriver by R2dbcDriver(connection) {
+class PostgreSqlR2dbcDriver(val connection: Connection, val closed: (Throwable?) -> Unit = { }) : SqlDriver by R2dbcDriver(connection, closed) {
   override fun createArguments(count: Int): String {
     if (count == 0) return "()"
     return buildString(count * 2 + 1) {
@@ -19,4 +22,26 @@ class PostgresqlR2dbcDriver(connection: Connection) : SqlDriver by R2dbcDriver(c
       append(')')
     }
   }
+}
+
+/**
+ * Creates and returns a [PostgreSqlR2dbcDriver] with the given [connection].
+ *
+ * The scope waits until the driver is closed [PostgreSqlR2dbcDriver.close].
+ */
+fun CoroutineScope.PostgreSqlR2dbcDriver(
+  connection: Connection,
+): PostgreSqlR2dbcDriver {
+  val completed = Job()
+  val driver = PostgreSqlR2dbcDriver(connection) {
+    if (it == null) {
+      completed.complete()
+    } else {
+      completed.completeExceptionally(it)
+    }
+  }
+  launch {
+    completed.join()
+  }
+  return driver
 }
