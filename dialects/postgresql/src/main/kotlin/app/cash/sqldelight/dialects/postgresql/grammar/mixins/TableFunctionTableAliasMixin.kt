dffd985b583b69e4e8f5d8be9ae958e42c269281
griff і⊙ <346896+griffio@users.noreply.github.com>
2025-03-19 14:50:25 +0000

    TableOrSubqueryMixin
    Try and support WHERE clause

--- dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/grammar/mixins/TableFunctionTableAliasMixin.kt
+++ dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/grammar/mixins/TableFunctionTableAliasMixin.kt
@@ -8,6 +8,19 @@ internal abstract class TableFunctionTableAliasMixin(
   node: ASTNode,
 ) : SqlTableAliasImpl(node) {
   override fun source(): PsiElement {
-    return (parent.parent.parent as SqlJoinClauseMixin).queryExposed().first().table!!
+    // return (parent.parent.parent as SqlJoinClauseMixin).queryExposed().first().table!!
+    return (parent.parent.parent as SqlJoinClauseMixin).tablesAvailable(this).map { it.tableName }.first()
   }
 }
+
+//
+//
+// //return (parent.parent.parent as SqlJoinClauseMixin).queryExposed().first().table!!
+// val tableOrSubquery = PsiTreeUtil.getParentOfType(this, PostgreSqlTableOrSubquery::class.java)
+// if (tableOrSubquery != null && tableOrSubquery.unnestTableFunction != null) {
+//  // Return the UNNEST function itself as the source
+//  return tableOrSubquery.unnestTableFunction!!
+// }
+//
+// // Fallback to default implementation if not in an UNNEST context
+// return this
