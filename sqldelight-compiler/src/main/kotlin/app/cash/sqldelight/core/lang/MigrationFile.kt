a9ded2f73533352194017547ef98c42b57a47bbb
Madis Pink <mail@madis.pink>
2025-05-13 11:41:13 +0300

    Implement stricter MigrationFile versioning

--- sqldelight-compiler/src/main/kotlin/app/cash/sqldelight/core/lang/MigrationFile.kt
+++ sqldelight-compiler/src/main/kotlin/app/cash/sqldelight/core/lang/MigrationFile.kt
@@ -4,12 +4,37 @@ import app.cash.sqldelight.core.SqlDelightFileIndex
 import com.alecstrong.sql.psi.core.SqlFileBase
 import com.intellij.psi.FileViewProvider
 
+private const val VERSION_ALLOWED_SEPARATORS = ",:.-_T "
+
+/**
+ * Parses a migration version out of a filename
+ *
+ * A version is a string of characters starting with a digit and allowing any number of
+ * [VERSION_ALLOWED_SEPARATORS] characters in between, e.g. `1-2-3`, `1..2.3`, `1_2_3` are all
+ * parsed as the same number.
+ * Any preceding or succeeding characters after the version are ignored.
+ * If the version contains no digits at all, returns 0.
+ */
+internal fun String.toMigrationVersion(): Long {
+  val start = indexOfFirst { it in '0'..'9' }
+  if (start == -1) {
+    // no digits
+    return 0L
+  }
+
+  val len = substring(start)
+    .indexOfFirst { it !in '0'..'9' && it !in VERSION_ALLOWED_SEPARATORS }
+    .takeIf { it != -1 }
+    ?: (length - start)
+
+  return substring(start, start + len).filter { it in '0'..'9' }.toLongOrNull() ?: 0L
+}
+
 class MigrationFile(
   viewProvider: FileViewProvider,
 ) : SqlDelightFile(viewProvider, MigrationLanguage) {
   val version: Long by lazy {
-    name.substringBeforeLast(".$MIGRATION_EXTENSION")
-      .filter { it in '0'..'9' }.toLongOrNull() ?: 0
+    name.toMigrationVersion()
   }
 
   internal fun sqlStatements() = sqlStmtList!!.stmtList
