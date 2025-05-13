572240c5471e3456744575b0f75c0b1351369035
Ilya Gulya <ilya@gulya.me>
2025-05-14 00:20:22 +0500

    Add support for wasmJs to web worker driver (#5534)
    Co-authored-by: Alec Kazakova <1675456+AlecKazakova@users.noreply.github.com>

---
+++ drivers/web-worker-driver/src/commonTest/kotlin/app/cash/sqldelight/drivers/worker/CreateBadWebWorkerDriver.kt
@@ -0,0 +1,5 @@
+package app.cash.sqldelight.drivers.worker
+
+import app.cash.sqldelight.db.SqlDriver
+
+expect fun createBadWebWorkerDriver(): SqlDriver
--- drivers/web-worker-driver/src/wasmJsMain/kotlin/app/cash/sqldelight/driver/worker/api/WorkerResult.kt
+++ drivers/web-worker-driver/src/wasmJsMain/kotlin/app/cash/sqldelight/driver/worker/api/WorkerResult.kt
@@ -3,12 +3,12 @@ package app.cash.sqldelight.driver.worker.api
 /**
  * The results of a SQL operation in the worker.
  */
-internal actual external interface WorkerResult {
+internal actual external interface WorkerResult : JsAny {
   /**
    * The "table" of values in the result, as rows of columns.
    * i.e. `values[row][col]`
    *
    * If the query returns no rows, then this should be an empty array.
    */
-  var values: Array<Array<dynamic>>
+  var values: JsArray<JsArray<JsAny>>?
 }
