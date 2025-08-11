56a4fc120e2fba6a8e572d3b79fd0611a2671398
griff і⊙ <346896+griffio@users.noreply.github.com>
2025-08-11 09:21:32 +0100

    Add exists operator

--- dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/grammar/mixins/ExistsOperatorExpressionMixin.kt
+++ dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/grammar/mixins/ExistsOperatorExpressionMixin.kt
@@ -3,17 +3,33 @@ package app.cash.sqldelight.dialects.postgresql.grammar.mixins
 import app.cash.sqldelight.dialects.postgresql.grammar.psi.PostgreSqlExistsOperatorExpression
 import com.alecstrong.sql.psi.core.SqlAnnotationHolder
 import com.alecstrong.sql.psi.core.psi.SqlBinaryExpr
+import com.alecstrong.sql.psi.core.psi.SqlColumnDef
+import com.alecstrong.sql.psi.core.psi.SqlColumnName
 import com.alecstrong.sql.psi.core.psi.SqlCompositeElementImpl
 import com.alecstrong.sql.psi.core.psi.SqlExpr
 import com.intellij.lang.ASTNode
+import kotlin.text.endsWith
 
+/**
+ * The "?" exists operator is used by Ltree, Jsonb (not Json)
+ * The type annotation is performed here for these types
+ * For other json operators see JsonExpressionMixin
+ */
 internal abstract class ExistsOperatorExpressionMixin(node: ASTNode) :
   SqlCompositeElementImpl(node),
   SqlBinaryExpr,
   PostgreSqlExistsOperatorExpression {
 
   override fun annotate(annotationHolder: SqlAnnotationHolder) {
-    super.annotate(annotationHolder)
+    val columnType = ((firstChild.firstChild.reference?.resolve() as? SqlColumnName)?.parent as? SqlColumnDef)?.columnType?.typeName?.text
+    when {
+      columnType == null ||
+        columnType == "LTREE" ||
+        columnType == "JSONB" ||
+        columnType.endsWith("[]") -> super.annotate(annotationHolder)
+      columnType == "JSON" -> annotationHolder.createErrorAnnotation(firstChild.firstChild, "Left side of jsonb expression must be a jsonb column.")
+      else -> annotationHolder.createErrorAnnotation(firstChild.firstChild, "expression must be JSONB, LTREE.")
+    }
   }
   override fun getExprList(): List<SqlExpr> {
     return children.filterIsInstance<SqlExpr>()
--- dialects/postgresql/src/testFixtures/resources/fixtures_postgresql/json_functions/Test.s
+++ dialects/postgresql/src/testFixtures/resources/fixtures_postgresql/json_functions/Test.s
@@ -12,6 +12,8 @@ SELECT
 --error[col 2]: Left side of jsonb expression must be a jsonb column.
   data <@ datab,
 --error[col 2]: Left side of jsonb expression must be a jsonb column.
+  data ?? 'b',
+--error[col 2]: Left side of jsonb expression must be a jsonb column.
   data ??| '{"a","b"}',
 --error[col 2]: Left side of jsonb expression must be a jsonb column.
   data ??& '{"a"}',
