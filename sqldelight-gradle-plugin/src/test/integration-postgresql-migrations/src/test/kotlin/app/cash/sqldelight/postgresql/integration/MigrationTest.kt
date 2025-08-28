162fd3a607dcf798e28cc6c6b007096521c59e85
Eric Duffy <eric.duffy@engine.com>
2025-08-28 06:13:51 -0500

    Reproduce empty migration

---
+++ sqldelight-gradle-plugin/src/test/integration-postgresql-migrations/src/test/kotlin/app/cash/sqldelight/postgresql/integration/MigrationTest.kt
@@ -0,0 +1,58 @@
+package app.cash.sqldelight.postgresql.integration
+
+import app.cash.sqldelight.Query
+import app.cash.sqldelight.driver.jdbc.JdbcDriver
+import com.google.common.truth.Truth.assertThat
+import java.sql.Connection
+import java.sql.DriverManager
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class MigrationTest {
+  val conn = DriverManager.getConnection("jdbc:tc:postgresql:13.11:///my_db")
+  val driver = object : JdbcDriver() {
+    override fun getConnection() = conn
+    override fun closeConnection(connection: Connection) = Unit
+    override fun addListener(vararg queryKeys: String, listener: Query.Listener) = Unit
+    override fun removeListener(vararg queryKeys: String, listener: Query.Listener) = Unit
+    override fun notifyListeners(vararg queryKeys: String) = Unit
+  }
+  val database = MyDatabase(driver)
+
+  @Before fun before() {
+//    MyDatabase.Schema.create(driver)
+    MyDatabase.Schema.migrate(driver, 0, MyDatabase.Schema.version)
+  }
+
+  @After fun after() {
+    conn.close()
+  }
+
+  @Test fun simpleProductsSelect() {
+    with(database) {
+      productsQueries.insert(Products(1, "sku", true))
+      assertThat(productsQueries.selectAll().executeAsList()).containsExactly(
+        Products(
+          1,
+          "sku",
+          true,
+        ),
+      )
+    }
+  }
+
+  @Test fun simpleOrdersSelect() {
+    with(database) {
+      ordersQueries.insert(Orders(1, "sku", 3, 165.98.toBigDecimal()))
+      assertThat(ordersQueries.selectAll().executeAsList()).containsExactly(
+        Orders(
+          1,
+          "sku",
+          3,
+          165.98.toBigDecimal(),
+        ),
+      )
+    }
+  }
+}
