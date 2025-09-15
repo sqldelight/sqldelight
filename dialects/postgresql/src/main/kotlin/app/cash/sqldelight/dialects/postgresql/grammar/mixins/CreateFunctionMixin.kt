31eb57cfb2220c08c9ff5a5b94dc307a7f38f249
griff і⊙ <346896+griffio@users.noreply.github.com>
2025-09-15 15:28:22 +0100

    Mixins
    Schema aware for Trigger
    
    Function todo

---
+++ dialects/postgresql/src/main/kotlin/app/cash/sqldelight/dialects/postgresql/grammar/mixins/CreateFunctionMixin.kt
@@ -0,0 +1,49 @@
+package app.cash.sqldelight.dialects.postgresql.grammar.mixins
+
+import app.cash.sqldelight.dialects.postgresql.grammar.psi.PostgreSqlCreateFunctionStmt
+import app.cash.sqldelight.dialects.postgresql.grammar.psi.PostgreSqlPlsqlAssignment
+import com.alecstrong.sql.psi.core.psi.QueryElement.QueryResult
+import com.alecstrong.sql.psi.core.psi.SqlCompositeElementImpl
+import com.alecstrong.sql.psi.core.psi.SqlCreateTriggerStmt
+import com.alecstrong.sql.psi.core.psi.SqlFunctionExpr
+import com.alecstrong.sql.psi.core.psi.SqlTypes
+import com.alecstrong.sql.psi.core.psi.mixins.SingleRow
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+/**
+ * CreateFunctionMixin is to support Trigger functions.
+ * The function links to the trigger using the function name
+ */
+internal abstract class CreateFunctionMixin(node: ASTNode) :
+  SqlCompositeElementImpl(node),
+  PostgreSqlCreateFunctionStmt {
+
+  override fun queryAvailable(child: PsiElement): Collection<QueryResult> {
+    val functionName = findChildByType<SqlFunctionExpr>(SqlTypes.FUNCTION_EXPR)!!.text
+    val trigger = containingFile.schema(SqlCreateTriggerStmt::class).find { it.node.findChildByType(SqlTypes.FUNCTION_EXPR)!!.text == functionName }
+    val tableName = trigger!!.tableName!!.name
+
+    if (child is PostgreSqlPlsqlAssignment) { // resolve the query for 'NEW.column := ...' using trigger table
+      return listOfNotNull(
+        tablesAvailable(this).firstOrNull { it.tableName.name == tableName }?.query,
+      )
+    }
+
+    val tableQuery = tablesAvailable(this).firstOrNull {
+      it.tableName.name == tableName
+    }!!.query
+
+    return listOf(
+      QueryResult(
+        SingleRow(tableQuery.table!!, "new"),
+        tableQuery.columns,
+        synthesizedColumns = tableQuery.synthesizedColumns,
+      ),
+      QueryResult(
+        SingleRow(tableQuery.table!!, "old"),
+        tableQuery.columns,
+        synthesizedColumns = tableQuery.synthesizedColumns,
+      ),
+    )
+  }
+}
