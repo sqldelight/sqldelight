2bbf380e5d747c84b745629afab54b400aac55ab
griff і⊙ <346896+griffio@users.noreply.github.com>
2025-09-28 14:26:13 +0100

    Update prepare_mkdocs.sh (#5952)
    Current error `sed: -e expression #1, char 66: unknown option to `s'`
    
    Fix the sed escape character '/' to be different from replacement character '/'
    
    The '/' doesn't need to be escaped now
    
    e.g use another character for sed s@/@/@g

--- .github/workflows/prepare_mkdocs.sh
+++ .github/workflows/prepare_mkdocs.sh
@@ -15,7 +15,7 @@ set -ex
 set +x
 find docs/2.x/ -name '*.html' \
   -exec perl -i~ -0777 -pe 's/<a href="(.*)">([\n\s]+)?(<span>)?([\n\s]+)?SQLDelight([\n\s]+)?(<\/span>)?([\n\s]+)?<\/a>/<a href="\/sqldelight\/'"$1"'"><span>SQLDelight<\/span><\/a>/g' {} \; \
-  -exec sed -i 's/<\/head>/<link rel="icon" href="\/sqldelight\/'"$1"'/images\/icon-cashapp.png"><\/head>/g' {} \;
+  -exec sed -i 's@</head>@<link rel="icon" href="/sqldelight/'"$1"'/images/icon-cashapp.png"></head>@g' {} \;
 set -x
 
 # Copy in special files that GitHub wants in the project root.
