d936e5f9ada46e0560bb5fafc6a6a911f38f2975
griff і⊙ <346896+griffio@users.noreply.github.com>
2025-12-22 12:15:26 +0000

    sqlite-3-38 Integration test
    Only way to be sure

---
+++ sqldelight-gradle-plugin/src/test/integration-sqlite-3-38/settings.gradle
@@ -0,0 +1,7 @@
+pluginManagement {
+    includeBuild("../build-logic-tests")
+}
+
+plugins {
+    id("sqldelightTests")
+}
---
+++ sqldelight-gradle-plugin/src/test/integration-sqlite-3-38/src/test/java/app/cash/sqldelight/integration/IntegrationTests.kt
@@ -0,0 +1,30 @@
+package app.cash.sqldelight.integration
+
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver.Companion.IN_MEMORY
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class IntegrationTests {
+  private lateinit var jsonQueries: JsonQueries
+
+  @Before fun before() {
+    val database = JdbcSqliteDriver(IN_MEMORY)
+    QueryWrapper.Schema.create(database)
+
+    val queryWrapper = QueryWrapper(database)
+    jsonQueries = queryWrapper.jsonQueries
+  }
+
+  @Test fun selectByAlpha() {
+    jsonQueries.insert("""{"alpha" : "abc"}""")
+    jsonQueries.insert("{}")
+    with(jsonQueries.selectAlpha().executeAsList()) {
+      assertThat(first().a).isEqualTo(""""abc"""")
+      assertThat(first().aa).isEqualTo("abc")
+      assertThat(last().a).isNull()
+      assertThat(last().a).isNull()
+    }
+  }
+}
