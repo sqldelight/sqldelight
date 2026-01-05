c1751a6b4b73f20d1201d670d762fa13a513adb1
FletchMcKee <cfmckee.dev@gmail.com>
2026-01-05 09:49:56 -0600

    Replaced mutable state dependencies with schema configurations
    - This is still a WIP as I need to test circular dependencies, but it is close to a final build. I also want to see if creating a single schemaClasspath/schemElements while creating secondary variants for each source is feasible.
    - The bulk of the changes are in SqlDelightDatabase and SqlDelightWorkerTask. The main thing was moving as much of the eager resolution out of SqlDelightDatabase and into the worker task. This caused the abi changes, but there may be a way to get around that. But the dependenciesFrom method requires splitting up the dependency files required for the compilation unit, and the dependency metadata required by the database properties class.

--- sqldelight-gradle-plugin/src/main/kotlin/app/cash/sqldelight/gradle/SqlDelightWorkerTask.kt
+++ sqldelight-gradle-plugin/src/main/kotlin/app/cash/sqldelight/gradle/SqlDelightWorkerTask.kt
@@ -1,10 +1,20 @@
 package app.cash.sqldelight.gradle
 
+import app.cash.sqldelight.core.SqlDelightCompilationUnit
+import app.cash.sqldelight.core.SqlDelightDatabaseProperties
+import app.cash.sqldelight.gradle.SqlDelightDatabase.Companion.DatabaseNameAttribute
+import app.cash.sqldelight.gradle.SqlDelightDatabase.Companion.PackageNameAttribute
 import javax.inject.Inject
+import org.gradle.api.artifacts.result.ResolvedArtifactResult
 import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.provider.ListProperty
+import org.gradle.api.provider.MapProperty
 import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Classpath
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.SourceTask
 import org.gradle.process.JavaForkOptions
@@ -24,6 +34,100 @@ abstract class SqlDelightWorkerTask : SourceTask() {
   /** @see ClassLoaderWorkerSpec.getClasspath */
   @get:Classpath
   abstract val classpath: ConfigurableFileCollection
+
+  @get:Input
+  abstract val packageName: Property<String>
+
+  @get:Input
+  abstract val className: Property<String>
+
+  @get:Input
+  abstract val sourceName: Property<String>
+
+  @get:Input
+  abstract val deriveSchemaFromMigrations: Property<Boolean>
+
+  @get:Input
+  abstract val treatNullAsUnknownForEquality: Property<Boolean>
+
+  @get:Input
+  abstract val generateAsync: Property<Boolean>
+
+  @get:Input
+  abstract val dependencies: ListProperty<SqlDelightDatabaseNameImpl>
+
+  @get:Internal
+  abstract val rootDirectory: DirectoryProperty
+
+  @get:Internal
+  abstract val schemaOutputDirectory: DirectoryProperty
+
+  // Internal since it is added to source.
+  @get:Internal
+  abstract val localSourceDirs: ConfigurableFileCollection
+
+  // Internal since it is added to source.
+  @get:Internal
+  abstract val dependencySourceDirs: ConfigurableFileCollection
+
+  init {
+    deriveSchemaFromMigrations.convention(false)
+    treatNullAsUnknownForEquality.convention(false)
+    generateAsync.convention(false)
+
+    source(localSourceDirs)
+    source(dependencySourceDirs)
+  }
+
+  // The metadata and files have to be split into separate inputs to maintain cache compatibility.
+  // See https://docs.gradle.org/current/userguide/artifact_resolution.html#resolving_artifacts
+  internal fun dependenciesFrom(artifacts: Provider<Set<ResolvedArtifactResult>>) {
+    dependencies.set(
+      artifacts.map { resolvedArtifacts ->
+        resolvedArtifacts.map { artifact ->
+          val packageName = requireNotNull(artifact.variant.attributes.getAttribute(PackageNameAttribute)) {
+            "Dependency missing packageName attribute for ${artifact.id.displayName}"
+          }
+          val databaseName = requireNotNull(artifact.variant.attributes.getAttribute(DatabaseNameAttribute)) {
+            "Dependency missing database name  attribute for ${artifact.id.displayName}"
+          }
+          SqlDelightDatabaseNameImpl(
+            packageName = packageName,
+            className = databaseName,
+          )
+        }
+      },
+    )
+
+    dependencySourceDirs.from(
+      artifacts.map { resolvedArtifacts ->
+        resolvedArtifacts.map { it.file }
+      },
+    )
+  }
+
+  internal fun resolveProperties(): SqlDelightDatabaseProperties = SqlDelightDatabasePropertiesImpl(
+    packageName = packageName.get(),
+    className = className.get(),
+    dependencies = dependencies.get(),
+    rootDirectory = rootDirectory.get().asFile,
+    deriveSchemaFromMigrations = deriveSchemaFromMigrations.get(),
+    treatNullAsUnknownForEquality = treatNullAsUnknownForEquality.get(),
+    generateAsync = generateAsync.get(),
+  )
+
+  internal fun resolveCompilationUnit(outputDirectory: DirectoryProperty): SqlDelightCompilationUnit {
+    val sourceFolders = buildSet {
+      localSourceDirs.forEach { add(SqlDelightSourceFolderImpl(it, false)) }
+      dependencySourceDirs.forEach { add(SqlDelightSourceFolderImpl(it, true)) }
+    }
+
+    return SqlDelightCompilationUnitImpl(
+      name = sourceName.get(),
+      sourceFolders = sourceFolders,
+      outputDirectoryFile = outputDirectory.get().asFile,
+    )
+  }
 
   /** @see JavaForkOptions.getMinHeapSize */
   @get:Internal
