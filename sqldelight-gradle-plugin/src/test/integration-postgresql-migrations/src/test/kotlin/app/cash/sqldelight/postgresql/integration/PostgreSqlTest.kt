911e9616042b30ff9ef226164b2b73c731e808d7
griff і⊙ <346896+griffio@users.noreply.github.com>
2026-02-10 15:45:14 +0000

    Integration Test
    Migration of column type USING expression

--- sqldelight-gradle-plugin/src/test/integration-postgresql-migrations/src/test/kotlin/app/cash/sqldelight/postgresql/integration/PostgreSqlTest.kt
+++ sqldelight-gradle-plugin/src/test/integration-postgresql-migrations/src/test/kotlin/app/cash/sqldelight/postgresql/integration/PostgreSqlTest.kt
@@ -5,8 +5,11 @@ import app.cash.sqldelight.driver.jdbc.JdbcDriver
 import com.google.common.truth.Truth.assertThat
 import java.sql.Connection
 import java.sql.DriverManager
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
 import migrations.app.cash.sqldelight.postgresql.integration.Orders
 import migrations.app.cash.sqldelight.postgresql.integration.Products
+import migrations.app.cash.sqldelight.postgresql.integration.Ts
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -54,6 +57,17 @@ class PostgreSqlTest {
           165.98.toBigDecimal(),
           null,
         ),
+      )
+    }
+  }
+
+  @Test fun migrateTimeStampToTimeStampWithTimeZone() {
+    val ts1 = OffsetDateTime.of(1980, 4, 9, 20, 15, 45, 0, ZoneOffset.ofHours(0))
+    val ts2 = OffsetDateTime.of(1981, 4, 9, 20, 15, 45, 0, ZoneOffset.ofHours(0))
+    with(database) {
+      tsQueries.insert(Ts(ts1, ts2))
+      assertThat(tsQueries.select().executeAsList()).containsExactly(
+        Ts(ts1, ts2),
       )
     }
   }
