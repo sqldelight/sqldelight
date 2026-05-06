634594a49263d2df8cf40d7c862216bacdbf2af5
griff і⊙ <346896+griffio@users.noreply.github.com>
2026-05-06 13:46:46 +0100

    Update Test.s

--- dialects/sqlite-3-44/src/testFixtures/resources/fixtures_sqlite_3_44/aggregate_functions/Test.s
+++ dialects/sqlite-3-44/src/testFixtures/resources/fixtures_sqlite_3_44/aggregate_functions/Test.s
@@ -11,10 +11,16 @@ FROM users;
 SELECT GROUP_CONCAT(DISTINCT name ORDER BY created_at)
 FROM users;
 
--- string_agg is an alias for group_concat in SQLite
-SELECT GROUP_CONCAT(name, ', ' ORDER BY name)
-         FILTER (WHERE active = 1)
+SELECT GROUP_CONCAT(name, '' ORDER BY
+    name COLLATE NOCASE, id
+  ) AS someNames
 FROM users;
 
+SELECT GROUP_CONCAT(name, ', ' ORDER BY name)
+   FILTER (WHERE active = 1)
+FROM users;
+
+-- string_agg is an alias for group_concat in SQLite
 SELECT STRING_AGG(name, ', ' ORDER BY created_at DESC)
 FROM users;
+
