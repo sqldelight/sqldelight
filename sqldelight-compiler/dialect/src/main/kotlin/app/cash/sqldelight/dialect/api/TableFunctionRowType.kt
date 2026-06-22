321da78cc245bc76b6c9d36fbabc0218f50daa50
griff і⊙ <346896+griffio@users.noreply.github.com>
2026-06-22 09:54:47 +0100

    Remove TableFunctionRowType
    Replaced with TableFunctionExprRowType

--- sqldelight-compiler/dialect/src/main/kotlin/app/cash/sqldelight/dialect/api/TableFunctionRowType.kt
+++
@@ -1,8 +0,0 @@
-package app.cash.sqldelight.dialect.api
-
-import com.alecstrong.sql.psi.core.psi.SqlAnnotatedElement
-import com.alecstrong.sql.psi.core.psi.SqlTypeName
-
-interface TableFunctionRowType : SqlAnnotatedElement {
-  fun columnType(): SqlTypeName
-}
